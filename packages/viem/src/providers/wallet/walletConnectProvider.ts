edacf778bf35160d12ec5121fa774dcd0f0fd496
moxey.eth <jakemoxey@gmail.com>
2022-07-30 20:52:39 +1000

    refactor: walletConnectProvider

--- packages/viem/src/providers/wallet/walletConnectProvider.ts
+++ packages/viem/src/providers/wallet/walletConnectProvider.ts
@@ -1,25 +1,44 @@
 import WalletConnect from '@walletconnect/ethereum-provider'
+import { IWCRpcConnectionOptions } from '@walletconnect/types'
 
 import { createProviderSigner } from '../../signers/createProviderSigner'
 import { Chain } from '../../types'
 import { WalletProvider, createWalletProvider } from './createWalletProvider'
 
-export type WalletConnectProviderConfig = {
+export type WalletConnectProviderConfig = IWCRpcConnectionOptions & {
   chains: Chain[]
+  rpcUrl?: (chain: Chain) => string
 }
 
 export type WalletConnectProvider = WalletProvider
 
 export function walletConnectProvider({
+  bridge,
+  chainId,
   chains,
+  clientMeta,
+  connector,
+  qrcode,
+  qrcodeModalOptions,
+  rpcUrl = (chain) => chain.rpcUrls.public,
+  signingMethods,
+  storageId = 'viem.wc',
 }: WalletConnectProviderConfig): WalletConnectProvider {
   const rpc = chains.reduce(
-    (rpc, chain) => ({ ...rpc, [chain.id]: chain.rpcUrls.public }),
+    (rpc, chain) => ({ ...rpc, [chain.id]: rpcUrl(chain) }),
     {},
   )
 
   const provider = new WalletConnect({
+    bridge,
+    chainId,
+    clientMeta,
+    connector,
+    qrcode,
+    qrcodeModalOptions,
     rpc,
+    signingMethods,
+    storageId,
   })
 
   return createWalletProvider({
