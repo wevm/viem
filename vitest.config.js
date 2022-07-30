0a3724b92055c7386ea966b303bb2b9e75234cc2
moxey.eth <jakemoxey@gmail.com>
2022-07-30 20:06:30 +1000

    chore: will not settle for less

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,10 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ['text', 'json', 'html'],
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: 100,
     },
     environment: 'jsdom',
   },
