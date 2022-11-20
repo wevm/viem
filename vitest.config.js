8b8e1d30f66bfec4d0dc7dfef2a5b4625632ea27
moxey.eth <jakemoxey@gmail.com>
2022-11-20 12:35:48 +1100

    chore: exclude tests

--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       branches: 98,
       functions: 98,
       statements: 98,
+      exclude: ['**/dist/**', '**/packages/test/**', '**/*.test.ts'],
     },
     environment: 'jsdom',
     globalSetup: './packages/test/src/setup.ts',
