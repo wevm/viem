65e6680f2d9ebd23ad9b74eede11ba29bf0277d8
moxey.eth <jakemoxey@gmail.com>
2022-11-25 15:00:49 +1100

    fix: test setup

--- packages/core/test/setup.ts
+++ packages/core/test/setup.ts
@@ -1,16 +1,16 @@
 import { testProvider } from '../../test/src/utils'
 
 export async function setup() {
-  testProvider.request({
+  await testProvider.request({
     method: 'anvil_reset',
     params: [
       {
         forking: {
           jsonRpcUrl: process.env.VITE_ANVIL_FORK_URL,
-          blockNumber: process.env.VITE_ANVIL_BLOCK_NUMBER,
+          blockNumber: parseInt(process.env.VITE_ANVIL_BLOCK_NUMBER!),
         },
       },
     ],
   })
-  testProvider.request({ method: 'evm_setAutomine', params: [false] })
+  await testProvider.request({ method: 'evm_setAutomine', params: [false] })
 }
