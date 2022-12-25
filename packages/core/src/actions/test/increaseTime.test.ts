6fd8594aafa832d5204799313462219e17f79245
moxey.eth <jakemoxey@gmail.com>
2022-12-25 19:03:50 +1100

    tests: fix increaseTime

--- packages/core/src/actions/test/increaseTime.test.ts
+++ packages/core/src/actions/test/increaseTime.test.ts
@@ -1,17 +1,17 @@
 import { expect, test } from 'vitest'
 
 import { publicClient, testClient } from '../../../test'
-import { wait } from '../../utils/wait'
 
 import { getBlock } from '../block'
 import { increaseTime } from './increaseTime'
+import { mine } from './mine'
 
 test('increases time', async () => {
   const block1 = await getBlock(publicClient, {
     blockTag: 'latest',
   })
   await increaseTime(testClient, { seconds: 86400 })
-  await wait(1000)
+  await mine(testClient, { blocks: 1 })
   const block2 = await getBlock(publicClient, { blockTag: 'latest' })
-  expect(block2.timestamp).toBeGreaterThan(block1.timestamp + 86400n)
+  expect(block2.timestamp).toBeGreaterThan(block1.timestamp + (86400n - 1000n))
 })
