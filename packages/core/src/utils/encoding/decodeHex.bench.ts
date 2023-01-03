d0467d8308c0117495635e3f1f2f65fd0a156927
moxey.eth <jakemoxey@gmail.com>
2023-01-03 16:20:47 +1100

    bench: more benchmarks

--- packages/core/src/utils/encoding/decodeHex.bench.ts
+++ packages/core/src/utils/encoding/decodeHex.bench.ts
@@ -1,7 +1,9 @@
+import { arrayify } from 'ethers/lib/utils'
+import { arrayify as arrayify2 } from 'essential-eth'
 import { bench, describe } from 'vitest'
 import Web3 from 'web3'
 
-import { hexToNumber } from './decodeHex'
+import { hexToBytes, hexToNumber, hexToString } from './decodeHex'
 
 describe('Hex to Number', () => {
   bench('viem: `hexToNumber`', () => {
@@ -10,5 +12,33 @@ describe('Hex to Number', () => {
 
   bench('web3.js: `hexToNumber`', () => {
     Web3.utils.hexToNumber('0x1a4')
+  })
+})
+
+describe('Hex to Bytes', () => {
+  bench('viem: `hexToBytes`', () => {
+    hexToBytes('0x48656c6c6f20576f726c6421')
+  })
+
+  bench('ethers: `arrayify`', () => {
+    arrayify('0x48656c6c6f20576f726c6421')
+  })
+
+  bench('web3.js: `hexToBytes`', () => {
+    Web3.utils.hexToBytes('0x48656c6c6f20576f726c6421')
+  })
+
+  bench('essential-eth: `arrayify`', () => {
+    arrayify2('0x48656c6c6f20576f726c6421')
+  })
+})
+
+describe('Hex to String', () => {
+  bench('viem: `hexToString`', () => {
+    hexToString('0x48656c6c6f20576f726c6421')
+  })
+
+  bench('web3.js: `hexToString`', () => {
+    Web3.utils.hexToString('0x48656c6c6f20576f726c6421')
   })
 })
--- packages/core/src/utils/encoding/encodeHex.bench.ts
+++ packages/core/src/utils/encoding/encodeHex.bench.ts
@@ -1,14 +1,60 @@
+import { hexlify } from 'ethers/lib/utils'
+import { hexlify as hexlify2 } from 'essential-eth'
 import { bench, describe } from 'vitest'
 import Web3 from 'web3'
 
-import { numberToHex } from './encodeHex'
+import { bytesToHex, numberToHex, stringToHex } from './encodeHex'
 
 describe('Number to Hex', () => {
   bench('viem: `numberToHex`', () => {
     numberToHex(52)
   })
 
+  bench('ethers: `hexlify`', () => {
+    hexlify(52)
+  })
+
   bench('web3.js: `numberToHex`', () => {
     Web3.utils.numberToHex(52)
+  })
+
+  bench('essential-eth: `hexlify`', () => {
+    hexlify2(52)
+  })
+})
+
+describe('String to Hex', () => {
+  bench('viem: `stringToHex`', () => {
+    stringToHex('Hello world.')
+  })
+
+  bench('web3.js: `stringToHex`', () => {
+    Web3.utils.stringToHex('Hello world.')
+  })
+})
+
+describe('Bytes to Hex', () => {
+  bench('viem: `bytesToHex`', () => {
+    bytesToHex(
+      new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]),
+    )
+  })
+
+  bench('ethers: `hexlify`', () => {
+    hexlify(
+      new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]),
+    )
+  })
+
+  bench('web3.js: `bytesToHex`', () => {
+    Web3.utils.bytesToHex([
+      72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33,
+    ])
+  })
+
+  bench('essential-eth: `hexlify`', () => {
+    hexlify2(
+      new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]),
+    )
   })
 })
