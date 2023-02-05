6e10cebb473b24f9bec637f013807e6fcd98b40e
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-05 12:54:17 -0800

    make WatchBlocks more typesafe
    make watchBlocks more typesafe

--- src/actions/public/watchBlocks.ts
+++ src/actions/public/watchBlocks.ts
@@ -6,32 +6,53 @@ import { poll } from '../../utils/poll'
 import type { GetBlockResponse } from './getBlock'
 import { getBlock } from './getBlock'
 
-export type OnBlockResponse<TChain extends Chain = Chain> =
-  GetBlockResponse<TChain>
-export type OnBlock<TChain extends Chain = Chain> = (
-  block: OnBlockResponse<TChain>,
-  prevBlock: OnBlockResponse<TChain> | undefined,
+export type OnBlockResponse<
+  TChain extends Chain = Chain,
+  TOmitFields extends string = '',
+> = Omit<GetBlockResponse<TChain>, TOmitFields>
+export type OnBlock<
+  TChain extends Chain = Chain,
+  TOmitFields extends string = '',
+> = (
+  block: OnBlockResponse<TChain, TOmitFields>,
+  prevBlock: OnBlockResponse<TChain, TOmitFields> | undefined,
 ) => void
 
-export type WatchBlocksArgs<TChain extends Chain = Chain> = {
+type BaseWatchBlocksArgs<TChain extends Chain = Chain> = {
   /** The block tag. Defaults to "latest". */
   blockTag?: BlockTag
   /** Whether or not to emit the missed blocks to the callback. */
   emitMissed?: boolean
   /** Whether or not to emit the block to the callback when the subscription opens. */
   emitOnBegin?: boolean
-  /** The callback to call when a new block is received. */
-  onBlock: OnBlock<TChain>
   /** The callback to call when an error occurred when trying to get for a new block. */
   onError?: (error: Error) => void
-  /** Whether or not to include transaction data in the response. */
-  includeTransactions?: boolean
   /** Polling frequency (in ms). Defaults to the client's pollingInterval config. */
   pollingInterval?: number
 }
 
+type OnBlockArgs<TChain extends Chain = Chain> =
+  | {
+      /** Whether or not to include transaction data in the response. */
+      includeTransactions: true
+      /** The callback to call when a new block is received. */
+      onBlock: OnBlock<TChain>
+    }
+  | {
+      /** Whether or not to include transaction data in the response. */
+      includeTransactions?: false
+      /** The callback to call when a new block is received. */
+      onBlock: OnBlock<TChain, 'transactions'>
+    }
+
+export type WatchBlocksArgs<TChain extends Chain = Chain> =
+  BaseWatchBlocksArgs<TChain> & OnBlockArgs<TChain>
+
 /** @description Watches and returns information for incoming blocks. */
-export function watchBlocks<TChain extends Chain>(
+export function watchBlocks<
+  TChain extends Chain,
+  TWatchBlocksArgs extends WatchBlocksArgs<TChain>,
+>(
   client: PublicClient<any, TChain>,
   {
     blockTag = 'latest',
@@ -41,7 +62,7 @@ export function watchBlocks<TChain extends Chain>(
     onError,
     includeTransactions = false,
     pollingInterval = client.pollingInterval,
-  }: WatchBlocksArgs<TChain>,
+  }: TWatchBlocksArgs,
 ) {
   const observerId = JSON.stringify([
     'watchBlocks',
