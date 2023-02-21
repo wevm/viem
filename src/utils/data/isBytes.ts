9ae8bc19cdc3b2ba55960124bd9c5f91186a4dec
Malone Hedges <malonehedges@users.noreply.github.com>
2023-02-21 09:06:06 -0800

    unknown instead of any

--- src/utils/data/isBytes.ts
+++ src/utils/data/isBytes.ts
@@ -1,8 +1,9 @@
 import { ByteArray } from '../../types'
 
-export function isBytes(value: any): value is ByteArray {
+export function isBytes(value: unknown): value is ByteArray {
   if (!value) return false
   if (typeof value !== 'object') return false
+  if (!('BYTES_PER_ELEMENT' in value)) return false
   return (
     value.BYTES_PER_ELEMENT === 1 && value.constructor.name === 'Uint8Array'
   )
--- src/utils/data/isHex.ts
+++ src/utils/data/isHex.ts
@@ -1,6 +1,6 @@
 import { Hex } from '../../types'
 
-export function isHex(value: any): value is Hex {
+export function isHex(value: unknown): value is Hex {
   if (!value) return false
   if (typeof value !== 'string') return false
   return /^0x[0-9a-fA-F]*$/.test(value)
