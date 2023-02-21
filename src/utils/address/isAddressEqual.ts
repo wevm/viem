0fdce023b13daafaa11940ff6c7f46ab3cc61daf
Malone Hedges <malonehedges@users.noreply.github.com>
2023-02-21 01:15:22 -0800

    remove old implementation

--- src/utils/address/isAddressEqual.ts
+++ src/utils/address/isAddressEqual.ts
@@ -1,11 +1,6 @@
 import type { Address } from '../../types'
 import { InvalidAddressError } from '../../errors'
-import { getAddress } from './getAddress'
 import { isAddress } from './isAddress'
-
-export function isAddressEqualOld(a: Address, b: Address) {
-  return getAddress(a) === getAddress(b)
-}
 
 export function isAddressEqual(a: Address, b: Address) {
   if (!isAddress(a)) throw new InvalidAddressError({ address: a })
