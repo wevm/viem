517aaca980ec101f484088f4207d9600a8685a0c
moxey.eth <jakemoxey@gmail.com>
2023-02-23 16:07:11 +1100

    fix: parseUnit conversion

--- src/utils/unit/formatUnit.test.ts
+++ src/utils/unit/formatUnit.test.ts
@@ -10,6 +10,7 @@ test('converts value to number', () => {
   expect(formatUnit(4200000000000n, 10)).toMatchInlineSnapshot('"420"')
   expect(formatUnit(20000000000n, 9)).toMatchInlineSnapshot('"20"')
   expect(formatUnit(40000000000000000000n, 18)).toMatchInlineSnapshot('"40"')
+  expect(formatUnit(10000000000000n, 18)).toMatchInlineSnapshot('"0.00001"')
   expect(formatUnit(12345n, 4)).toMatchInlineSnapshot('"1.2345"')
   expect(formatUnit(12345n, 4)).toMatchInlineSnapshot('"1.2345"')
   expect(formatUnit(6942069420123456789123450000n, 18)).toMatchInlineSnapshot(
--- src/utils/unit/formatUnit.ts
+++ src/utils/unit/formatUnit.ts
@@ -4,11 +4,12 @@ export function formatUnit(value: bigint, decimals: number) {
   const negative = display.startsWith('-')
   if (negative) display = display.slice(1)
 
+  display = display.padStart(decimals, '0')
+
   let [integer, fraction] = [
     display.slice(0, display.length - decimals),
     display.slice(display.length - decimals),
   ]
-  fraction = fraction.padStart(decimals, '0')
   fraction = fraction.replace(/(0+)$/, '')
   return `${negative ? '-' : ''}${integer || '0'}${
     fraction ? `.${fraction}` : ''
