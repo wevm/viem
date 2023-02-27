a09017099f27fb21175c359fe8099e56dd409772
jxom <jakemoxey@gmail.com>
2023-02-28 09:33:58 +1100

    docs: update footer

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -60,7 +60,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022 wagmi',
+      copyright: 'Copyright © 2023-present weth, LLC',
     },
 
     logo: { light: '/icon-light.png', dark: '/icon-dark.png' },
