6f43dbb2c639428c1379e37f91c59a8a6d694a95
Tom Meagher <tom@meagher.co>
2023-03-03 14:03:57 -0500

    chore: fix lint error

--- src/actions/public/estimateGas.bench.ts
+++ src/actions/public/estimateGas.bench.ts
@@ -5,7 +5,6 @@ import {
   ethersProvider,
   ethersV6Provider,
   publicClient,
-  web3Provider,
 } from '../../_test'
 import { getAccount, parseEther } from '../../utils'
 
@@ -20,7 +19,7 @@ describe.skip('Estimate Gas', () => {
     })
   })
 
-  bench('ethers: `estimateGas`', async () => {
+  bench('ethers@5: `estimateGas`', async () => {
     await ethersProvider.estimateGas({
       from: accounts[0].address,
       to: accounts[1].address,
@@ -33,14 +32,6 @@ describe.skip('Estimate Gas', () => {
       from: accounts[0].address,
       to: accounts[1].address,
       value: parseEther('1'),
-    })
-  })
-
-  bench('web3.js: `estimateGas`', async () => {
-    await web3Provider.eth.estimateGas({
-      from: accounts[0].address,
-      to: accounts[1].address,
-      value: '1000000000000',
     })
   })
 })
