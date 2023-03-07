802b21cc098f2d195e66a21e1d2b85b510dcbcf0
0xYami <118700396+0xYami@users.noreply.github.com>
2023-03-07 21:45:52 +0100

    examples: add `watching-blocks` example (#149)

---
+++ examples/blocks/watching-blocks/README.md
@@ -0,0 +1,5 @@
+# Watching Blocks Example
+
+An example of watching block numbers & blocks in viem.
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/wagmi-dev/viem/tree/main/examples/blocks/watching-blocks)
---
+++ examples/blocks/watching-blocks/index.ts
@@ -0,0 +1,23 @@
+import { createPublicClient, http, stringify } from 'viem'
+import { mainnet } from 'viem/chains'
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
+client.watchBlockNumber({
+  onBlockNumber: (blockNumber) => {
+    document.getElementById('block-number')!.innerHTML = `Block number: ${blockNumber}`;
+  }
+})
+
+client.watchBlocks({
+  onBlock: (block) => {
+    document.getElementById('block')!.innerHTML = `Block: <pre><code>${stringify(
+      block,
+      null,
+      2
+    )}</code></pre>`;
+  }
+})
