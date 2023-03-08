97d08f1f148566e700c43d43e54d564d6677826d
moxey.eth <jakemoxey@gmail.com>
2023-03-08 12:38:23 +1100

    tests: update snaps

--- src/utils/formatters/log.test.ts
+++ src/utils/formatters/log.test.ts
@@ -60,7 +60,7 @@ test('nullish values', () => {
   ).toMatchInlineSnapshot(`
     {
       "address": "0x15d4c048f83bd7e37d49ea4c83a07267ec4203da",
-      "blockHash": "0x89644bbd5c8d682a2e9611170e6c1f02573d866d286f006cbf517eec7254ec2d",
+      "blockHash": null,
       "blockNumber": null,
       "data": "0x0000000000000000000000000000000000000000000000000000002b3b6fb3d0",
       "logIndex": null,
@@ -70,7 +70,7 @@ test('nullish values', () => {
         "0x000000000000000000000000a00f99bc38b1ecda1fd70eaa1cd31d576a9f46b0",
         "0x000000000000000000000000f16e9b0d03470827a95cdfd0cb8a8a3b46969b91",
       ],
-      "transactionHash": "0xa4b1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
+      "transactionHash": null,
       "transactionIndex": null,
     }
   `)
