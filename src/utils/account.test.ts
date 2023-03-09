8ad09f16b48bb0d65288ec53d43c4d7ed96b932d
moxey.eth <jakemoxey@gmail.com>
2023-03-09 16:48:06 +1100

    update snapshots

--- src/utils/account.test.ts
+++ src/utils/account.test.ts
@@ -40,6 +40,7 @@ describe('account', () => {
         "address": "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
         "signMessage": [Function],
         "signTransaction": [Function],
+        "signTypedData": [Function],
         "type": "local",
       }
     `)
