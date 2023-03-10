b6f6f20c46b45ed866bb8d3d21cbdb53e3579a25
Tom Meagher <tom@meagher.co>
2023-03-10 16:52:44 -0500

    refactor: internal abitype file

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -1,5 +1,5 @@
 import { Chain as Chain_ } from '@wagmi/chains'
-import { Address } from './abitype'
+import { Address } from 'abitype'
 import { Formatters } from './formatter'
 
 export type Chain<TFormatters extends Formatters = Formatters> = Chain_ & {
