8ccd76854e259d13505a4536aa99ff658bf10791
saklani <50768838+saklani@users.noreply.github.com>
2023-03-12 02:22:10 +0530

    added some tests for verifyMessage

--- src/actions/wallet/verifyMessage.test.ts
+++ src/actions/wallet/verifyMessage.test.ts
@@ -1,30 +1,41 @@
 import { expect, test } from 'vitest'
 
 import { accounts, walletClient } from '../../_test'
-import { getAccount } from '../../utils'
+import { getAccount, toHex } from '../../utils'
 import { getAccount as getEthersAccount } from '../../ethers'
 
 import { signMessage } from './signMessage'
-import { Wallet } from 'ethers@6'
+import { hashMessage, Wallet } from 'ethers@6'
+import { verifyMessage } from './verifyMessage'
 
-test('default', async () => {
+test('sign message', async () => {
+  const data = 'hello world'
+  const signature = await signMessage(walletClient!, {
+    account: getAccount(accounts[0].address),
+    data: data,
+  })
   expect(
-    await signMessage(walletClient!, {
-      account: getAccount(accounts[0].address),
-      data: 'hello world',
+    verifyMessage(walletClient!, {
+      messageHash: toHex(hashMessage(data)),
+      signature: signature,
+      address: accounts[0].address,
     }),
-  ).toMatchInlineSnapshot(
-    '"0xa461f509887bd19e312c0c58467ce8ff8e300d3c1a90b608a760c5b80318eaf15fe57c96f9175d6cd4daad4663763baa7e78836e067d0163e9a2ccf2ff753f5b1b"',
-  )
+  ).toBeTruthy()
+})
 
+test('sign message ethers', async () => {
+  const data = 'hello world'
   const wallet = new Wallet(accounts[0].privateKey)
   const account = getEthersAccount(wallet)
+  const signature = await signMessage(walletClient!, {
+    account,
+    data: data,
+  })
   expect(
-    await signMessage(walletClient!, {
-      account,
-      data: 'hello world',
+    verifyMessage(walletClient!, {
+      messageHash: toHex(hashMessage(data)),
+      signature: signature,
+      address: account.address,
     }),
-  ).toMatchInlineSnapshot(
-    '"0xa461f509887bd19e312c0c58467ce8ff8e300d3c1a90b608a760c5b80318eaf15fe57c96f9175d6cd4daad4663763baa7e78836e067d0163e9a2ccf2ff753f5b1b"',
-  )
+  ).toBeTruthy()
 })
