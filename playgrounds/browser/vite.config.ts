0e5130373d07bedc24766ae1cdf3b3236f9bdcdf
Sebastian Lorenz <fubhy@fubhy.com>
2023-03-31 09:52:41 +0200

    chore: add path aliases to playgrounds to link them to the `viem` src

--- playgrounds/browser/vite.config.ts
+++ playgrounds/browser/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import aliases from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), aliases()],
 })
