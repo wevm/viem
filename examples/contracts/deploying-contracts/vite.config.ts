3baa78394d841f6eaf8f72b713c13cb2770d6864
Sebastian Lorenz <fubhy@fubhy.com>
2023-03-31 16:05:29 +0200

    chore: standardize all examples

--- examples/contracts/deploying-contracts/vite.config.ts
+++ examples/contracts/deploying-contracts/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import aliases from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), aliases()],
+  build: {
+    target: 'esnext',
+  },
 })
