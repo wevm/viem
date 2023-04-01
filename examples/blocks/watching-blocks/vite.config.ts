b849ff7a1e716cb2d7c5ce0cb9b03541606eddc5
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-01 13:04:17 +0200

    chore: remove path aliases from examples

--- examples/blocks/watching-blocks/vite.config.ts
+++ examples/blocks/watching-blocks/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import aliases from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [aliases()],
   build: {
     target: 'esnext',
   },
--- examples/contracts/writing-to-contracts/vite.config.ts
+++ examples/contracts/writing-to-contracts/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import aliases from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
