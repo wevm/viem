1eb42b611b89ab8f15eabd2079d57ea8595c5a06
moxey.eth <jakemoxey@gmail.com>
2023-04-04 09:48:20 +1000

    wip:

--- src/accounts/wordlists/czech.ts
+++ src/accounts/wordlists/czech.ts
@@ -1,2 +1,2 @@
-import czech_ from '@scure/bip39/wordlists/czech'
+import czech_ from '@scure/bip39/wordlists/czech.js'
 export const czech = czech_.wordlist
--- src/accounts/wordlists/english.ts
+++ src/accounts/wordlists/english.ts
@@ -1,2 +1,2 @@
-import english_ from '@scure/bip39/wordlists/english'
+import english_ from '@scure/bip39/wordlists/english.js'
 export const english = english_.wordlist
