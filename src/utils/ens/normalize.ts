b866c4ae0391e76a450a8f5700dc0c16367eb3b2
moxey.eth <jakemoxey@gmail.com>
2023-04-04 08:43:56 +1000

    fix: uts46 lib import

--- src/utils/ens/normalize.ts
+++ src/utils/ens/normalize.ts
@@ -1,4 +1,4 @@
-import { toUnicode } from 'idna-uts46-hx'
+import uts46 from 'idna-uts46-hx'
 
 /**
  * @description Normalizes ENS name
@@ -10,5 +10,5 @@ import { toUnicode } from 'idna-uts46-hx'
  * @see https://docs.ens.domains/contract-api-reference/name-processing#normalising-names
  */
 export function normalize(name: string) {
-  return toUnicode(name, { useStd3ASCII: true })
+  return uts46.toUnicode(name, { useStd3ASCII: true })
 }
