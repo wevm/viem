e037b46644a35e54c08c90c9c7e72bcc12409f4a
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-04 23:20:48 +0200

    chore: apply rome on full package (#307)

--- examples/transactions/fetching-transactions/index.ts
+++ examples/transactions/fetching-transactions/index.ts
@@ -6,14 +6,14 @@ const client = createPublicClient({
   transport: http(),
 })
 
-const hash = '0xfa76a53a1b816c30f98f5456982cd0ccbb2c88b24049c928dfe4db6c8ae89aed'
+const hash =
+  '0xfa76a53a1b816c30f98f5456982cd0ccbb2c88b24049c928dfe4db6c8ae89aed'
 
-const [transaction, transactionReceipt, confirmations] =
-  await Promise.all([
-    client.getTransaction({ hash }),
-    client.getTransactionReceipt({ hash }),
-    client.getTransactionConfirmations({ hash }),
-  ])
+const [transaction, transactionReceipt, confirmations] = await Promise.all([
+  client.getTransaction({ hash }),
+  client.getTransactionReceipt({ hash }),
+  client.getTransactionConfirmations({ hash }),
+])
 
 export default [
   `Current confirmations: ${confirmations}`,
