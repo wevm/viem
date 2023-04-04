134618d34c71335e905ed1612aa211e36542cf12
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-05 00:54:17 +0200

    chore: switch to tsc

---
+++ scripts/updateVersion.ts
@@ -0,0 +1,9 @@
+import { readJsonSync, writeFileSync } from 'fs-extra'
+import path from 'path'
+
+// Writes the current package.json version to `./src/errors/version.ts`.
+const versionFilePath = path.join(__dirname, '../src/errors/version.ts')
+const packageJsonPath = path.join(__dirname, '../package.json')
+const packageVersion = readJsonSync(packageJsonPath).version
+
+writeFileSync(versionFilePath, `export const version = '${packageVersion}'`)
