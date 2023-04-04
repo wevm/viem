9ba2f1c8c3125e37f4558d20cc62ce698c169223
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-03 18:49:58 -0700

    toBytes -> packetToBytes

--- src/utils/ens/packetToBytes.ts
+++ src/utils/ens/packetToBytes.ts
@@ -1,6 +1,6 @@
 // Adapted from https://github.com/mafintosh/dns-packet
 import type { ByteArray } from '../../types'
-import { toBytes } from '../encoding'
+import { stringToBytes } from '../encoding'
 
 /*
  * @description Encodes a DNS packet into a ByteArray containing a UDP payload.
@@ -8,7 +8,7 @@ import { toBytes } from '../encoding'
 export function packetToBytes(packet: string): ByteArray {
   function length(value: string) {
     if (value === '.' || value === '..') return 1
-    return toBytes(value.replace(/^\.|\.$/gm, '')).length + 2
+    return stringToBytes(value.replace(/^\.|\.$/gm, '')).length + 2
   }
 
   const bytes = new Uint8Array(length(packet))
@@ -19,7 +19,7 @@ export function packetToBytes(packet: string): ByteArray {
   let offset = 0
   const list = value.split('.')
   for (let i = 0; i < list.length; i++) {
-    const encoded = toBytes(list[i])
+    const encoded = stringToBytes(list[i])
     bytes[offset] = encoded.length
     bytes.set(encoded, offset + 1)
     offset += encoded.length + 1
