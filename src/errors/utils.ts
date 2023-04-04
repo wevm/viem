297c3ab44fb0e5aaa0980a1fd4171127ba04b57a
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-05 00:54:17 +0200

    chore: switch to tsc

--- src/errors/utils.ts
+++ src/errors/utils.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { Address } from 'abitype'
-import pkg from '../../package.json'
+import { version } from './version.js'
 
 export const getContractAddress = (address: Address) => address
 export const getUrl = (url: string) => url
-export const getVersion = () => `${pkg.name}@${pkg.version}`
+export const getVersion = () => `viem@${version}`
