26b781b83d3c245ea6b09e972f14285bb5f2c544
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2023-04-17 17:16:31 +0100

    chore: lint

--- src/utils/transaction/serializeTransaction.ts
+++ src/utils/transaction/serializeTransaction.ts
@@ -2,7 +2,7 @@ import {
   InvalidAddressError,
   InvalidLegacyVError,
   InvalidStorageKeySizeError,
-} from "../../errors/index.js";
+} from '../../errors/index.js'
 import type {
   AccessList,
   Hex,
@@ -16,53 +16,53 @@ import type {
   TransactionSerializedEIP2930,
   TransactionSerializedLegacy,
   TransactionType,
-} from "../../types/index.js";
-import { isAddress } from "../address/index.js";
-import { concatHex } from "../data/index.js";
-import { toHex, toRlp } from "../encoding/index.js";
-import type { RecursiveArray } from "../encoding/toRlp.js";
+} from '../../types/index.js'
+import { isAddress } from '../address/index.js'
+import { concatHex } from '../data/index.js'
+import { toHex, toRlp } from '../encoding/index.js'
+import type { RecursiveArray } from '../encoding/toRlp.js'
 import {
   assertTransactionEIP1559,
   assertTransactionEIP2930,
   assertTransactionLegacy,
-} from "./assertTransaction.js";
-import { getTransactionType } from "./getTransactionType.js";
-import type { GetTransactionType } from "./getTransactionType.js";
+} from './assertTransaction.js'
+import { getTransactionType } from './getTransactionType.js'
+import type { GetTransactionType } from './getTransactionType.js'
 
 export type SerializedTransactionReturnType<
   TTransactionSerializable extends TransactionSerializable = TransactionSerializable,
-  TTransactionType extends TransactionType = GetTransactionType<TTransactionSerializable>
-> = TransactionSerialized<TTransactionType>;
+  TTransactionType extends TransactionType = GetTransactionType<TTransactionSerializable>,
+> = TransactionSerialized<TTransactionType>
 
 export function serializeTransaction<
-  TTransactionSerializable extends TransactionSerializable
+  TTransactionSerializable extends TransactionSerializable,
 >(
   transaction: TTransactionSerializable,
-  signature?: Signature
+  signature?: Signature,
 ): SerializedTransactionReturnType<TTransactionSerializable> {
-  const type = getTransactionType(transaction) as GetTransactionType;
+  const type = getTransactionType(transaction) as GetTransactionType
 
-  if (type === "eip1559")
+  if (type === 'eip1559')
     return serializeTransactionEIP1559(
       transaction as TransactionSerializableEIP1559,
-      signature
-    ) as SerializedTransactionReturnType;
+      signature,
+    ) as SerializedTransactionReturnType
 
-  if (type === "eip2930")
+  if (type === 'eip2930')
     return serializeTransactionEIP2930(
       transaction as TransactionSerializableEIP2930,
-      signature
-    ) as SerializedTransactionReturnType;
+      signature,
+    ) as SerializedTransactionReturnType
 
   return serializeTransactionLegacy(
     transaction as TransactionSerializableLegacy,
-    signature
-  ) as SerializedTransactionReturnType;
+    signature,
+  ) as SerializedTransactionReturnType
 }
 
 function serializeTransactionEIP1559(
   transaction: TransactionSerializableEIP1559,
-  signature?: Signature
+  signature?: Signature,
 ): TransactionSerializedEIP1559 {
   const {
     chainId,
@@ -74,131 +74,131 @@ function serializeTransactionEIP1559(
     maxPriorityFeePerGas,
     accessList,
     data,
-  } = transaction;
+  } = transaction
 
-  assertTransactionEIP1559(transaction);
+  assertTransactionEIP1559(transaction)
 
-  const serializedAccessList = serializeAccessList(accessList);
+  const serializedAccessList = serializeAccessList(accessList)
 
   const serializedTransaction = [
     toHex(chainId),
-    nonce ? toHex(nonce) : "0x",
-    maxPriorityFeePerGas ? toHex(maxPriorityFeePerGas) : "0x",
-    maxFeePerGas ? toHex(maxFeePerGas) : "0x",
-    gas ? toHex(gas) : "0x",
-    to ?? "0x",
-    value ? toHex(value) : "0x",
-    data ?? "0x",
+    nonce ? toHex(nonce) : '0x',
+    maxPriorityFeePerGas ? toHex(maxPriorityFeePerGas) : '0x',
+    maxFeePerGas ? toHex(maxFeePerGas) : '0x',
+    gas ? toHex(gas) : '0x',
+    to ?? '0x',
+    value ? toHex(value) : '0x',
+    data ?? '0x',
     serializedAccessList,
-  ];
+  ]
 
   if (signature)
     serializedTransaction.push(
-      signature.v === 27n ? "0x" : toHex(1), // yParity
+      signature.v === 27n ? '0x' : toHex(1), // yParity
       signature.r,
-      signature.s
-    );
+      signature.s,
+    )
 
   return concatHex([
-    "0x02",
+    '0x02',
     toRlp(serializedTransaction),
-  ]) as TransactionSerializedEIP1559;
+  ]) as TransactionSerializedEIP1559
 }
 
 function serializeTransactionEIP2930(
   transaction: TransactionSerializableEIP2930,
-  signature?: Signature
+  signature?: Signature,
 ): TransactionSerializedEIP2930 {
   const { chainId, gas, data, nonce, to, value, accessList, gasPrice } =
-    transaction;
+    transaction
 
-  assertTransactionEIP2930(transaction);
+  assertTransactionEIP2930(transaction)
 
-  const serializedAccessList = serializeAccessList(accessList);
+  const serializedAccessList = serializeAccessList(accessList)
 
   const serializedTransaction = [
     toHex(chainId),
-    nonce ? toHex(nonce) : "0x",
-    gasPrice ? toHex(gasPrice) : "0x",
-    gas ? toHex(gas) : "0x",
-    to ?? "0x",
-    value ? toHex(value) : "0x",
-    data ?? "0x",
+    nonce ? toHex(nonce) : '0x',
+    gasPrice ? toHex(gasPrice) : '0x',
+    gas ? toHex(gas) : '0x',
+    to ?? '0x',
+    value ? toHex(value) : '0x',
+    data ?? '0x',
     serializedAccessList,
-  ];
+  ]
 
   if (signature)
     serializedTransaction.push(
-      signature.v === 27n ? "0x" : toHex(1), // yParity
+      signature.v === 27n ? '0x' : toHex(1), // yParity
       signature.r,
-      signature.s
-    );
+      signature.s,
+    )
 
   return concatHex([
-    "0x01",
+    '0x01',
     toRlp(serializedTransaction),
-  ]) as TransactionSerializedEIP2930;
+  ]) as TransactionSerializedEIP2930
 }
 
 function serializeTransactionLegacy(
   transaction: TransactionSerializableLegacy,
-  signature?: Signature
+  signature?: Signature,
 ): TransactionSerializedLegacy {
-  const { chainId = 0, gas, data, nonce, to, value, gasPrice } = transaction;
+  const { chainId = 0, gas, data, nonce, to, value, gasPrice } = transaction
 
-  assertTransactionLegacy(transaction);
+  assertTransactionLegacy(transaction)
 
   let serializedTransaction = [
-    nonce ? toHex(nonce) : "0x",
-    gasPrice ? toHex(gasPrice) : "0x",
-    gas ? toHex(gas) : "0x",
-    to ?? "0x",
-    value ? toHex(value) : "0x",
-    data ?? "0x",
-  ];
+    nonce ? toHex(nonce) : '0x',
+    gasPrice ? toHex(gasPrice) : '0x',
+    gas ? toHex(gas) : '0x',
+    to ?? '0x',
+    value ? toHex(value) : '0x',
+    data ?? '0x',
+  ]
 
   if (signature) {
-    let v = 27n + (signature.v === 27n ? 0n : 1n);
-    if (chainId > 0) v = BigInt(chainId * 2) + BigInt(35n + signature.v - 27n);
+    let v = 27n + (signature.v === 27n ? 0n : 1n)
+    if (chainId > 0) v = BigInt(chainId * 2) + BigInt(35n + signature.v - 27n)
     else if (signature.v !== v)
-      throw new InvalidLegacyVError({ v: signature.v });
+      throw new InvalidLegacyVError({ v: signature.v })
 
     serializedTransaction = [
       ...serializedTransaction,
       toHex(v),
       signature.r,
       signature.s,
-    ];
+    ]
   } else if (chainId > 0) {
     serializedTransaction = [
       ...serializedTransaction,
       toHex(chainId),
-      "0x",
-      "0x",
-    ];
+      '0x',
+      '0x',
+    ]
   }
 
-  return toRlp(serializedTransaction);
+  return toRlp(serializedTransaction)
 }
 
 function serializeAccessList(accessList?: AccessList): RecursiveArray<Hex> {
-  if (!accessList || accessList.length === 0) return [];
+  if (!accessList || accessList.length === 0) return []
 
-  const serializedAccessList: RecursiveArray<Hex> = [];
+  const serializedAccessList: RecursiveArray<Hex> = []
   for (let i = 0; i < accessList.length; i++) {
-    const { address, storageKeys } = accessList[i];
+    const { address, storageKeys } = accessList[i]
 
     for (let j = 0; j < storageKeys.length; j++) {
       if (storageKeys[j].length - 2 !== 64) {
-        throw new InvalidStorageKeySizeError({ storageKey: storageKeys[j] });
+        throw new InvalidStorageKeySizeError({ storageKey: storageKeys[j] })
       }
     }
 
     if (!isAddress(address)) {
-      throw new InvalidAddressError({ address });
+      throw new InvalidAddressError({ address })
     }
 
-    serializedAccessList.push([address, storageKeys]);
+    serializedAccessList.push([address, storageKeys])
   }
-  return serializedAccessList;
+  return serializedAccessList
 }
