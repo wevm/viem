ff0d8d9bc6ae9e7086de731f0f43102cece1c4b3
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-25 23:03:01 +0200

    chore: use anvil lib for test setup (#406)
    * chore: use anvil lib for test setup
    
    * chore: update
    
    * chore: better logs on error
    
    * chore: use proper package name

--- src/_test/setup.ts
+++ src/_test/setup.ts
@@ -4,6 +4,8 @@ import { cleanupCache, listenersCache } from '../utils/observe.js'
 import { promiseCache, responseCache } from '../utils/promise/withCache.js'
 import { setBlockNumber, testClient } from './utils.js'
 import { setAutomine, setIntervalMining } from '../test.js'
+import { fetchLogs } from '@viem/anvil'
+import { poolId } from './constants.js'
 
 beforeAll(() => {
   vi.mock('../errors/utils.ts', () => ({
@@ -37,32 +39,22 @@ afterEach((context) => {
   // Print the last log entries from anvil after each test.
   context.onTestFailed(async (result) => {
     try {
-      const pool = process.env.VITEST_POOL_ID ?? 1
-      const response = await fetch(`http://127.0.0.1:8545/${pool}/logs`)
-      const logs = (((await response.json()) ?? []) as string[])
-        .slice(1)
-        .slice(-20)
+      const response = await fetchLogs('http://127.0.0.1:8545', poolId)
+      const logs = response.slice(-20)
 
-      if (!Array.isArray(logs) || logs.length === 0) {
+      if (logs.length === 0) {
         return
       }
 
       // Try to append the log messages to the vitest error message if possible. Otherwise, print them to the console.
       const error = result.errors?.[0]
-      const label =
-        'Anvil log output\n=======================================\n'
 
       if (error !== undefined) {
-        error.message += `\n\n${label}`
+        error.message +=
+          '\n\nAnvil log output\n=======================================\n'
         error.message += `\n${logs.join('\n')}`
       } else {
-        console.group(label)
-
-        for (const log of logs) {
-          console.log(log)
-        }
-
-        console.groupEnd()
+        console.log(...logs)
       }
     } catch {}
   })
