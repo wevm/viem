75745a60fc83a13c3bec20ff04829b82d0852c07
moxey.eth <jakemoxey@gmail.com>
2023-06-22 15:06:18 +1000

    refactor: lax `parseUnits` parameter

--- src/utils/unit/parseGwei.ts
+++ src/utils/unit/parseGwei.ts
@@ -2,6 +2,6 @@ import { gweiUnits } from '../../constants/unit.js'
 
 import { parseUnits } from './parseUnits.js'
 
-export function parseGwei(ether: `${number}`, unit: 'wei' = 'wei') {
+export function parseGwei(ether: string, unit: 'wei' = 'wei') {
   return parseUnits(ether, gweiUnits[unit])
 }
