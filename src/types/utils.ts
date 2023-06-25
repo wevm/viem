ce01b3167938942a7e096f97f4b571ab5be7b2d9
Yurii Kabannik <yura_kabannik@icloud.com>
2023-06-25 16:14:19 +0300

    docs: Correct example and link for Prettify type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -176,9 +176,9 @@ export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 /**
  * @description Combines members of an intersection into a readable type.
  *
- * @link https://twitter.com/mattpocockuk/status/1622730173446557697?s=20&t=NdpAcmEFXY01xkqU3KO0Mg
+ * {@link https://twitter.com/mattpocockuk/status/1622730173446557697?s=20&t=NdpAcmEFXY01xkqU3KO0Mg}
  * @example
- * Prettify<{ a: string } | { b: string } | { c: number, d: bigint }>
+ * Prettify<{ a: string } & { b: string } & { c: number, d: bigint }>
  * => { a: string, b: string, c: number, d: bigint }
  */
 export type Prettify<T> = {
