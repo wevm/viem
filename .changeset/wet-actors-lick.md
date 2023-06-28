96cb7534c1be9ab635e70ad463551a29b7f2c553
moxey.eth <jakemoxey@gmail.com>
2023-06-28 12:52:39 +1000

    chore: changeset

---
+++ .changeset/wet-actors-lick.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Fixed multicall with ABI overrides.
--- src/actions/public/multicall.ts
+++ src/actions/public/multicall.ts
@@ -194,6 +194,7 @@ export async function multicall<
       if (!success) throw new RawContractError({ data: returnData })
       const result = decodeFunctionResult({
         abi,
+        args,
         data: returnData,
         functionName: functionName,
       })
