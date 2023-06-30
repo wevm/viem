d7b55684b85fe74f32959ba1a17d4fad85d65e70
RielJ <bulaybulay.rielj@gmail.com>
2023-06-30 23:08:55 +0800

    chore: lint changes

--- src/_test/abis.ts
+++ src/_test/abis.ts
@@ -4,11 +4,12 @@ export const payableContractConfig = {
   abi: [
     {
       inputs: [],
-      stateMutability: "payable",
-      type: "constructor",
+      stateMutability: 'payable',
+      type: 'constructor',
     },
   ],
-  bytecode: '0x608060405260358060116000396000f3006080604052600080fd00a165627a7a72305820f86ff341f0dff29df244305f8aa88abaf10e3a0719fa6ea1dcdd01b8b7d750970029'
+  bytecode:
+    '0x608060405260358060116000396000f3006080604052600080fd00a165627a7a72305820f86ff341f0dff29df244305f8aa88abaf10e3a0719fa6ea1dcdd01b8b7d750970029',
 } as const
 
 export const baycContractConfig = {
