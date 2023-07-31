036172f32a75a6576f1c934359723e3c484fa3e4
moxey.eth <jakemoxey@gmail.com>
2023-07-31 06:57:42 +0200

    feat: add `hexToSignature`

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -9,7 +9,10 @@ export type SignableMessage =
       raw: Hex | ByteArray
     }
 export type Signature = {
+  // TODO(v2): Make `bigint`
   r: Hex
+  // TODO(v2): Make `bigint`
   s: Hex
+  // TODO(v2): `v` to `recovery`
   v: bigint
 }
