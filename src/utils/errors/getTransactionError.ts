f7f04201984e01cde95426e4a0b5b7ea2c0f34fb
heka <siosio3103@gmail.com>
2023-08-21 17:10:42 +0900

    fix: In the case of Wallet Connect, add logic to handle when a user rejects a transaction

--- src/utils/errors/getTransactionError.ts
+++ src/utils/errors/getTransactionError.ts
@@ -2,6 +2,7 @@ import type { Account } from '../../accounts/types.js'
 import type { SendTransactionParameters } from '../../actions/wallet/sendTransaction.js'
 import type { BaseError } from '../../errors/base.js'
 import { TransactionExecutionError } from '../../errors/transaction.js'
+import { UserRejectedRequestError, type ProviderRpcError } from '../../index.js'
 import type { Chain } from '../../types/chain.js'
 
 import {
@@ -24,6 +25,20 @@ export function getTransactionError(
   { docsPath, ...args }: GetTransactionErrorParameters,
 ) {
   let cause = err
+
+  /**
+   * 
+    When the user rejected the transaction after connecting the wallet through walletconnect,
+    the error code of walletconnect is 5000, not 4001, so a unification process is needed.
+    https://docs.walletconnect.com/2.0/specs/clients/sign/error-codes#rejected-caip-25
+
+    See the code in the link below.
+    https://github.com/wagmi-dev/references/blob/849d7a5d2c630c1b55f3414cea034303e84250f9/packages/connectors/src/walletConnect.ts#L159C32-L159C32
+   */
+  if (/user rejected/i.test((err as ProviderRpcError)?.message)) {
+    return new UserRejectedRequestError(err as Error)
+  }
+
   if (containsNodeError(err))
     cause = getNodeError(err, args as GetNodeErrorParameters)
   return new TransactionExecutionError(cause, {
