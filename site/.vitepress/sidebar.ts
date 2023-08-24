4945d4ecccac0e8b2925690b50119e183fa226ed
moxey.eth <jakemoxey@gmail.com>
2023-08-25 09:19:03 +1000

    docs

--- site/.vitepress/sidebar.ts
+++ site/.vitepress/sidebar.ts
@@ -173,6 +173,10 @@ export const sidebar: DefaultTheme.Sidebar = {
           items: [
             { text: 'call', link: '/docs/actions/public/call' },
             {
+              text: 'prepareTransactionRequest',
+              link: '/docs/actions/public/prepareTransactionRequest',
+            },
+            {
               text: 'getTransaction',
               link: '/docs/actions/public/getTransaction',
             },
@@ -183,6 +187,10 @@ export const sidebar: DefaultTheme.Sidebar = {
             {
               text: 'getTransactionReceipt',
               link: '/docs/actions/public/getTransactionReceipt',
+            },
+            {
+              text: 'sendRawTransaction',
+              link: '/docs/actions/public/sendRawTransaction',
             },
             {
               text: 'waitForTransactionReceipt',
@@ -266,11 +274,19 @@ export const sidebar: DefaultTheme.Sidebar = {
           text: 'Transaction',
           items: [
             {
+              text: 'prepareTransactionRequest',
+              link: '/docs/actions/wallet/prepareTransactionRequest',
+            },
+            {
+              text: 'sendRawTransaction',
+              link: '/docs/actions/wallet/sendRawTransaction',
+            },
+            {
               text: 'sendTransaction',
               link: '/docs/actions/wallet/sendTransaction',
             },
             {
-              text: 'signTransaction 🚧',
+              text: 'signTransaction',
               link: '/docs/actions/wallet/signTransaction',
             },
           ],
