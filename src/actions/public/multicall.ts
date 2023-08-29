b4f9992def50d5d126123471973ad9997837f4e3
Artem Kuzmenko <artemk@defiyield.app>
2023-08-29 17:06:34 +0300

    fix(multicall): results decoding perfomance

--- src/actions/public/multicall.ts
+++ src/actions/public/multicall.ts
@@ -184,8 +184,9 @@ export async function multicall<
     ),
   )
 
+  const calls = chunkedCalls.flat()
+
   return results.flat().map(({ returnData, success }, i) => {
-    const calls = chunkedCalls.flat()
     const { callData } = calls[i]
     const { abi, address, functionName, args } = contracts[
       i
