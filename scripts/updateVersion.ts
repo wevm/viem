050e9c33f8ad18f192d2c7a1359010842932303b
Tom Meagher <tom@meagher.co>
2023-08-29 17:15:58 -0400

    chore: restore version

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,8 +1,8 @@
 import { readJsonSync, writeFileSync } from 'fs-extra'
 import path from 'path'
 
-// Writes the current package.json version to `./src/version.ts`.
-const versionFilePath = path.join(__dirname, '../src/version.ts')
+// Writes the current package.json version to `./src/errors/version.ts`.
+const versionFilePath = path.join(__dirname, '../src/errors/version.ts')
 const packageJsonPath = path.join(__dirname, '../package.json')
 const packageVersion = readJsonSync(packageJsonPath).version
 
