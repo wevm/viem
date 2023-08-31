9d201acce4d610312412252a14c6eadcba8b72b5
Simon.Z <zhanghan.simon@gmail.com>
2023-08-31 16:32:59 -0700

    add support for meter/meterTestnet

---
+++ src/chains/definitions/meter.ts
@@ -0,0 +1,22 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const meter = /*#__PURE__*/ defineChain({
+  id: 82,
+  name: 'Meter',
+  network: 'meter',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MTR',
+    symbol: 'MTR',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.meter.io'] },
+    public: { http: ['https://rpc.meter.io'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'MeterScan', url: 'https://scan.meter.io' },
+    default: { name: 'MeterScan', url: 'https://scan.meter.io' },
+  },
+  contracts: {
+  },
+})
