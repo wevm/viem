a81c71df8d1c0cb97ef71d24ab6d4d84d0012414
Ravil Ayupov <35985944+RexCloud@users.noreply.github.com>
2023-09-01 09:30:12 +0000

    feat: add multicall3 for Scroll Sepolia

--- src/chains/definitions/scrollSepolia.ts
+++ src/chains/definitions/scrollSepolia.ts
@@ -21,5 +21,11 @@ export const scrollSepolia = /*#__PURE__*/ defineChain({
       url: 'https://sepolia-blockscout.scroll.io',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 9473,
+    },
+  },
   testnet: true,
 })
