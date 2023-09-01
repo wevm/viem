af2858841a75f0985e90a83b55ad5d00e3fdb4f0
moxey.eth <jakemoxey@gmail.com>
2023-09-01 10:35:10 +1000

    refactor: remove hardcoded priority fee

--- src/chains/definitions/zora.ts
+++ src/chains/definitions/zora.ts
@@ -1,5 +1,4 @@
 import { defineChain } from '../../utils/chain.js'
-import { feesOptimism } from '../optimism/fees.js'
 import { formattersOptimism } from '../optimism/formatters.js'
 
 export const zora = /*#__PURE__*/ defineChain(
@@ -33,7 +32,6 @@ export const zora = /*#__PURE__*/ defineChain(
     },
   },
   {
-    fees: feesOptimism,
     formatters: formattersOptimism,
   },
 )
--- src/chains/optimism/fees.ts
+++
@@ -1,5 +0,0 @@
-import type { ChainFees } from '../../types/chain.js'
-
-export const feesOptimism = {
-  defaultPriorityFee: 1_000_000n, // 0.001 gwei
-} as const satisfies ChainFees
