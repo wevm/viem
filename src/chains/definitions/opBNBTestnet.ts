29f4492ea4affeec8af363cbd73953b1bc883450
Goraniya Karan <karangoraniya@gmail.com>
2023-09-02 18:26:11 +0530

    feat: add opBNB & opBNB testnet chain config 🚀

---
+++ src/chains/definitions/opBNBTestnet.ts
@@ -0,0 +1,30 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const opBNBTestnet = /*#__PURE__*/ defineChain({
+  id: 5611,
+  name: 'opBNB Testnet',
+  network: 'opBNB Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'tBNB',
+    symbol: 'tBNB',
+  },
+  rpcUrls: {
+    public: { http: ['https://opbnb-testnet-rpc.bnbchain.org'] },
+    default: { http: ['https://opbnb-testnet-rpc.bnbchain.org'] },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'opbnbscan',
+      url: 'https://opbnb-testnet.bscscan.com',
+    },
+    default: { name: 'opbnbscan', url: 'https://opbnbscan.com' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 3705108,
+    },
+  },
+  testnet: true,
+})
