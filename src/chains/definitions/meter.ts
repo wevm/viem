2327aa0741bac51058d7038840bdc2895ead5456
Simon Zhang <simonzg@gmail.com>
2023-09-03 14:15:09 -0700

    feat: add support for meter/meterTestnet chains (#1108)
    * add support for meter/meterTestnet
    
    * suggested changes on PR #1108
    
    * Update src/chains/definitions/meter.ts
    
    Co-authored-by: awkweb <tom@meagher.co>
    
    * Update src/chains/definitions/meterTestnet.ts
    
    Co-authored-by: awkweb <tom@meagher.co>
    
    * Update src/chains/definitions/meterTestnet.ts
    
    Co-authored-by: awkweb <tom@meagher.co>
    
    * Update src/chains/definitions/meter.ts
    
    Co-authored-by: awkweb <tom@meagher.co>
    
    * Update index.ts
    
    ---------
    
    Co-authored-by: awkweb <tom@meagher.co>
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ src/chains/definitions/meter.ts
@@ -0,0 +1,19 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const meter = /*#__PURE__*/ defineChain({
+  id: 82,
+  name: 'Meter',
+  network: 'meter',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MTR',
+    symbol: 'MTR',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.meter.io'] },
+    public: { http: ['https://rpc.meter.io'] },
+  },
+  blockExplorers: {
+    default: { name: 'MeterScan', url: 'https://scan.meter.io' },
+  },
+})
