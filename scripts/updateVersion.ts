8d4990fb3862ddcf2178f7fcc39998cdbba12a49
moxey.eth <jakemoxey@gmail.com>
2023-09-09 18:18:26 +1000

    chore: remap

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { readJsonSync, writeFileSync } from 'fs-extra'
 
-// Writes the current package.json version to `./src/errors/version.ts`.
+// Writes the current package.json version to `./package/errors/version.ts`.
 const versionFilePath = path.join(__dirname, '../package/errors/version.ts')
 const packageJsonPath = path.join(__dirname, '../package/package.json')
 const packageVersion = readJsonSync(packageJsonPath).version
