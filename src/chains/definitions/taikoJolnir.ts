5f0485869edd55605544c56c07258c8a4dcb2800
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-09-10 17:19:19 -0400

    feat: taiko L2 chain (#1182)
    * Taiko L2 chainId 167007
    
    * Create beige-donkeys-glow.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ src/chains/definitions/taikoJolnir.ts
@@ -0,0 +1,22 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const taikoJolnir = /*#__PURE__*/ defineChain({
+  id: 167007,
+  name: 'Taiko Jolnir L2',
+  network: 'tko-jolnir',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://jolnir.taiko.xyz'],
+    },
+    public: {
+      http: ['https://jolnir.taiko.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'blockscout',
+      url: 'https://explorer.jolnir.taiko.xyz',
+    },
+  },
+})
