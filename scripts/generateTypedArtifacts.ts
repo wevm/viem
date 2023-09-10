b930ab35995873207abf974107023ba713cbaf1b
moxey.eth <jakemoxey@gmail.com>
2023-09-10 18:02:22 +1000

    fix

--- scripts/generateTypedArtifacts.ts
+++ scripts/generateTypedArtifacts.ts
@@ -1,21 +1,27 @@
+import path from 'node:path'
 import { globby } from 'globby'
 
-const generatedPath = `${import.meta.dir}/generated.ts`
+const generatedPath = path.join(
+  import.meta.dir,
+  '../test/contracts/generated.ts',
+)
 Bun.write(generatedPath, '')
 
-globby([`${import.meta.dir}/out/**/*.json`]).then((paths) => {
-  const generated = Bun.file(generatedPath)
-  const writer = generated.writer()
+globby([path.join(import.meta.dir, '../test/contracts/out/**/*.json')]).then(
+  (paths) => {
+    const generated = Bun.file(generatedPath)
+    const writer = generated.writer()
 
-  paths.forEach(async (path) => {
-    const fileName = path.split('/').pop()?.replace('.json', '')
-    const json = await Bun.file(path, { type: 'application/json' }).json()
-    writer.write(
-      `export const ${fileName} = ${JSON.stringify(
-        json,
-        null,
-        2,
-      )} as const;\n\n`,
-    )
-  })
-})
+    paths.forEach(async (path) => {
+      const fileName = path.split('/').pop()?.replace('.json', '')
+      const json = await Bun.file(path, { type: 'application/json' }).json()
+      writer.write(
+        `export const ${fileName} = ${JSON.stringify(
+          json,
+          null,
+          2,
+        )} as const;\n\n`,
+      )
+    })
+  },
+)
