a83abe780f7707d0e7167b54dca4de311ed50ccc
moxey.eth <jakemoxey@gmail.com>
2023-09-14 07:32:55 +1000

    ci: fix script

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -8,16 +8,18 @@ const packageVersion = (await Bun.file(packageJsonPath).json()).version
 
 Bun.write(versionFilePath, `export const version = '${packageVersion}'\n`)
 
-const tmpChangelogPath = join(import.meta.dir, '../src/CHANGELOG.md')
-if (existsSync(tmpChangelogPath)) {
-  const changelogPath = join(import.meta.dir, '../CHANGELOG.md')
+try {
+  const tmpChangelogPath = join(import.meta.dir, '../src/CHANGELOG.md')
+  if (existsSync(tmpChangelogPath)) {
+    const changelogPath = join(import.meta.dir, '../CHANGELOG.md')
 
-  const tmpChangelog = await Bun.file(tmpChangelogPath).text()
-  const changelog = await Bun.file(changelogPath).text()
+    const tmpChangelog = await Bun.file(tmpChangelogPath).text()
+    const changelog = await Bun.file(changelogPath).text()
 
-  Bun.write(
-    changelogPath,
-    `${tmpChangelog}\n${changelog.replace('# viem\n\n', '')}`,
-  )
-  unlinkSync(tmpChangelogPath)
-}
+    Bun.write(
+      changelogPath,
+      `${tmpChangelog}\n${changelog.replace('# viem\n\n', '')}`,
+    )
+    unlinkSync(tmpChangelogPath)
+  }
+} catch {}
