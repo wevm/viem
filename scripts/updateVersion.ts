0142bdfc7751ed0de07a676f4728b6ba736b93b0
moxey.eth <jakemoxey@gmail.com>
2023-09-14 07:01:57 +1000

    ci: fix canary action

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,4 +1,4 @@
-import { unlinkSync } from 'node:fs'
+import { existsSync, unlinkSync } from 'node:fs'
 import { join } from 'node:path'
 
 // Writes the current package.json version to `./src/errors/version.ts`.
@@ -9,13 +9,15 @@ const packageVersion = (await Bun.file(packageJsonPath).json()).version
 Bun.write(versionFilePath, `export const version = '${packageVersion}'\n`)
 
 const tmpChangelogPath = join(import.meta.dir, '../src/CHANGELOG.md')
-const changelogPath = join(import.meta.dir, '../CHANGELOG.md')
+if (existsSync(tmpChangelogPath)) {
+  const changelogPath = join(import.meta.dir, '../CHANGELOG.md')
 
-const tmpChangelog = await Bun.file(tmpChangelogPath).text()
-const changelog = await Bun.file(changelogPath).text()
+  const tmpChangelog = await Bun.file(tmpChangelogPath).text()
+  const changelog = await Bun.file(changelogPath).text()
 
-Bun.write(
-  changelogPath,
-  `${tmpChangelog}\n${changelog.replace('# viem\n\n', '')}`,
-)
-unlinkSync(tmpChangelogPath)
+  Bun.write(
+    changelogPath,
+    `${tmpChangelog}\n${changelog.replace('# viem\n\n', '')}`,
+  )
+  unlinkSync(tmpChangelogPath)
+}
