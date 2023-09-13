7d85fe8615d10b5265b400f46da8821ecf314cde
moxey.eth <jakemoxey@gmail.com>
2023-09-13 17:06:00 +1000

    chore: changelog gen

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,3 +1,4 @@
+import { unlinkSync } from 'node:fs'
 import { join } from 'node:path'
 
 // Writes the current package.json version to `./src/errors/version.ts`.
@@ -6,3 +7,15 @@ const packageJsonPath = join(import.meta.dir, '../src/package.json')
 const packageVersion = (await Bun.file(packageJsonPath).json()).version
 
 Bun.write(versionFilePath, `export const version = '${packageVersion}'\n`)
+
+const tmpChangelogPath = join(import.meta.dir, '../src/CHANGELOG.md')
+const changelogPath = join(import.meta.dir, '../CHANGELOG.md')
+
+const tmpChangelog = await Bun.file(tmpChangelogPath).text()
+const changelog = await Bun.file(changelogPath).text()
+
+Bun.write(
+  changelogPath,
+  `${tmpChangelog}\n${changelog.replace('# viem\n\n', '')}`,
+)
+unlinkSync(tmpChangelogPath)
