7be411652a0f037a7f41919acfc06e2a603954dc
Jim McDonald <Jim@mcdee.net>
2023-09-14 10:20:35 +0100

    Added Holesky chain.

---
+++ src/chains/definitions/holesky.ts
@@ -0,0 +1,19 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const holesky = /*#__PURE__*/ defineChain({
+  id: 17000,
+  network: 'holesky',
+  name: 'Holesky',
+  nativeCurrency: { name: 'Holesky Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.holesky.ethpandaops.io'],
+    },
+    public: {
+      http: ['https://rpc.holesky.ethpandaops.io'],
+    },
+  },
+  blockExplorers: {},
+  contracts: {},
+  testnet: true,
+})
