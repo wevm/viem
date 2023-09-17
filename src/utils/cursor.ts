676dadf6dde828937353dd91cf5d73fd0e188882
moxey.eth <jakemoxey@gmail.com>
2023-09-18 08:45:57 +1000

    chore: perf boost
    Co-authored-by: Sebastian Lorenz <fubhy@fubhy.com>

--- src/utils/cursor.ts
+++ src/utils/cursor.ts
@@ -32,127 +32,136 @@ export type Cursor = {
   setPosition(position: number): void
 }
 
+const staticCursor: Cursor = {
+  bytes: new Uint8Array(),
+  dataView: new DataView(new ArrayBuffer(0)),
+  position: 0,
+  assertPosition(position) {
+    if (position < 0 || position > this.bytes.length - 1)
+      throw new PositionOutOfBoundsError({
+        length: this.bytes.length,
+        position,
+      })
+  },
+  decrementPosition(offset) {
+    if (offset < 0) throw new NegativeOffsetError({ offset })
+    const position = this.position - offset
+    this.assertPosition(position)
+    this.position = position
+  },
+  incrementPosition(offset) {
+    if (offset < 0) throw new NegativeOffsetError({ offset })
+    const position = this.position + offset
+    this.assertPosition(position)
+    this.position = position
+  },
+  inspectByte(position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position)
+    return this.bytes[position]
+  },
+  inspectBytes(length, position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position + length - 1)
+    return this.bytes.subarray(position, position + length)
+  },
+  inspectUint8(position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position)
+    return this.bytes[position]
+  },
+  inspectUint16(position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position + 1)
+    return this.dataView.getUint16(position)
+  },
+  inspectUint24(position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position + 2)
+    return (
+      (this.dataView.getUint16(position) << 8) +
+      this.dataView.getUint8(position + 2)
+    )
+  },
+  inspectUint32(position_) {
+    const position = position_ ?? this.position
+    this.assertPosition(position + 3)
+    return this.dataView.getUint32(position)
+  },
+  pushByte(byte: ByteArray[number]) {
+    this.assertPosition(this.position)
+    this.bytes[this.position] = byte
+    this.position++
+  },
+  pushBytes(bytes: ByteArray) {
+    this.assertPosition(this.position + bytes.length - 1)
+    this.bytes.set(bytes, this.position)
+    this.position += bytes.length
+  },
+  pushUint8(value: number) {
+    this.assertPosition(this.position)
+    this.bytes[this.position] = value
+    this.position++
+  },
+  pushUint16(value: number) {
+    this.assertPosition(this.position + 1)
+    this.dataView.setUint16(this.position, value)
+    this.position += 2
+  },
+  pushUint24(value: number) {
+    this.assertPosition(this.position + 2)
+    this.dataView.setUint16(this.position, value >> 8)
+    this.dataView.setUint8(this.position + 2, value & ~4294967040)
+    this.position += 3
+  },
+  pushUint32(value: number) {
+    this.assertPosition(this.position + 3)
+    this.dataView.setUint32(this.position, value)
+    this.position += 4
+  },
+  readByte() {
+    const value = this.inspectByte()
+    this.position++
+    return value
+  },
+  readBytes(length) {
+    const value = this.inspectBytes(length)
+    this.position += length
+    return value
+  },
+  readUint8() {
+    const value = this.inspectUint8()
+    this.position += 1
+    return value
+  },
+  readUint16() {
+    const value = this.inspectUint16()
+    this.position += 2
+    return value
+  },
+  readUint24() {
+    const value = this.inspectUint24()
+    this.position += 3
+    return value
+  },
+  readUint32() {
+    const value = this.inspectUint32()
+    this.position += 4
+    return value
+  },
+  setPosition(position) {
+    this.assertPosition(position)
+    this.position = position
+  },
+}
+
 export function createCursor(bytes: ByteArray): Cursor {
-  return {
-    bytes,
-    dataView: new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength),
-    position: 0,
-    assertPosition(position) {
-      if (position < 0 || position > this.bytes.length - 1)
-        throw new PositionOutOfBoundsError({
-          length: this.bytes.length,
-          position,
-        })
-    },
-    decrementPosition(offset) {
-      if (offset < 0) throw new NegativeOffsetError({ offset })
-      const position = this.position - offset
-      this.assertPosition(position)
-      this.position = position
-    },
-    incrementPosition(offset) {
-      if (offset < 0) throw new NegativeOffsetError({ offset })
-      const position = this.position + offset
-      this.assertPosition(position)
-      this.position = position
-    },
-    inspectByte(position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position)
-      return this.bytes[position]
-    },
-    inspectBytes(length, position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position + length - 1)
-      return this.bytes.subarray(position, position + length)
-    },
-    inspectUint8(position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position)
-      return this.bytes[position]
-    },
-    inspectUint16(position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position + 1)
-      return this.dataView.getUint16(position)
-    },
-    inspectUint24(position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position + 2)
-      return (
-        (this.dataView.getUint16(position) << 8) +
-        this.dataView.getUint8(position + 2)
-      )
-    },
-    inspectUint32(position_) {
-      const position = position_ ?? this.position
-      this.assertPosition(position + 3)
-      return this.dataView.getUint32(position)
-    },
-    pushByte(byte: ByteArray[number]) {
-      this.assertPosition(this.position)
-      this.bytes[this.position] = byte
-      this.position++
-    },
-    pushBytes(bytes: ByteArray) {
-      this.assertPosition(this.position + bytes.length - 1)
-      this.bytes.set(bytes, this.position)
-      this.position += bytes.length
-    },
-    pushUint8(value: number) {
-      this.assertPosition(this.position)
-      this.bytes[this.position] = value
-      this.position++
-    },
-    pushUint16(value: number) {
-      this.assertPosition(this.position + 1)
-      this.dataView.setUint16(this.position, value)
-      this.position += 2
-    },
-    pushUint24(value: number) {
-      this.assertPosition(this.position + 2)
-      this.dataView.setUint16(this.position, value >> 8)
-      this.dataView.setUint8(this.position + 2, value & ~4294967040)
-      this.position += 3
-    },
-    pushUint32(value: number) {
-      this.assertPosition(this.position + 3)
-      this.dataView.setUint32(this.position, value)
-      this.position += 4
-    },
-    readByte() {
-      const value = this.inspectByte()
-      this.position++
-      return value
-    },
-    readBytes(length) {
-      const value = this.inspectBytes(length)
-      this.position += length
-      return value
-    },
-    readUint8() {
-      const value = this.inspectUint8()
-      this.position += 1
-      return value
-    },
-    readUint16() {
-      const value = this.inspectUint16()
-      this.position += 2
-      return value
-    },
-    readUint24() {
-      const value = this.inspectUint24()
-      this.position += 3
-      return value
-    },
-    readUint32() {
-      const value = this.inspectUint32()
-      this.position += 4
-      return value
-    },
-    setPosition(position) {
-      this.assertPosition(position)
-      this.position = position
-    },
-  }
+  const cursor = Object.create(staticCursor)
+  cursor.bytes = bytes
+  cursor.dataView = new DataView(
+    bytes.buffer,
+    bytes.byteOffset,
+    bytes.byteLength,
+  )
+  return cursor
 }
