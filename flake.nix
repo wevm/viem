cfb4b29c7aad03dc8ed544e2621a0a7e50b3d89f
Sebastian Lorenz <fubhy@fubhy.com>
2023-09-18 10:32:38 +0200

    chore: fork shazow/foundry.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,27 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
-
-    utils = {
-      url = "github:numtide/flake-utils";
-    };
-
-    foundry = {
-      url = "github:shazow/foundry.nix/monthly";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:fubhy/foundry.nix";
   };
 
-  outputs = { self, nixpkgs, utils, foundry }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [foundry.overlay];
-        };
-      in {
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            foundry-bin
+  outputs = { self, nixpkgs, utils, foundry, ... }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ foundry.overlay ];
+      };
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
             bun
+            foundry-bin
             nodejs_20
           ];
         };
-      });
+      };
+    });
 }
