44367dbe921cfd12a45d78744758474b67d28791
Ilya Kubariev <ilya.kubariev@redduck.io>
2023-09-22 04:51:23 +0300

    Fix sepolia multicall3 blockCreated variable (#1242)
    * fix: update multicall3 blockCreated to real one
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: Tom Meagher <tom@meagher.co>

--- src/chains/definitions/sepolia.ts
+++ src/chains/definitions/sepolia.ts
@@ -34,7 +34,7 @@ export const sepolia = /*#__PURE__*/ defineChain({
   contracts: {
     multicall3: {
       address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 6507670,
+      blockCreated: 751532,
     },
     ensRegistry: { address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e' },
     ensUniversalResolver: {
