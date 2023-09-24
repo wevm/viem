ce1c709ac68612641110f521915370aba316b031
Paul Miller <paul@paulmillr.com>
2023-09-24 13:55:35 +0000

    Speed-up hexToBytes 6x.

--- src/utils/encoding/toBytes.ts
+++ src/utils/encoding/toBytes.ts
@@ -88,6 +88,25 @@ export type HexToBytesOpts = {
   size?: number
 }
 
+// We use very optimized technique to convert hex string to byte array
+enum HexC {
+  ZERO = 48, // 0
+  NINE = 57, // 9
+  A_UP = 65, // A
+  F_UP = 70, // F
+  A_LO = 97, // a
+  F_LO = 102, // f
+}
+
+function charCodeToBase16(char: number) {
+  if (char >= HexC.ZERO && char <= HexC.NINE) return char - HexC.ZERO
+  else if (char >= HexC.A_UP && char <= HexC.F_UP)
+    return char - (HexC.A_UP - 10)
+  else if (char >= HexC.A_LO && char <= HexC.F_LO)
+    return char - (HexC.A_LO - 10)
+  return undefined
+}
+
 /**
  * Encodes a hex string into a byte array.
  *
@@ -117,16 +136,19 @@ export function hexToBytes(hex_: Hex, opts: HexToBytesOpts = {}): ByteArray {
   let hexString = hex.slice(2) as string
   if (hexString.length % 2) hexString = `0${hexString}`
 
-  const bytes = new Uint8Array(hexString.length / 2)
-  for (let index = 0; index < bytes.length; index++) {
-    const start = index * 2
-    const hexByte = hexString.slice(start, start + 2)
-    const byte = Number.parseInt(hexByte, 16)
-    if (Number.isNaN(byte) || byte < 0)
+  const al = hexString.length / 2
+  const bytes = new Uint8Array(al)
+  for (let index = 0, j = 0; index < al; index++) {
+    const n1 = charCodeToBase16(hexString.charCodeAt(j++))
+    const n2 = charCodeToBase16(hexString.charCodeAt(j++))
+    if (n1 === undefined || n2 === undefined) {
       throw new BaseError(
-        `Invalid byte sequence ("${hexByte}" in "${hexString}").`,
+        `Invalid byte sequence ("${hexString[j - 2]}${
+          hexString[j - 1]
+        }" in "${hexString}).`,
       )
-    bytes[index] = byte
+    }
+    bytes[index] = n1 * 16 + n2
   }
   return bytes
 }
