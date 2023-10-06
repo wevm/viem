fca1d7feefd218eee9e18e7f6d67f24491f586dc
Daniel Helm <dan.helm@gmail.com>
2023-10-05 21:32:13 -0500

    feat: add Scroll chain

---
+++ src/chains/definitions/scroll.ts
@@ -0,0 +1,24 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const scroll = /*#__PURE__*/ defineChain({
+  id: 534_352,
+  name: 'Scroll',
+  network: 'scroll',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.scroll.io'],
+      webSocket: ['wss://wss-rpc.scroll.io/ws'],
+    },
+    public: {
+      http: ['https://rpc.scroll.io'],
+      webSocket: ['wss://wss-rpc.scroll.io/ws'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Scrollscan',
+      url: 'https://scrollscan.com',
+    },
+  },
+})
