210e2b943d6d04ab130151dfdb5a94e590e42fd4
Saša Pul <4625220+codespool@users.noreply.github.com>
2023-10-26 23:12:14 +0300

    feat: add multicall3 contract to astarZkatana (#1399)
    * add multicall3 contract to astarZkatana
    
    * Create neat-suits-sing.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

--- src/chains/definitions/astarZkatana.ts
+++ src/chains/definitions/astarZkatana.ts
@@ -29,5 +29,11 @@ export const astarZkatana = /*#__PURE__*/ defineChain({
       url: 'https://zkatana.explorer.startale.com',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 31317,
+    },
+  },
   testnet: true,
 })
