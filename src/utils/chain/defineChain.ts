d21507462dd2ec987ffe116f042b381f03417db9
Tom Meagher <tom@meagher.co>
2023-10-30 16:03:43 -0400

    refactor: defineChain type

--- src/utils/chain/defineChain.ts
+++ src/utils/chain/defineChain.ts
@@ -1,8 +1,14 @@
+import type { Assign, Prettify } from '~viem/types/utils.js'
 import type { Chain, ChainFormatters } from '../../types/chain.js'
 
 export function defineChain<
   formatters extends ChainFormatters,
   const chain extends Chain<formatters>,
->(chain: chain): chain {
-  return chain as any
+>(chain: chain): Prettify<Assign<Chain<undefined>, chain>> {
+  return {
+    formatters: undefined,
+    fees: undefined,
+    serializers: undefined,
+    ...chain,
+  } as Assign<Chain<undefined>, chain>
 }
