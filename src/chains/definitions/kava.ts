81fd325f63d18e6ccd905b8b01f9fada303c471b
0xRaghu <shriraghuraman@gmail.com>
2023-11-06 20:37:10 +0530

    Exported chain, Changeset and format

--- src/chains/definitions/kava.ts
+++ src/chains/definitions/kava.ts
@@ -1,26 +1,26 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const kava = /*#__PURE__*/ defineChain({
   id: 2222,
-  name: "Kava EVM",
-  network: "kava-mainnet",
+  name: 'Kava EVM',
+  network: 'kava-mainnet',
   nativeCurrency: {
-    name: "Kava",
-    symbol: "KAVA",
+    name: 'Kava',
+    symbol: 'KAVA',
     decimals: 18,
   },
   rpcUrls: {
-    public: { http: ["https://evm.kava.io"] },
-    default: { http: ["https://evm.kava.io"] },
+    public: { http: ['https://evm.kava.io'] },
+    default: { http: ['https://evm.kava.io'] },
   },
   blockExplorers: {
-    default: { name: "Kava EVM Explorer", url: "https://kavascan.com" },
+    default: { name: 'Kava EVM Explorer', url: 'https://kavascan.com' },
   },
   contracts: {
     multicall3: {
-      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 3661165,
     },
   },
   testnet: false,
-});
+})
--- src/chains/definitions/kavaTestnet.ts
+++ src/chains/definitions/kavaTestnet.ts
@@ -1,29 +1,29 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const kava = /*#__PURE__*/ defineChain({
+export const kavaTestnet = /*#__PURE__*/ defineChain({
   id: 2221,
-  name: "Kava EVM Testnet",
-  network: "kava-testnet",
+  name: 'Kava EVM Testnet',
+  network: 'kava-testnet',
   nativeCurrency: {
-    name: "Kava",
-    symbol: "KAVA",
+    name: 'Kava',
+    symbol: 'KAVA',
     decimals: 18,
   },
   rpcUrls: {
-    public: { http: ["https://evm.testnet.kava.io"] },
-    default: { http: ["https://evm.testnet.kava.io"] },
+    public: { http: ['https://evm.testnet.kava.io'] },
+    default: { http: ['https://evm.testnet.kava.io'] },
   },
   blockExplorers: {
     default: {
-      name: "Kava EVM Testnet Explorer",
-      url: "https://testnet.kavascan.com/",
+      name: 'Kava EVM Testnet Explorer',
+      url: 'https://testnet.kavascan.com/',
     },
   },
   contracts: {
     multicall3: {
-      address: "0xDf1D724A7166261eEB015418fe8c7679BBEa7fd6",
+      address: '0xDf1D724A7166261eEB015418fe8c7679BBEa7fd6',
       blockCreated: 7242179,
     },
   },
   testnet: true,
-});
+})
