91ce253294e7f381152287aa18dfc622074b24c9
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-08 12:23:46 +0000

    Update formatGwei.ts
    Add explanation for formatGwei

--- src/utils/unit/formatGwei.ts
+++ src/utils/unit/formatGwei.ts
@@ -4,6 +4,11 @@ import { type FormatUnitsErrorType, formatUnits } from './formatUnits.js'
 
 export type FormatGweiErrorType = FormatUnitsErrorType
 
+/**
+ * Converts numerical wei to a string representation of gwei.
+ *
+ * Example: formatGwei(1000000000n)  -> '1'
+ */
 export function formatGwei(wei: bigint, unit: 'wei' = 'wei') {
   return formatUnits(wei, gweiUnits[unit])
 }
