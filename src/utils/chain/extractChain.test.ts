20ca3ae94692534a0ca5074e1279c21a2be89dc3
moxey.eth <jakemoxey@gmail.com>
2023-11-09 10:04:40 +1100

    chore: update snaps

--- src/utils/chain/extractChain.test.ts
+++ src/utils/chain/extractChain.test.ts
@@ -62,6 +62,24 @@ test('default', async () => {
         },
       },
       "contracts": {
+        "gasPriceOracle": {
+          "address": "0x420000000000000000000000000000000000000F",
+        },
+        "l1Block": {
+          "address": "0x4200000000000000000000000000000000000015",
+        },
+        "l2CrossDomainMessenger": {
+          "address": "0x4200000000000000000000000000000000000007",
+        },
+        "l2Erc721Bridge": {
+          "address": "0x4200000000000000000000000000000000000014",
+        },
+        "l2StandardBridge": {
+          "address": "0x4200000000000000000000000000000000000010",
+        },
+        "l2ToL1MessagePasser": {
+          "address": "0x4200000000000000000000000000000000000016",
+        },
         "multicall3": {
           "address": "0xca11bde05977b3631167028862be2a173976ca11",
           "blockCreated": 4286263,
