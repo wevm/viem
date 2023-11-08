61c087fb0391a7c05630a999c23b49be5824af0f
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-08 12:22:39 +0000

    Update parseEther.ts
    Added explanation for parseEther

--- src/utils/unit/parseEther.ts
+++ src/utils/unit/parseEther.ts
@@ -5,6 +5,11 @@ import { type ParseUnitsErrorType, parseUnits } from './parseUnits.js'
 
 export type ParseEtherErrorType = ParseUnitsErrorType | ErrorType
 
+/**
+ * Converts a string representation of ether to numerical wei.
+ *
+ * Example: parseEther('420')  -> 420000000000000000000n
+ */
 export function parseEther(ether: string, unit: 'wei' | 'gwei' = 'wei') {
   return parseUnits(ether, etherUnits[unit])
 }
