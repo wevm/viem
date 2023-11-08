9f7c31dd4af590bbe4f43e24bd60ab48b0288e70
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-08 12:18:57 +0000

    Update formatUnits.ts
    Adds explanation for formatUnits function

--- src/utils/unit/formatUnits.ts
+++ src/utils/unit/formatUnits.ts
@@ -2,6 +2,11 @@ import type { ErrorType } from '../../errors/utils.js'
 
 export type FormatUnitsErrorType = ErrorType
 
+/**
+ * Takes a bigint and converts it to a string with the correct number of decimal places
+ *
+ * Example: formatUnits(420000000000n, 9) -> '420'
+ */
 export function formatUnits(value: bigint, decimals: number) {
   let display = value.toString()
 
