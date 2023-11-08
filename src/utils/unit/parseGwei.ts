d152b1c8f10bf1180041bcaca77695d87b424fc9
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-08 12:21:50 +0000

    Update parseGwei.ts
    Added explanation for parseGwei

--- src/utils/unit/parseGwei.ts
+++ src/utils/unit/parseGwei.ts
@@ -5,6 +5,11 @@ import { type ParseUnitsErrorType, parseUnits } from './parseUnits.js'
 
 export type ParseGweiErrorType = ParseUnitsErrorType | ErrorType
 
+/**
+ * Takes a string in gwei and returns a bigint in wei
+ *
+ * Example: parseGwei('420') -> 420000000000n
+ */
 export function parseGwei(ether: string, unit: 'wei' = 'wei') {
   return parseUnits(ether, gweiUnits[unit])
 }
