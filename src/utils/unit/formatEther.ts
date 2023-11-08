3e6c14d6a878799e011cae760c79e2d8a05cf6df
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-08 12:24:21 +0000

    Update formatEther.ts
    Added explanation for formatEther

--- src/utils/unit/formatEther.ts
+++ src/utils/unit/formatEther.ts
@@ -4,6 +4,11 @@ import { type FormatUnitsErrorType, formatUnits } from './formatUnits.js'
 
 export type FormatEtherErrorType = FormatUnitsErrorType
 
+/**
+ * Converts numerical wei to a string representation of ether.
+ *
+ * Example: formatEther(1000000000000000000n)  -> '1'
+ */
 export function formatEther(wei: bigint, unit: 'wei' | 'gwei' = 'wei') {
   return formatUnits(wei, etherUnits[unit])
 }
