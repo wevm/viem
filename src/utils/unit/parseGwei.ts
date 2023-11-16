583bc9ef26e71d71071bff1a628b4fd928f5c9b4
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-16 13:27:05 +0000

    Update parseGwei.ts

--- src/utils/unit/parseGwei.ts
+++ src/utils/unit/parseGwei.ts
@@ -7,8 +7,14 @@ export type ParseGweiErrorType = ParseUnitsErrorType | ErrorType
 
 /**
  * Converts a string representation of gwei to numerical wei.
+ * 
+ * - Docs: https://viem.sh/docs/utilities/parseGwei.html
  *
- * Example: parseGwei('420') -> 420000000000n
+ * @example
+ * import { parseGwei } from 'viem'
+ *
+ * parseGwei('420') 
+ * // 420000000000n
  */
 export function parseGwei(ether: string, unit: 'wei' = 'wei') {
   return parseUnits(ether, gweiUnits[unit])
