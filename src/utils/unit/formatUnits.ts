7b29bde412358ac2ab68293e2f673d77ed99b47a
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-16 13:24:43 +0000

    Update formatUnits.ts

--- src/utils/unit/formatUnits.ts
+++ src/utils/unit/formatUnits.ts
@@ -3,9 +3,15 @@ import type { ErrorType } from '../../errors/utils.js'
 export type FormatUnitsErrorType = ErrorType
 
 /**
- * Divides a number by a given exponent of base 10 (10exponent), and formats it into a string representation of the number.
+ *  Divides a number by a given exponent of base 10 (10exponent), and formats it into a string representation of the number..
+ * 
+ * - Docs: https://viem.sh/docs/utilities/formatUnits.html
  *
- * Example: formatUnits(420000000000n, 9) -> '420'
+ * @example
+ * import { formatUnits } from 'viem'
+ *
+ * formatUnits(420000000000n, 9) 
+ * // '420'
  */
 export function formatUnits(value: bigint, decimals: number) {
   let display = value.toString()
