00bdd2efac033a3b167cb4e911e761e2859f85eb
AM <9613565+thevolcanomanishere@users.noreply.github.com>
2023-11-16 13:22:20 +0000

    Update formatEther.ts

--- src/utils/unit/formatEther.ts
+++ src/utils/unit/formatEther.ts
@@ -6,8 +6,14 @@ export type FormatEtherErrorType = FormatUnitsErrorType
 
 /**
  * Converts numerical wei to a string representation of ether.
+ * 
+ * - Docs: https://viem.sh/docs/utilities/formatEther.html
  *
- * Example: formatEther(1000000000000000000n)  -> '1'
+ * @example
+ * import { formatEther } from 'viem'
+ *
+ * formatEther(1000000000000000000n) 
+ * // '1'
  */
 export function formatEther(wei: bigint, unit: 'wei' | 'gwei' = 'wei') {
   return formatUnits(wei, etherUnits[unit])
