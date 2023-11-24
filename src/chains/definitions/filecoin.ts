929d32696b592f7a28f6f9503c05306e1eb98786
abs3ntdev <abs3ntdev@users.noreply.github.com>
2023-11-23 23:23:57 -0800

    filecoin add multicall3

--- src/chains/definitions/filecoin.ts
+++ src/chains/definitions/filecoin.ts
@@ -1,22 +1,28 @@
-import { defineChain } from '../../utils/chain/defineChain.js'
+import { defineChain } from "../../utils/chain/defineChain.js";
 
 export const filecoin = /*#__PURE__*/ defineChain({
   id: 314,
-  name: 'Filecoin Mainnet',
-  network: 'filecoin-mainnet',
+  name: "Filecoin Mainnet",
+  network: "filecoin-mainnet",
   nativeCurrency: {
     decimals: 18,
-    name: 'filecoin',
-    symbol: 'FIL',
+    name: "filecoin",
+    symbol: "FIL",
   },
   rpcUrls: {
-    default: { http: ['https://api.node.glif.io/rpc/v1'] },
-    public: { http: ['https://api.node.glif.io/rpc/v1'] },
+    default: { http: ["https://api.node.glif.io/rpc/v1"] },
+    public: { http: ["https://api.node.glif.io/rpc/v1"] },
   },
   blockExplorers: {
-    default: { name: 'Filfox', url: 'https://filfox.info/en' },
-    filscan: { name: 'Filscan', url: 'https://filscan.io' },
-    filscout: { name: 'Filscout', url: 'https://filscout.io/en' },
-    glif: { name: 'Glif', url: 'https://explorer.glif.io' },
+    default: { name: "Filfox", url: "https://filfox.info/en" },
+    filscan: { name: "Filscan", url: "https://filscan.io" },
+    filscout: { name: "Filscout", url: "https://filscout.io/en" },
+    glif: { name: "Glif", url: "https://explorer.glif.io" },
   },
-})
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      blockCreated: 3328594,
+    },
+  },
+});
