8771e7bb7d83cb8b498e8dde20f719bf2e48cbac
jxom <jakemoxey@gmail.com>
2023-12-07 07:14:17 +1100

    Update ziliqaTestnet.ts

--- src/chains/definitions/ziliqaTestnet.ts
+++ src/chains/definitions/ziliqaTestnet.ts
@@ -1,10 +1,10 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const ziliqaTestnet = /*#__PURE__*/ defineChain({
+export const zilliqaTestnet = /*#__PURE__*/ defineChain({
   id: 33101,
-  name: 'Ziliqa Testnet',
-  network: 'ziliqa-testnet',
-  nativeCurrency: { name: 'Ziliqa', symbol: 'ZIL', decimals: 18 },
+  name: 'Zilliqa Testnet',
+  network: 'zilliqa-testnet',
+  nativeCurrency: { name: 'Zilliqa', symbol: 'ZIL', decimals: 18 },
   rpcUrls: {
     default: {
       http: ['https://dev-api.zilliqa.com'],
