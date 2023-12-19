e318cb375c22df6139c4e34bde35abf30dff9cc7
Kenk <kenghin_lim@hotmail.com>
2023-12-20 05:23:19 +0800

    feat: updated Taiko Jolnir Config (#1597)
    * update taiko jolnir rpc
    
    * fix taiko jolnir rpc
    
    * revert removal of taikoTestnetSepolia
    
    * Create shiny-carrots-double.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ .changeset/shiny-carrots-double.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Updated Taiko Jolnir RPC URL.
--- src/chains/definitions/taikoJolnir.ts
+++ src/chains/definitions/taikoJolnir.ts
@@ -2,15 +2,15 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const taikoJolnir = /*#__PURE__*/ defineChain({
   id: 167007,
-  name: 'Taiko Jolnir L2',
+  name: 'Taiko Jolnir (Alpha-5 Testnet)',
   network: 'tko-jolnir',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://jolnir.taiko.xyz'],
+      http: ['https://rpc.jolnir.taiko.xyz'],
     },
     public: {
-      http: ['https://jolnir.taiko.xyz'],
+      http: ['https://rpc.jolnir.taiko.xyz'],
     },
   },
   blockExplorers: {
