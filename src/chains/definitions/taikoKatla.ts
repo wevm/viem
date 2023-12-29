5bc5ddd26f30f6a7229d687ea3e56d84339b1b2a
Kenk <kenghin_lim@hotmail.com>
2023-12-29 23:14:40 +0800

    Add Taiko Katla

---
+++ src/chains/definitions/taikoKatla.ts
@@ -0,0 +1,22 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const taikoKatla = /*#__PURE__*/ defineChain({
+  id: 167008,
+  name: 'Taiko Katla (Alpha-6 Testnet)',
+  network: 'tko-katla',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.katla.taiko.xyz'],
+    },
+    public: {
+      http: ['https://rpc.katla.taiko.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'blockscout',
+      url: 'https://explorer.katla.taiko.xyz',
+    },
+  },
+})
