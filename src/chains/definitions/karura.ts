e6855a9b650ecfda3de3ed395fe8a38906709f5e
Shunji Zhan <shunhahaha@live.com>
2023-12-31 06:00:59 +0800

    feat: add acala family (#1629)
    add acala family

---
+++ src/chains/definitions/karura.ts
@@ -0,0 +1,29 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const karura = /*#__PURE__*/ defineChain({
+  id: 686,
+  name: 'Karura',
+  network: 'karura',
+  nativeCurrency: {
+    name: 'Karura',
+    symbol: 'KAR',
+    decimals: 18,
+  },
+  rpcUrls: {
+    public: {
+      http: ['https://eth-rpc-karura.aca-api.network'],
+      webSocket: ['wss://eth-rpc-karura.aca-api.network'],
+    },
+    default: {
+      http: ['https://eth-rpc-karura.aca-api.network'],
+      webSocket: ['wss://eth-rpc-karura.aca-api.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Karura Blockscout',
+      url: 'https://blockscout.karura.network',
+    },
+  },
+  testnet: false,
+})
