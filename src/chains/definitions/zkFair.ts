9750860b9fbd993489bc497e53ec9e1d22aea089
Mr. Kenikh <kenikh@me.com>
2023-12-30 15:12:48 +0400

    fix: remove multicall2 from zkfair mainnet

--- src/chains/definitions/zkFair.ts
+++ src/chains/definitions/zkFair.ts
@@ -23,11 +23,5 @@ export const zkFair = /*#__PURE__*/ defineChain({
       url: 'https://scan.zkfair.io',
     },
   },
-  contracts: {
-    multicall2: {
-      address: '0x1CbC08bf0D48b18F9f97796c61352b192d1850A5',
-      blockCreated: 6,
-    },
-  },
   testnet: false,
 })
