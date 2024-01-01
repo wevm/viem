f35806f3e59334771eed11dd9fb9144d82267cb2
moxey.eth <jakemoxey@gmail.com>
2024-01-02 08:06:03 +1100

    wip

--- vocs.config.ts
+++ vocs.config.ts
@@ -6,10 +6,171 @@ const require = createRequire(import.meta.url)
 const pkg = require('./src/package.json')
 
 export default defineConfig({
+  logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
   rootDir: 'site',
   sidebar,
+  socials: [
+    {
+      icon: 'github',
+      link: 'https://github.com/wevm/viem',
+    },
+    {
+      icon: 'discord',
+      link: 'https://discord.gg/xCUz9FRcXD',
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/wevm_dev',
+    },
+  ],
+  sponsors: [
+    {
+      name: 'Collaborator',
+      height: 120,
+      items: [
+        [
+          {
+            name: 'Paradigm',
+            link: 'https://paradigm.xyz',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/paradigm-light.svg',
+          },
+        ],
+      ],
+    },
+    {
+      name: 'Large Enterprise',
+      height: 60,
+      items: [
+        [
+          {
+            name: 'WalletConnect',
+            link: 'https://walletconnect.com',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/walletconnect-light.svg',
+          },
+          {
+            name: 'Stripe',
+            link: 'https://www.stripe.com',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/stripe-light.svg',
+          },
+        ],
+      ],
+    },
+    {
+      name: 'Small Enterprise',
+      height: 40,
+      items: [
+        [
+          {
+            name: 'Family',
+            link: 'https://twitter.com/family',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/family-light.svg',
+          },
+          {
+            name: 'Context',
+            link: 'https://twitter.com/context',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/context-light.svg',
+          },
+          {
+            name: 'PartyDAO',
+            link: 'https://twitter.com/prtyDAO',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/partydao-light.svg',
+          },
+        ],
+        [
+          {
+            name: 'SushiSwap',
+            link: 'https://www.sushi.com',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/sushi-light.svg',
+          },
+          {
+            name: 'Dynamic',
+            link: 'https://www.dynamic.xyz',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/dynamic-light.svg',
+          },
+          {
+            name: 'BitKeep',
+            link: 'https://bitkeep.com',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/bitkeep-light.svg',
+          },
+        ],
+        [
+          {
+            name: 'Privy',
+            link: 'https://privy.io',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/privy-light.svg',
+          },
+          {
+            name: 'Spruce',
+            link: 'https://spruceid.com',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/spruce-light.svg',
+          },
+          {
+            name: 'rollup.id',
+            link: 'https://rollup.id',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/rollup.id-light.svg',
+          },
+        ],
+        [
+          {
+            name: 'PancakeSwap',
+            link: 'https://pancakeswap.finance',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/pancake-light.svg',
+          },
+          {
+            name: 'Celo',
+            link: 'https://celo.org',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/celo-light.svg',
+          },
+          {
+            name: 'Rainbow',
+            link: 'https://rainbow.me',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/rainbow-light.svg',
+          },
+        ],
+        [
+          {
+            name: 'Pimlico',
+            link: 'https://pimlico.io',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/pimlico-light.svg',
+          },
+          {
+            name: 'Zora',
+            link: 'https://zora.co',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/zora-light.svg',
+          },
+          {
+            name: 'Supa',
+            link: 'https://twitter.com/supafinance',
+            image:
+              'https://raw.githubusercontent.com/wevm/.github/main/content/sponsors/supa-light.svg',
+          },
+        ],
+      ],
+    },
+  ],
   theme: {
-    accentColor: 'orange',
+    accentColor: {
+      light: '#ff9318',
+      dark: '#ffc517',
+    },
+    colorScheme: 'system',
   },
   topNav: [
     { text: 'Docs', link: '/docs/getting-started', match: '/docs' },
