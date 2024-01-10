7542621acf979a7d83c899ea179c1751608ae424
Mr. Kenikh <kenikh@me.com>
2024-01-10 22:15:48 +0400

    feat: add multicall3 for zkFair Mainnet

---
+++ .changeset/sweet-singers-burn.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Add multicall3 for zkFair Mainnet
--- src/chains/definitions/zkFair.ts
+++ src/chains/definitions/zkFair.ts
@@ -23,5 +23,11 @@ export const zkFair = /*#__PURE__*/ defineChain({
       url: 'https://scan.zkfair.io',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 6090959,
+    },
+  },
   testnet: false,
 })
