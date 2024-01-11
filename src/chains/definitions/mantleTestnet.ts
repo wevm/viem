7fccf05ab3b246faf34cb47a25b1a0670a5794b9
jxom <jxom@users.noreply.github.com>
2024-01-11 02:33:22 +0000

    chore: format

--- src/chains/definitions/mantleTestnet.ts
+++ src/chains/definitions/mantleTestnet.ts
@@ -20,7 +20,7 @@ export const mantleTestnet = /*#__PURE__*/ defineChain({
   contracts: {
     multicall3: {
       address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-      blockCreated: 561333
+      blockCreated: 561333,
     },
   },
   testnet: true,
