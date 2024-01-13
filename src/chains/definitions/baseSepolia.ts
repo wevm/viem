c6a92739b8830a07ec38a32d4a7a18a4fa518951
Yuri <yuripetusko@gmail.com>
2024-01-13 19:43:26 +0100

    add biome format/lint results

--- src/chains/definitions/baseSepolia.ts
+++ src/chains/definitions/baseSepolia.ts
@@ -40,7 +40,7 @@ export const baseSepolia = /*#__PURE__*/ defineChain({
     multicall3: {
       address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 1059647,
-    }
+    },
   },
   testnet: true,
   sourceId,
