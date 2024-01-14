ba0e437df16f11a026ffc808311e34cafc0552eb
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-01-14 14:38:57 -0500

    fix: optimism explorer url, previous entry redirects to this

--- src/chains/definitions/optimism.ts
+++ src/chains/definitions/optimism.ts
@@ -16,7 +16,7 @@ export const optimism = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: 'Optimism Explorer',
-      url: 'https://explorer.optimism.io',
+      url: 'https://optimistic.etherscan.io',
       apiUrl: 'https://api-optimistic.etherscan.io',
     },
   },
