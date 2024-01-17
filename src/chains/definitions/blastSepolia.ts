f5d0410e56f464dbbf9ac2f6fcf9d22d9e5ae338
aaron <aaron@0xdefi.club>
2024-01-17 15:53:13 +0800

    Create blastSepolia.ts

---
+++ src/chains/definitions/blastSepolia.ts
@@ -0,0 +1,24 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const blastSepolia = /*#__PURE__*/ defineChain({
+  id: 168_587_773,
+  name: 'Blast Sepolia',
+  nativeCurrency: {
+    name: 'Blast Sepolia Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://sepolia.blast.io'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blastscan',
+      url: 'https://testnet.blastscan.io',
+      apiUrl: 'https://testnet.blastscan.io/api',
+    },
+  },
+  testnet: true,
+})
