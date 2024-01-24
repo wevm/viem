20cfabe72d22ebd302f128a64aa7a27305bd8936
zongzheng <304389312@qq.com>
2024-01-24 19:49:22 +0800

    feat: add multicall3 contract configuration for iotex
    Signed-off-by: zongzheng <304389312@qq.com>

--- src/chains/definitions/iotex.ts
+++ src/chains/definitions/iotex.ts
@@ -20,4 +20,10 @@ export const iotex = /*#__PURE__*/ defineChain({
       url: 'https://iotexscan.io',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 22163670,
+    },
+  },
 })
