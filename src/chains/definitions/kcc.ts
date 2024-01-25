c48159a35646675215e36c239241ba6a265f0e2b
berrybai <berrybai@ethsign.xyz>
2024-01-25 11:53:32 +0800

    Added kcc chain

--- src/chains/definitions/kcc.ts
+++ src/chains/definitions/kcc.ts
@@ -1,30 +1,30 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const kcc = /*#__PURE__*/ defineChain({
   id: 321,
-  name: "KCC Mainnet",
-  network: "KCC Mainnet",
+  name: 'KCC Mainnet',
+  network: 'KCC Mainnet',
   nativeCurrency: {
     decimals: 18,
-    name: "KCS",
-    symbol: "KCS",
+    name: 'KCS',
+    symbol: 'KCS',
   },
   rpcUrls: {
     default: {
-      http: ["https://kcc-rpc.com"],
+      http: ['https://kcc-rpc.com'],
     },
     public: {
-      http: ["https://kcc-rpc.com"],
+      http: ['https://kcc-rpc.com'],
     },
   },
   blockExplorers: {
-    default: { name: "Explorer", url: "https://explorer.kcc.io/en" },
+    default: { name: 'Explorer', url: 'https://explorer.kcc.io/en' },
   },
   contracts: {
     multicall3: {
-      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 11760430,
     },
   },
   testnet: false,
-});
+})
