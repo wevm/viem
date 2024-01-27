6c18af54eceb5153df699747d51c1d03a9de8648
jxom <jakemoxey@gmail.com>
2024-01-28 10:17:29 +1100

    Update berachainTestnet.ts

--- src/chains/definitions/berachainTestnet.ts
+++ src/chains/definitions/berachainTestnet.ts
@@ -2,11 +2,11 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const berachainTestnet = /*#__PURE__*/ defineChain({
   id: 80085,
-  name: 'Berachain Testnet',
+  name: 'Berachain Artio',
   nativeCurrency: {
     decimals: 18,
-    name: 'tBera Coin',
-    symbol: 'tBERA',
+    name: 'BERA Token',
+    symbol: 'BERA',
   },
   rpcUrls: {
     default: { http: ['https://artio.rpc.berachain.com'] },
@@ -15,13 +15,6 @@ export const berachainTestnet = /*#__PURE__*/ defineChain({
     default: {
       name: 'Berachain',
       url: 'https://artio.beratrail.io',
-      apiUrl: 'https://api.routescan.io/v2/network/testnet/evm/80085/etherscan',
-    },
-  },
-  contracts: {
-    multicall3: {
-      address: '0x9d1dB8253105b007DDDE65Ce262f701814B91125',
-      blockCreated: 1,
     },
   },
   testnet: true,
