56f0a958f801342250304faba50c796040f61fbf
moxey.eth <jakemoxey@gmail.com>
2024-01-31 10:57:42 +1100

    chore: trusted setup json

--- test/src/kzg.ts
+++ test/src/kzg.ts
@@ -2,6 +2,6 @@ import { resolve } from 'node:path'
 import * as cKzg from 'c-kzg'
 import { setupKzg } from '~viem/utils/kzg/setupKzg.js'
 
-export const trustedSetupPath = resolve(__dirname, '../kzg/trusted_setup.txt')
+export const trustedSetupPath = resolve(__dirname, '../kzg/trusted_setup.json')
 
 export const kzg = setupKzg(trustedSetupPath, cKzg)
