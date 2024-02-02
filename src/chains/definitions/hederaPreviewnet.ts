071e22ed33aca5e69ce47783024aee7530e7d4df
jxom <jxom@users.noreply.github.com>
2024-02-02 00:52:05 +0000

    chore: format

--- src/chains/definitions/hederaPreviewnet.ts
+++ src/chains/definitions/hederaPreviewnet.ts
@@ -1,24 +1,24 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const hederaPreviewnet = /*#__PURE__*/ defineChain({
-    id: 297,
-    name: 'Hedera Previewnet',
-    network: 'hedera-previewnet',
-    nativeCurrency: {
-        symbol: 'HBAR',
-        name: 'HBAR',
-        decimals:  18,
+  id: 297,
+  name: 'Hedera Previewnet',
+  network: 'hedera-previewnet',
+  nativeCurrency: {
+    symbol: 'HBAR',
+    name: 'HBAR',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://previewnet.hashio.io/api'],
     },
-    rpcUrls: {
-        default: {
-            http: ['https://previewnet.hashio.io/api'],
-        },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Hashscan',
+      url: 'https://hashscan.io/previewnet',
     },
-    blockExplorers: {
-        default: {
-            name: 'Hashscan',
-            url: 'https://hashscan.io/previewnet'
-        },
-    },
-    testnet: true,
+  },
+  testnet: true,
 })
