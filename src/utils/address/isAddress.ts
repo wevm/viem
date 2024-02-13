25b6ff7f9304a2a2592295ccb0feeac81fdfc026
dawsbot <dawsonbotsford@gmail.com>
2024-02-13 09:35:40 -0700

    formatting

--- src/utils/address/isAddress.ts
+++ src/utils/address/isAddress.ts
@@ -1,18 +1,18 @@
 import type { Address } from 'abitype'
 import type { ErrorType } from '../../errors/utils.js'
-import { checksumAddress} from './getAddress.js'
+import { checksumAddress } from './getAddress.js'
 
 const addressRegex = /^0x[a-fA-F0-9]{40}$/
 
 export type IsAddressErrorType = ErrorType
 
 export function isAddress(address: string): address is Address {
-  if (!addressRegex.test(address) ) {
-    return false;
+  if (!addressRegex.test(address)) {
+    return false
   }
   if (address.toLowerCase() === address) {
-    return true;
+    return true
   }
   // @ts-ignore
-  return checksumAddress(address) === address;
+  return checksumAddress(address) === address
 }
