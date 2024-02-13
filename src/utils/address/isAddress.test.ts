5d840375294054f53f45c9f407706a3f23673236
dawsbot <dawsonbotsford@gmail.com>
2024-02-13 09:30:43 -0700

    Fix checksum validation on isAddress

--- src/utils/address/isAddress.test.ts
+++ src/utils/address/isAddress.test.ts
@@ -3,7 +3,9 @@ import { expect, test } from 'vitest'
 import { isAddress } from './isAddress.js'
 
 test('checks if address is valid', () => {
-  expect(isAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac')).toBeTruthy()
+  expect(isAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac')).toBeFalsy()
+  expect(isAddress('x')).toBeFalsy()
+  expect(isAddress('0xa')).toBeFalsy()
   expect(isAddress('0xa0cf798816d4b9b9866b5330eea46a18382f251e')).toBeTruthy()
   expect(isAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678az')).toBeFalsy()
   expect(isAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678aff')).toBeFalsy()
--- src/utils/address/isAddress.ts
+++ src/utils/address/isAddress.ts
@@ -1,10 +1,18 @@
 import type { Address } from 'abitype'
 import type { ErrorType } from '../../errors/utils.js'
+import { checksumAddress} from './getAddress.js'
 
 const addressRegex = /^0x[a-fA-F0-9]{40}$/
 
 export type IsAddressErrorType = ErrorType
 
 export function isAddress(address: string): address is Address {
-  return addressRegex.test(address)
+  if (!addressRegex.test(address) ) {
+    return false;
+  }
+  if (address.toLowerCase() === address) {
+    return true;
+  }
+  // @ts-ignore
+  return checksumAddress(address) === address;
 }
