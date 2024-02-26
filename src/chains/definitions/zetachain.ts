55d89118c97180c390320326b66e517d29f2872f
jxom <jxom@users.noreply.github.com>
2024-02-26 20:33:40 +0000

    chore: format

--- src/chains/definitions/zetachain.ts
+++ src/chains/definitions/zetachain.ts
@@ -1,23 +1,23 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const zetachain = /*#__PURE__*/ defineChain({
-    id: 7000,
-    name: 'ZetaChain',
-    nativeCurrency: {
-        decimals: 18,
-        name: 'Zeta',
-        symbol: 'ZETA'
+  id: 7000,
+  name: 'ZetaChain',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Zeta',
+    symbol: 'ZETA',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://zetachain-evm.blockpi.network/v1/rpc/public'],
     },
-    rpcUrls: {
-        default: {
-            http: ['https://zetachain-evm.blockpi.network/v1/rpc/public']
-        }
+  },
+  blockExplorers: {
+    default: {
+      name: 'ZetaScan',
+      url: 'https://explorer.zetachain.com',
     },
-    blockExplorers: {
-        default: {
-            name: 'ZetaScan',
-            url: 'https://explorer.zetachain.com'
-        }
-    },
-    testnet: false
-});
+  },
+  testnet: false,
+})
