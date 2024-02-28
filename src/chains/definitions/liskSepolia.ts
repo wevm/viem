21f4a69c4135bd70f6034c599f5b17eb6c81aa66
Tschakki <tschakki@posteo.de>
2024-02-28 10:42:43 +0100

    Add contract info

--- src/chains/definitions/liskSepolia.ts
+++ src/chains/definitions/liskSepolia.ts
@@ -23,6 +23,24 @@ export const liskSepolia = /*#__PURE__*/ defineChain({
   },
   contracts: {
     ...chainConfig.contracts,
+    l2OutputOracle: {
+        [sourceId]: {
+          address: '0xA0E35F56C318DE1bD5D9ca6A94Fe7e37C5663348',
+        },
+      },
+      multicall3: {
+        address: '0xca11bde05977b3631167028862be2a173976ca11',
+      },
+      portal: {
+        [sourceId]: {
+          address: '0xe3d90F21490686Ec7eF37BE788E02dfC12787264',
+        },
+      },
+      l1StandardBridge: {
+        [sourceId]: {
+          address: '0x1Fb30e446eA791cd1f011675E5F3f5311b70faF5',
+        },
+      },
   },
   testnet: true,
   sourceId,
