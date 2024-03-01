3f703fd0c699aae758dfd25594554de5428b581c
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-01 02:59:49 -0500

    feat: add Plume Testnet

---
+++ src/chains/definitions/plumeTestnet.ts
@@ -0,0 +1,33 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+const sourceId = 11_155_111 // sepolia
+
+export const plumeTestnet = /*#__PURE__*/ defineChain({
+  id: 161_221_135,
+  network: 'plume-testnet',
+  name: 'Plume Testnet',
+  nativeCurrency: {
+    name: 'Plume Sepolia Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://plume-testnet.rpc.caldera.xyz/http'],
+      webSocket: ['wss://plume-testnet.rpc.caldera.xyz/ws'],
+    },
+    public: {
+      http: ['https://plume-testnet.rpc.caldera.xyz/http'],
+      webSocket: ['wss://plume-testnet.rpc.caldera.xyz/ws'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://plume-testnet.explorer.caldera.xyz',
+      apiUrl: 'https://plume-testnet.explorer.caldera.xyz/api',
+    },
+  },
+  testnet: true,
+  sourceId,
+})
