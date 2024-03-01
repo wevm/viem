23f4c618e205042034cfffa692f3909aa0afaae1
jxom <jakemoxey@gmail.com>
2024-03-01 13:36:56 +1100

    Update blast.ts

--- src/chains/definitions/blast.ts
+++ src/chains/definitions/blast.ts
@@ -1,5 +1,7 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
+const sourceId = 1 // mainnet
+
 export const blast = /*#__PURE__*/ defineChain({
     id: 81457,
     name: 'Blast',
@@ -17,9 +19,10 @@ export const blast = /*#__PURE__*/ defineChain({
       default: { name: 'Blastscan', url: 'https://blastscan.io' },
     },
     contracts: {
-        multicall3: {
-          address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-          blockCreated: 212929,
-        },
+      multicall3: {
+        address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+        blockCreated: 212929,
       },
+    },
+    sourceId
   })
