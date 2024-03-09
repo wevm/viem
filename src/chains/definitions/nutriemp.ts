0b4c3cae97a65306139283dd7a51633cc997c5da
NUTRIEMP.CRYPTO <99441186+NUTRIEMP@users.noreply.github.com>
2024-03-09 14:33:08 +0000

    Create nutriemp.ts
    Details to add NutriEmp Chain.

---
+++ src/chains/definitions/nutriemp.ts
@@ -0,0 +1,20 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const mainnet = /*#__PURE__*/ defineChain({
+  id: 420000,
+  name: 'NutriEmp - Chain',
+  nativeCurrency: { name: 'GRAMZ', symbol: 'GRAMZ', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://node2.nutriempchain.link'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Nutriemp - Explorer',
+      url: 'https://explorer.nutriempchain.link',
+
+    },
+  },
+
+})
