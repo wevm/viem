2dd2b8eda3ab810041657b98aaec60c51e2cb084
moxey.eth <jakemoxey@gmail.com>
2024-03-12 06:18:37 +1100

    chore: 4844 vectors

--- vectors/src/transaction.ts
+++ vectors/src/transaction.ts
@@ -4,10 +4,11 @@ import { join } from 'node:path'
 
 import { generatePrivateKey } from '../../src/accounts/generatePrivateKey.js'
 import { signTransaction } from '../../src/accounts/utils/signTransaction.js'
-import { parseTransaction } from '../../src/index.js'
+import { Hex, parseTransaction } from '../../src/index.js'
 import {
   TransactionSerializableEIP1559,
   TransactionSerializableEIP2930,
+  TransactionSerializableEIP4844,
   TransactionSerializableLegacy,
 } from '../../src/types/transaction.js'
 import { bytesToHex } from '../../src/utils/encoding/toHex.js'
@@ -42,6 +43,69 @@ export async function generateTransactionVectors() {
   const writer = generated.writer()
 
   writer.write('[')
+
+  // eip4844
+  for (let i = 0; i < 10_000; i++) {
+    if (i > 0) writer.write(',')
+    const transaction: TransactionSerializableEIP4844 = {
+      blobVersionedHashes: generateList(3).map((hash) => {
+        const hash_ = hash.slice(4)
+        return `0x01${hash_}` as Hex
+      }),
+      chainId: Math.floor(Math.random() * 2 ** 4) + 1,
+      type: 'eip4844',
+    }
+    if (i > 0) {
+      if (Math.random() > 0.5)
+        transaction.data = bytesToHex(
+          generateBytes(Math.floor(Math.random() * 2048)),
+        )
+      if (Math.random() > 0.5) transaction.gas = randomBigInt()
+      if (Math.random() > 0.5) transaction.maxFeePerGas = randomBigInt()
+      if (Math.random() > 0.5) transaction.maxFeePerBlobGas = randomBigInt()
+      if (Math.random() > 0.5 && transaction.maxFeePerGas)
+        transaction.maxPriorityFeePerGas =
+          transaction.maxFeePerGas -
+          transaction.maxFeePerGas /
+            (BigInt(Math.floor(Math.random() * 2)) + 1n)
+      if (Math.random() > 0.5)
+        transaction.nonce = Math.floor(Math.random() * 2 ** 16)
+      if (Math.random() > 0.5)
+        transaction.to = bytesToHex(generateBytes(20), { size: 20 })
+      if (Math.random() > 0.5) transaction.value = randomBigInt()
+      if (Math.random() > 0.5) {
+        const listLength = Math.floor(Math.random() * 16)
+        transaction.accessList = []
+        for (let i = 0; i < listLength; i++)
+          transaction.accessList.push({
+            address: bytesToHex(generateBytes(20), { size: 20 }),
+            storageKeys: generateList(Math.floor(Math.random() * 4)),
+          })
+      }
+    }
+    const serialized = serializeTransaction(transaction)
+
+    const privateKey = generatePrivateKey()
+    const serializedSigned = await signTransaction({ privateKey, transaction })
+    const { r, s, v } = parseTransaction(serializedSigned)
+
+    writer.write(
+      stringify(
+        {
+          name: `eip4844: ${i}`,
+          privateKey,
+          transaction,
+          serialized,
+          serializedSigned,
+          signature: { r, s, v, yParity: v === 28n ? 1 : 0 },
+        },
+        null,
+        2,
+      ),
+    )
+  }
+
+  writer.write(',')
 
   // eip1559
   for (let i = 0; i < 10_000; i++) {
