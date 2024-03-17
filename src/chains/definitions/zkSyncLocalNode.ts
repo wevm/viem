1e34e5b99ad1bb13cae911b023d1f3e23aea9a89
jxom <jakemoxey@gmail.com>
2024-03-18 07:15:58 +1100

    Update zkSyncLocalNode.ts

--- src/chains/definitions/zkSyncLocalNode.ts
+++ src/chains/definitions/zkSyncLocalNode.ts
@@ -2,7 +2,7 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const zkSyncLocalNode = /*#__PURE__*/ defineChain({
   id: 270,
-  name: 'zkSync CLI Local node',
+  name: 'zkSync CLI Local Node',
   network: 'zksync-cli-local-node',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
