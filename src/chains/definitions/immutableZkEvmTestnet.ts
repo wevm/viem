300e2f185942be081011b3fee3d3a07fb1d220b6
jxom <jakemoxey@gmail.com>
2024-03-22 09:34:57 +1100

    Update immutableZkEvmTestnet.ts

--- src/chains/definitions/immutableZkEvmTestnet.ts
+++ src/chains/definitions/immutableZkEvmTestnet.ts
@@ -3,26 +3,17 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const immutableZkEvmTestnet = defineChain({
   id: 13473,
   name: 'Immutable zkEVM Testnet',
-  testnet: true,
-  network: 'immutable',
   nativeCurrency: {
     decimals: 18,
     name: 'Immutable Coin',
     symbol: 'IMX',
   },
   rpcUrls: {
-    public: {
-      http: ['https://rpc.testnet.immutable.com'],
-    },
     default: {
       http: ['https://rpc.testnet.immutable.com'],
     },
   },
   blockExplorers: {
-    etherscan: {
-      name: 'Immutable Testnet Explorer',
-      url: 'https://explorer.testnet.immutable.com/',
-    },
     default: {
       name: 'Immutable Testnet Explorer',
       url: 'https://explorer.testnet.immutable.com/',
@@ -34,4 +25,5 @@ export const immutableZkEvmTestnet = defineChain({
       blockCreated: 5977391,
     },
   },
+  testnet: true,
 })
