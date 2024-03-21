f4ada5c389c5915e950c8d2b563de35105acd18c
jxom <jakemoxey@gmail.com>
2024-03-22 09:34:35 +1100

    Update immutableZkEvm.ts

--- src/chains/definitions/immutableZkEvm.ts
+++ src/chains/definitions/immutableZkEvm.ts
@@ -3,26 +3,17 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const immutableZkEvm = /*#__PURE__*/ defineChain({
   id: 13371,
   name: 'Immutable zkEVM',
-  testnet: false,
-  network: 'immutable',
   nativeCurrency: {
     decimals: 18,
     name: 'Immutable Coin',
     symbol: 'IMX',
   },
   rpcUrls: {
-    public: {
-      http: ['https://rpc.immutable.com'],
-    },
     default: {
       http: ['https://rpc.immutable.com'],
     },
   },
   blockExplorers: {
-    etherscan: {
-      name: 'Immutable Explorer',
-      url: 'https://explorer.immutable.com',
-    },
     default: {
       name: 'Immutable Explorer',
       url: 'https://explorer.immutable.com',
