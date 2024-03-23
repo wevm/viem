3aab3dbdb8d87363d1f46905ee5e5a2b975799a3
Albert Su <albertsu2001@gmail.com>
2024-03-22 17:58:43 -0700

    ✅Lint code

--- src/chains/definitions/edgeless.ts
+++ src/chains/definitions/edgeless.ts
@@ -3,7 +3,11 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const edgeless = /*#__PURE__*/ defineChain({
   id: 2_026,
   name: 'Edgeless Network',
-  nativeCurrency: { name: 'Edgeless Wrapped ETH', symbol: 'EwETH', decimals: 18 },
+  nativeCurrency: {
+    name: 'Edgeless Wrapped ETH',
+    symbol: 'EwETH',
+    decimals: 18,
+  },
   rpcUrls: {
     default: {
       http: ['https://rpc.edgeless.network/http'],
--- src/chains/definitions/edgelessTestnet.ts
+++ src/chains/definitions/edgelessTestnet.ts
@@ -3,7 +3,11 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const edgelessTestnet = /*#__PURE__*/ defineChain({
   id: 202,
   name: 'Edgeless Testnet',
-  nativeCurrency: { name: 'Edgeless Wrapped ETH', symbol: 'EwETH', decimals: 18 },
+  nativeCurrency: {
+    name: 'Edgeless Wrapped ETH',
+    symbol: 'EwETH',
+    decimals: 18,
+  },
   rpcUrls: {
     default: {
       http: ['https://edgeless-testnet.rpc.caldera.xyz/http'],
