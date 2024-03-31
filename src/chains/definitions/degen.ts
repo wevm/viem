1aeda2564b72bb5e12b2edfceb48a4f660ea9d2a
Cryptoflop <max.david1007@gmail.com>
2024-03-31 18:57:39 +0200

    Add Degen chain

---
+++ src/chains/definitions/degen.ts
@@ -0,0 +1,26 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const degen = /*#__PURE__*/ defineChain({
+  id: 666666666,
+  name: 'Degen',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Degen',
+    symbol: 'DEGEN',
+  },
+  rpcUrls: {
+    default: {
+      http: [
+        'https://rpc.degen.tips',
+        'https://nitrorpc-degen-mainnet-1.t.conduit.xyz',
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Degen Chain explorer',
+      url: 'https://explorer.degen.tips',
+      apiUrl: 'https://explorer.degen.tips/api/v2',
+    },
+  },
+})
