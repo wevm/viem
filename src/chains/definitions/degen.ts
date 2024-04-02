445ec244c7be329eb26048102d282bac9bf23506
Marco <marcokaul@gmail.com>
2024-04-02 21:32:29 +0200

    feat: update degen chain config with websocket rpc and multicall (#2052)

--- src/chains/definitions/degen.ts
+++ src/chains/definitions/degen.ts
@@ -11,6 +11,7 @@ export const degen = /*#__PURE__*/ defineChain({
   rpcUrls: {
     default: {
       http: ['https://rpc.degen.tips'],
+      webSocket: ["wss://rpc.degen.tips"],
     },
   },
   blockExplorers: {
@@ -18,6 +19,12 @@ export const degen = /*#__PURE__*/ defineChain({
       name: 'Degen Chain Explorer',
       url: 'https://explorer.degen.tips',
       apiUrl: 'https://explorer.degen.tips/api/v2',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xFBF562a98aB8584178efDcFd09755FF9A1e7E3a2",
+      blockCreated: 414273,
     },
   },
 })
