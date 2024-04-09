c359f6166d875d6dcdcafebd72875eb31a451676
jxom <jakemoxey@gmail.com>
2024-04-09 17:46:30 +1000

    Update bevmMainnet.ts

--- src/chains/definitions/bevmMainnet.ts
+++ src/chains/definitions/bevmMainnet.ts
@@ -6,7 +6,6 @@ export const bevmMainnet = /*#__PURE__*/ defineChain({
   nativeCurrency: { name: 'Bitcoin', symbol: 'BTC', decimals: 18 },
   rpcUrls: {
     default: { http: ['https://rpc-mainnet-1.bevm.io'] },
-    public: { http: ['https://rpc-mainnet-1.bevm.io'] },
   },
   blockExplorers: {
     default: {
@@ -14,11 +13,5 @@ export const bevmMainnet = /*#__PURE__*/ defineChain({
       url: 'https://scan-mainnet.bevm.io',
       apiUrl: 'https://scan-mainnet-api.bevm.io/api',
     },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xa7487A536968Be0D563901aeb3Fc07B099e2fb04',
-      blockCreated: 129086,
-    },
-  },
+  }
 })
