3cd26e3a8512b4da6e2298e6ebc9976c93853ac9
jxom <jakemoxey@gmail.com>
2024-04-15 15:29:00 +1000

    Update taikoHekla.ts

--- src/chains/definitions/taikoHekla.ts
+++ src/chains/definitions/taikoHekla.ts
@@ -1,23 +1,18 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const taikoHekla = /*#__PURE__*/ defineChain({
-  id: 167009,
-  name: 'Taiko Hekla (Alpha-7 Testnet)',
-  network: 'tko-hekla',
+  id: 167_009,
+  name: 'Taiko Hekla L2',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
     default: {
       http: ['https://rpc.hekla.taiko.xyz'],
     },
-    public: {
-      http: ['https://rpc.hekla.taiko.xyz'],
-    },
   },
   blockExplorers: {
     default: {
-      name: 'taikoscan',
+      name: 'Taikoscan',
       url: 'https://hekla.taikoscan.network',
     },
   },
-  testnet: true,
 })
