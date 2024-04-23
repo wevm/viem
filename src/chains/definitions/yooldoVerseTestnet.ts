fc7789987ab2cf554ee494b1dc7c90833e5a4392
aiiiden <hjjam100@gmail.com>
2024-04-23 15:46:36 +0900

    Add yooldo verse mainnet and testnet chains

---
+++ src/chains/definitions/yooldoVerseTestnet.ts
@@ -0,0 +1,20 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const yooldoVerseTestnet = /*#__PURE__*/ defineChain({
+  id: 50_006,
+  name: 'Yooldo Verse Testnet',
+  network: 'yooldoVerseTestnet',
+  nativeCurrency: { name: 'OAS', symbol: 'OAS', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.testnet.yooldo-verse.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Yooldo Verse Testnet Explorer',
+      url: 'https://explorer.testnet.yooldo-verse.xyz',
+    },
+  },
+  testnet: true,
+})
