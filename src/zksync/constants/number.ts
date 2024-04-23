19a0fcb0dad048b7f20971192be2086d0bccd92f
moxey.eth <jakemoxey@gmail.com>
2024-04-23 13:51:27 +1000

    tweaks

--- src/zksync/constants/number.ts
+++ src/zksync/constants/number.ts
@@ -1,4 +1,4 @@
 import { maxUint16 } from '../../constants/number.js'
 
-export const maxBytecodeSize = maxUint16 * 32n
 export const gasPerPubdataDefault = 50000n
+export const maxBytecodeSize = maxUint16 * 32n
