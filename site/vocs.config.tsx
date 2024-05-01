89b8b56bb3bdddb9e8ba22c6392bfa8b7acd3dce
moxey.eth <jakemoxey@gmail.com>
2024-05-01 13:21:51 +1000

    docs: update vocs; add document boosting for core API

--- site/vocs.config.tsx
+++ site/vocs.config.tsx
@@ -40,6 +40,12 @@ export default defineConfig({
   iconUrl: { light: '/favicons/light.png', dark: '/favicons/dark.png' },
   logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
   rootDir: '.',
+  search: {
+    boostDocument(documentId) {
+      if (documentId.startsWith('pages/docs')) return 2
+      return 1
+    },
+  },
   sidebar,
   socials: [
     {
