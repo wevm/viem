69e823d35a438dad1daec28d140ba8f660a2000e
Wilson Cusack <wilson.cusack@coinbase.com>
2024-05-02 16:49:58 -0400

    update name to parseERC6492Signature

--- src/utils/signature/parseERC6492Signature.ts
+++ src/utils/signature/parseERC6492Signature.ts
@@ -3,13 +3,13 @@ import type { Hex } from '../../types/misc.js'
 import { decodeAbiParameters } from '../abi/decodeAbiParameters.js'
 import { isERC6492Signature } from './isERC6492Signature.js'
 
-export type parseSignatureReturnType = {
+export type ParseERC6492SignatureReturnType = {
   sigToValidate: Hex
   factory?: Address
   factoryCalldata?: Hex
 }
 
-export function parseSignature(signature: Hex): parseSignatureReturnType {
+export function parseERC6492Signature(signature: Hex): ParseERC6492SignatureReturnType {
   if (!isERC6492Signature(signature)) return { sigToValidate: signature }
 
   const [factory, factoryCalldata, sigToValidate] = decodeAbiParameters(
