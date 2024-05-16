08b62935d1c010fd8b7fc8cd2f7ab5b5936ba442
moxey.eth <jakemoxey@gmail.com>
2024-05-16 11:21:17 +1000

    fix: svelte test env

--- environments/sveltekit/src/routes/+page.svelte
+++ environments/sveltekit/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 import { createPublicClient, webSocket } from 'viem'
 import { mainnet } from 'viem/chains'
 
-const _promise = getBlockNumber()
+const promise = getBlockNumber()
 async function getBlockNumber() {
   const client = createPublicClient({
     chain: mainnet,
