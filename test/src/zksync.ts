6a4832e50adbab2d35d1c0e28ceb3cf299932a25
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-05-17 21:32:16 +0200

    fix: add missing zksync test clients

---
+++ test/src/zksync.ts
@@ -0,0 +1,29 @@
+import { zkSyncLocalNode } from '~viem/chains/index.js'
+import { createClient } from '~viem/clients/createClient.js'
+import { http } from '~viem/index.js'
+import { accounts } from './constants.js'
+
+export const zkSyncClientLocalNode = createClient({
+  chain: zkSyncLocalNode,
+  transport: http(),
+})
+
+export const zkSyncClientLocalNodeWithAccount = createClient({
+  account: accounts[0].address,
+  chain: zkSyncLocalNode,
+  transport: http(),
+})
+
+export function getZksyncMockProvider(
+  request: ({
+    method,
+    params,
+  }: { method: string; params?: unknown }) => Promise<any>,
+) {
+  return {
+    on: () => null,
+    removeListener: () => null,
+    request: ({ method: string, params: any }) =>
+      request({ method: string, params: any }) as any,
+  }
+}
