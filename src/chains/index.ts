5de7e542c53471a44723d304f35212202f4f1c80
abs3ntdev <86447830+abs3ntdev@users.noreply.github.com>
2024-05-17 17:37:01 -0700

    feat: add Lisk chain (#2272)
    lisk
    
    Co-authored-by: abs3ntdev <abs3ntdev@users.noreply.github.com>

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -130,6 +130,7 @@ export { lineaGoerli } from './definitions/lineaGoerli.js'
 export { lineaSepolia } from './definitions/lineaSepolia.js'
 /** @deprecated Use `lineaGoerli` instead. */
 export { lineaTestnet } from './definitions/lineaTestnet.js'
+export { lisk } from './definitions/lisk.js'
 export { liskSepolia } from './definitions/liskSepolia.js'
 export { localhost } from './definitions/localhost.js'
 export { lukso } from './definitions/lukso.js'
