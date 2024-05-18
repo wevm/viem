072cd966eab37ae9fabf355ab983114d687799b2
hoangtan282 <tan.nguyen@sipher.xyz>
2024-05-18 10:15:42 +0700

    fix according to comments

--- src/chains/definitions/funkiSepolia.ts
+++ src/chains/definitions/funkiSepolia.ts
@@ -23,23 +23,6 @@ export const funkiSepolia = defineChain({
   testnet: true,
   contracts: {
     ...chainConfig.contracts,
-    l2OutputOracle: {
-      [sourceId]: {
-        address: "0xB25812386D1Cb976b50de7387F5CBc10Fec3F27c",
-      },
-    },
-    portal: {
-      [sourceId]: {
-        address: "0xCeE7ef4dDF482447FE14c605Ea94B37cBE87Ca9D",
-        blockCreated: 5854312,
-      },
-    },
-    l1StandardBridge: {
-      [sourceId]: {
-        address: "0x1ba82f688eF3C5B4363Ff667254ed4DC59E97477",
-        blockCreated: 5854312,
-      },
-    },
     multicall3: {
       address: "0xca11bde05977b3631167028862be2a173976ca11",
       blockCreated: 1620204,
