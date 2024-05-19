70dc0a92c18c7ee746598036b8a1c93ca54e71a3
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-05-19 18:34:14 +0200

    fix: remove chainOverride from zksyncLocalNodeL1

--- src/chains/definitions/zkSyncLocalNodeL1.ts
+++ src/chains/definitions/zkSyncLocalNodeL1.ts
@@ -1,8 +1,6 @@
-import { chainConfig } from '~viem/zksync/chainConfig.js'
 import { defineChain } from '../utils.js'
 
 export const zkSyncLocalNodeL1 = /*#__PURE__*/ defineChain({
-  ...chainConfig,
   id: 9,
   name: 'zkSync CLI Local Node L1',
   network: 'zksync-cli-local-node',
