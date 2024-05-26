41b48d47eb326de497773ba73ea89be41f980add
Tom Meagher <tom@meagher.co>
2024-05-26 11:02:06 -0400

    chore: remove console log

--- src/utils/typedData.ts
+++ src/utils/typedData.ts
@@ -57,8 +57,6 @@ export function serializeTypedData<
     return normalizeData(types[primaryType], message_)
   })()
 
-  console.log(message)
-
   return stringify({ domain, message, primaryType, types })
 }
 
