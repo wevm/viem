27ba542111f42004b21cec987dbe621e26e4b0a7
Pfed-prog <Pfed-prog@users.noreply.github.com>
2024-05-27 18:20:26 +0000

    chore: format

--- src/chains/definitions/redstone.ts
+++ src/chains/definitions/redstone.ts
@@ -1,20 +1,20 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const redstone = defineChain({
   id: 690,
-  name: "Redstone",
+  name: 'Redstone',
   nativeCurrency: {
     decimals: 18,
-    name: "Ether",
-    symbol: "ETH",
+    name: 'Ether',
+    symbol: 'ETH',
   },
   rpcUrls: {
     default: {
-      http: ["https://rpc.redstonechain.com"],
-      webSocket: ["wss://rpc.redstonechain.com"],
+      http: ['https://rpc.redstonechain.com'],
+      webSocket: ['wss://rpc.redstonechain.com'],
     },
   },
   blockExplorers: {
-    default: { name: "Explorer", url: "	https://explorer.redstone.xyz" },
+    default: { name: 'Explorer', url: '	https://explorer.redstone.xyz' },
   },
-});
+})
