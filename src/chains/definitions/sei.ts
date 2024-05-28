c3757afb6bf1587c51787a6fa67e435217a54ee5
abs3ntdev <abs3ntdev@users.noreply.github.com>
2024-05-28 08:27:24 -0700

    websocket

--- src/chains/definitions/sei.ts
+++ src/chains/definitions/sei.ts
@@ -1,24 +1,24 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const sei = /*#__PURE__*/ defineChain({
   id: 1329,
-  name: "Sei",
-  nativeCurrency: { name: "Sei", symbol: "SEI", decimals: 18 },
+  name: 'Sei',
+  nativeCurrency: { name: 'Sei', symbol: 'SEI', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ["https://evm-rpc.sei-apis.com/"],
-      webSocket: ["wss://evm-ws.sei-apis.com/"],
+      http: ['https://evm-rpc.sei-apis.com/'],
+      webSocket: ['wss://evm-ws.sei-apis.com/'],
     },
   },
   blockExplorers: {
     default: {
-      name: "Seitrace",
-      url: "https://seitrace.com",
+      name: 'Seitrace',
+      url: 'https://seitrace.com',
     },
   },
   contracts: {
     multicall3: {
-      address: "0xcb2436774C3e191c85056d248EF4260ce5f27A9D",
+      address: '0xcb2436774C3e191c85056d248EF4260ce5f27A9D',
     },
   },
-});
+})
