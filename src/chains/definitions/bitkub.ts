0ac55c43ba6950343b4ebdb7490c17996ea32841
jxom <j@wevm.dev>
2024-06-04 14:36:48 +1000

    Update bitkub.ts

--- src/chains/definitions/bitkub.ts
+++ src/chains/definitions/bitkub.ts
@@ -3,13 +3,9 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const bitkub = /*#__PURE__*/ defineChain({
   id: 96,
   name: 'Bitkub',
-  network: 'Bitkub',
   nativeCurrency: { name: 'Bitkub', symbol: 'KUB', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://rpc.bitkubchain.io'],
-    },
-    public: {
       http: ['https://rpc.bitkubchain.io'],
     },
   },
@@ -20,11 +16,4 @@ export const bitkub = /*#__PURE__*/ defineChain({
       apiUrl: 'https://www.bkcscan.com/api',
     },
   },
-  contracts: {
-    multicall3: {
-      address: '0x45Ed3FFA3903cf3c7D740aE2A9b7C7f0fFCe61Fe',
-      blockCreated: 15967658,
-    },
-  },
-  testnet: false,
 })
