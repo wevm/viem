41996567d7c97e6eaf10258318173e7cda4d3888
Arthur <0xa1337@proton.me>
2024-06-17 01:57:06 +0200

    feat(chains): update berachain testnet

--- src/chains/definitions/berachainTestnet.ts
+++ src/chains/definitions/berachainTestnet.ts
@@ -1,20 +1,20 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const berachainTestnet = /*#__PURE__*/ defineChain({
-  id: 80085,
-  name: 'Berachain Artio',
+  id: 80084,
+  name: 'Berachain bArtio',
   nativeCurrency: {
     decimals: 18,
     name: 'BERA Token',
     symbol: 'BERA',
   },
   rpcUrls: {
-    default: { http: ['https://artio.rpc.berachain.com'] },
+    default: { http: ['https://bartio.rpc.berachain.com'] },
   },
   blockExplorers: {
     default: {
-      name: 'Berachain',
-      url: 'https://artio.beratrail.io',
+      name: 'Beratrail',
+      url: 'https://bartio.beratrail.io',
     },
   },
   testnet: true,
