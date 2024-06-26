838aa032bdcc1df861a779e601a8ff1aad6aef9f
jxom <j@wevm.dev>
2024-06-26 15:00:27 +0100

    Update bitTorrent.ts

--- src/chains/definitions/bitTorrent.ts
+++ src/chains/definitions/bitTorrent.ts
@@ -18,7 +18,7 @@ export const bitTorrent = /*#__PURE__*/ defineChain({
   },
   contracts: {
     multicall3: {
-      address: "0xcA11bde05977b3631167028862bE2a173976CA11"
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
       blockCreated: 31078552,
     }
   },
