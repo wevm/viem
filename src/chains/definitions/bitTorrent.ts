40f9d80b791bd9187191cb792b67a18fa484db29
jxom <jxom@users.noreply.github.com>
2024-06-26 14:05:10 +0000

    chore: format

--- src/chains/definitions/bitTorrent.ts
+++ src/chains/definitions/bitTorrent.ts
@@ -18,8 +18,8 @@ export const bitTorrent = /*#__PURE__*/ defineChain({
   },
   contracts: {
     multicall3: {
-      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 31078552,
-    }
+    },
   },
 })
