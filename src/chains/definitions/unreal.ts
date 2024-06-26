9143dc9b53d7c62976fd5e314143a666ddaf1de1
jxom <jxom@users.noreply.github.com>
2024-06-26 14:08:42 +0000

    chore: format

--- src/chains/definitions/unreal.ts
+++ src/chains/definitions/unreal.ts
@@ -1,4 +1,4 @@
-import { defineChain } from '../../utils/chain/defineChain.js';
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const unreal = /*#__PURE__*/ defineChain({
   id: 18233,
@@ -25,4 +25,4 @@ export const unreal = /*#__PURE__*/ defineChain({
       blockCreated: 1745,
     },
   },
-});
+})
