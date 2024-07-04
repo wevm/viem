729bff8e066540cce570286ebe4fff4992a2796d
tinystone <jinshi.tinystone@gmail.com>
2024-07-04 13:42:37 +0800

    feat: Added zkLinkNova chain

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -282,6 +282,7 @@ export { zkSyncTestnet } from './definitions/zkSyncTestnet.js'
 export { zora } from './definitions/zora.js'
 export { zoraSepolia } from './definitions/zoraSepolia.js'
 export { zoraTestnet } from './definitions/zoraTestnet.js'
+export { zkLinkNova } from './definitions/zkLinkNova.js'
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Required type exports to prevent TypeScript error "TS2742".
