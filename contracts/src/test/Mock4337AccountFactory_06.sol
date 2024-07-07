39e9e65e74b17efa5bf1cdc8fc9aaa6c00d39442
jxom <jakemoxey@gmail.com>
2024-07-07 10:56:17 +1000

    wip

--- contracts/src/test/Mock4337AccountFactory_06.sol
+++ contracts/src/test/Mock4337AccountFactory_06.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import {ERC4337Factory} from "solady-0.6/accounts/ERC4337Factory.sol";
+import {ERC4337Factory} from "solady-dc09481/accounts/ERC4337Factory.sol";
 
 contract Mock4337AccountFactory06 is ERC4337Factory {
     constructor(address erc4337) ERC4337Factory(erc4337) {}
--- contracts/src/test/Mock4337Account_06.sol
+++ contracts/src/test/Mock4337Account_06.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import {ERC4337} from "solady-0.6/accounts/ERC4337.sol";
+import {ERC4337} from "solady-dc09481/accounts/ERC4337.sol";
 
 contract Mock4337Account06 is ERC4337 {
     function _domainNameAndVersion()
