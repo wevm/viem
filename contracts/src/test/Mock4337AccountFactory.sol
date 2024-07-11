b7846e223a81dc8151a75d9d97e311279e910bb0
jxom <jakemoxey@gmail.com>
2024-07-12 07:36:15 +1000

    chore: rebase

--- contracts/src/test/Mock4337AccountFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
-
-import {ERC4337Factory} from "solady/accounts/ERC4337Factory.sol";
-
-contract Mock4337AccountFactory07 is ERC4337Factory {
-    constructor(address erc4337) ERC4337Factory(erc4337) {}
-}
