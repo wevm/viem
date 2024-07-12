ea956f2c23f5578a909c695e3d8862ce9b022b82
billalxcode <billal.xcode@gmail.com>
2024-07-12 17:08:04 +0700

    feat(dreyerx): add dreyerx mainnet to defined chain

---
+++ src/chains/definitions/dreyerxTestnet.ts
@@ -0,0 +1,22 @@
+import { defineChain } from '../utils.js'
+
+export const dreyerxTestnet = defineChain({
+  id: 23452,
+  name: 'DreyerX Testnet',
+  nativeCurrency: {
+    name: 'DreyerX',
+    symbol: 'DRX',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://testnet-rpc.dreyerx.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'DreyerX Testnet Scan',
+      url: 'https://testnet-scan.dreyerx.com',
+    },
+  },
+})
