c0811052aa3fc346de4ea827d07951048bd51cca
wantedsystem <amine.harty@consensys.net>
2024-07-23 12:01:57 +0200

    feat: Add support for linea_estimateGas

---
+++ src/linea/chains.ts
@@ -0,0 +1,3 @@
+// biome-ignore lint/performance/noBarrelFile: entrypoint module
+export { linea } from '../chains/definitions/linea.js'
+export { lineaTestnet } from '../chains/definitions/lineaTestnet.js'
---
+++ src/linea/index.ts
@@ -0,0 +1,5 @@
+// biome-ignore lint/performance/noBarrelFile: entrypoint module
+export { lineaEstimateGas } from './actions/lineaEstimateGas.js'
+export { lineaEstimateFeesPerGas } from './actions/lineaEstimateFeesPerGas.js'
+
+export { linea, lineaTestnet } from './chains.js'
