8f3a2fd10e92a6c630477407c128294efa4c0a25
jonodavis <jono.davis66@gmail.com>
2024-07-24 10:11:17 +1200

    upd: add ens contracts to root and porciniroot chains

--- src/chains/definitions/root.ts
+++ src/chains/definitions/root.ts
@@ -25,5 +25,10 @@ export const root = /*#__PURE__*/ defineChain({
       address: '0xc9C2E2429AeC354916c476B30d729deDdC94988d',
       blockCreated: 9218338,
     },
+    ensRegistry: { address: '0xEC58C26B8E0A4bc0fe1ad21D216e4ecAd9e037A8' },
+    ensUniversalResolver: {
+      address: '0x7808dF0A1F1d58c6Ad0F3bA07E749D730F02f13A',
+      blockCreated: 12_519_044,
+    },
   },
 })
--- src/chains/definitions/rootPorcini.ts
+++ src/chains/definitions/rootPorcini.ts
@@ -25,6 +25,11 @@ export const rootPorcini = /*#__PURE__*/ defineChain({
       address: '0xc9C2E2429AeC354916c476B30d729deDdC94988d',
       blockCreated: 10555692,
     },
+    ensRegistry: { address: '0xA931c1F9621ECa562c258B81bF9fA8401f12241B' },
+    ensUniversalResolver: {
+      address: '0xB3c0AE882b35E72B7b84F7A1E0cF01fBDC617170',
+      blockCreated: 11_983_898,
+    },
   },
   testnet: true,
 })
