ca4633eb44e38212b837dbb73030273685e15664
Jonathan <jonathangus@proton.me>
2024-07-29 11:12:27 +0200

    added Abstract Testnet chain

---
+++ src/chains/definitions/abstractTestnet.ts
@@ -0,0 +1,21 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const abstractTestnet = /*#__PURE__*/ defineChain({
+  id: 11_124,
+  name: 'Abstract Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: { http: ['https://api.testnet.abs.xyz'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Abstract Block Explorer',
+      url: 'https://explorer.testnet.abs.xyz',
+    },
+  },
+  testnet: true,
+})
