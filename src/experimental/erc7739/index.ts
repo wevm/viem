6766ef82351802076d320778cedb367db35cef42
jxom <jakemoxey@gmail.com>
2024-08-07 12:01:05 +1000

    chore: up

--- src/experimental/erc7739/index.ts
+++ src/experimental/erc7739/index.ts
@@ -13,14 +13,8 @@ export {
 } from './actions/signTypedData.js'
 export {
   type Erc7739Actions,
-  /** @deprecated Use `Erc7739Actions` instead. */
-  type Erc7739Actions as SoladyActions,
   type Erc7739ActionsParameters,
-  /** @deprecated Use `erc7739Actions` instead. */
-  type Erc7739ActionsParameters as SoladyActionsParameters,
   erc7739Actions,
-  /** @deprecated Use `erc7739Actions` instead. */
-  erc7739Actions as soladyActions,
 } from './decorators/erc7739.js'
 export {
   type HashMessageErrorType,
