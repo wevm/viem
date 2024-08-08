f1dda726aa59e48a5d65b3cb98e8572e30b99b94
jxom <jakemoxey@gmail.com>
2024-08-09 09:25:52 +1000

    chore: tweak

--- src/zksync/accounts/toSmartAccount.ts
+++ src/zksync/accounts/toSmartAccount.ts
@@ -4,11 +4,12 @@ import { toAccount } from '../../accounts/toAccount.js'
 import type { LocalAccount } from '../../accounts/types.js'
 import type { ErrorType } from '../../errors/utils.js'
 import { concatHex } from '../../utils/data/concat.js'
-import { keccak256 } from '../../utils/hash/keccak256.js'
 import { hashMessage } from '../../utils/signature/hashMessage.js'
 import { hashTypedData } from '../../utils/signature/hashTypedData.js'
-import { serializeTransaction } from '../../utils/transaction/serializeTransaction.js'
 import type { ZksyncSmartAccount } from '../types/account.js'
+import { serializeTransaction } from '../serializers.js'
+import { keccak256 } from '../../utils/index.js'
+import type { ZksyncTransactionSerializableEIP712 } from '../types/transaction.js'
 
 export type ToSmartAccountParameters = {
   /** Address of the deployed Account's Contract implementation. */
@@ -41,13 +42,17 @@ export function toSmartAccount(
         hash: hashMessage(message),
       })
     },
-    async signTransaction(transaction, { serializer } = {}) {
-      return this.sign!({
-        hash: keccak256(
-          serializer
-            ? serializer(transaction)
-            : serializeTransaction(transaction),
-        ),
+    async signTransaction(transaction) {
+      const signableTransaction = {
+        ...transaction,
+        from: this.address!,
+      } as ZksyncTransactionSerializableEIP712
+
+      return serializeTransaction({
+        ...signableTransaction,
+        customSignature: await this.sign!({
+          hash: keccak256(serializeTransaction(signableTransaction)),
+        }),
       })
     },
     async signTypedData(typedData) {
