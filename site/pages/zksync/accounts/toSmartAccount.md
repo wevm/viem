b05a81f3d2093815503356fe585b27500ff989fb
jxom <jakemoxey@gmail.com>
2024-08-09 09:05:10 +1000

    feat: zksync `toSmartAccount`

---
+++ site/pages/zksync/accounts/toSmartAccount.md
@@ -0,0 +1,56 @@
+---
+description: Creates a ZKsync Smart Account
+---
+
+# toSmartAccount (ZKsync)
+
+Creates a [ZKsync Smart Account](https://docs.zksync.io/build/developer-reference/account-abstraction/building-smart-accounts) from a Contract Address and an array of owner(s).
+
+## Usage
+
+```ts twoslash
+import { privateKeyToAccount } from 'viem/accounts'
+import { toSmartAccount } from 'viem/zksync'
+
+const account = toSmartAccount({
+  address: '0xf39Fd6e51aad8F6F4ce6aB8827279cffFb92266', 
+  owners: [
+    privateKeyToAccount('0x...'), 
+    privateKeyToAccount('0x...')
+  ]
+})
+```
+
+## Parameters
+
+### address
+
+- **Type:** `Hex`
+
+Address of the deployed multisig Contract.
+
+```ts
+const account = toSmartAccount({
+  address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', // [!code focus]
+  owners: [
+    privateKeyToAccount('0x...'), 
+    privateKeyToAccount('0x...')
+  ]
+})
+```
+
+### privateKeys
+
+- **Type:** `LocalAccount[]`
+
+Owners of the Smart Account. Must be a set of [Local Accounts](/docs/accounts/local) (Private Key, Mnemonic, etc).
+
+```ts
+const account = toSmartAccount({
+  address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 
+  owners: [ // [!code focus]
+    privateKeyToAccount('0x...'), // [!code focus]
+    privateKeyToAccount('0x...') // [!code focus]
+  ] // [!code focus]
+})
+```
---
+++ src/zksync/accounts/toSmartAccount.ts
@@ -0,0 +1,62 @@
+import type { Address } from 'abitype'
+
+import type { ErrorType } from '../../errors/utils.js'
+import { toAccount } from '../../accounts/toAccount.js'
+import { concatHex } from '../../utils/data/concat.js'
+import { keccak256 } from '../../utils/hash/keccak256.js'
+import { serializeTransaction } from '../../utils/transaction/serializeTransaction.js'
+import { hashMessage } from '../../utils/signature/hashMessage.js'
+import { hashTypedData } from '../../utils/signature/hashTypedData.js'
+import type { LocalAccount } from '../../accounts/types.js'
+import type { ZksyncSmartAccount } from '../types/account.js'
+
+export type ToSmartAccountParameters = {
+  address: Address
+  owners: readonly LocalAccount[]
+}
+
+export type ToSmartAccountErrorType = ErrorType
+
+/**
+ * @description Creates an Account from private keys.
+ *
+ * @returns A Private Key Account.
+ */
+export function toSmartAccount(
+  parameters: ToSmartAccountParameters,
+): ZksyncSmartAccount {
+  const { address, owners } = parameters
+
+  const account = toAccount({
+    address,
+    async sign({ hash }) {
+      return concatHex(
+        await Promise.all(owners.map((owner) => owner.sign!({ hash }))),
+      )
+    },
+    async signMessage({ message }) {
+      return this.sign!({
+        hash: hashMessage(message),
+      })
+    },
+    async signTransaction(transaction, { serializer } = {}) {
+      return this.sign!({
+        hash: keccak256(
+          serializer
+            ? serializer(transaction)
+            : serializeTransaction(transaction),
+        ),
+      })
+    },
+    async signTypedData(typedData) {
+      return this.sign!({
+        hash: hashTypedData(typedData),
+      })
+    },
+  })
+
+  return {
+    ...account,
+    source: 'smartAccountZksync',
+  } as ZksyncSmartAccount
+}
