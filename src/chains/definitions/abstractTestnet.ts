72ab4224cddf0048be1fbe6412c8777797202463
cygaar <tmpftl1@gmail.com>
2024-09-03 14:47:44 -0400

    Use custom EIP712 domain for Abstract

--- src/chains/definitions/abstractTestnet.ts
+++ src/chains/definitions/abstractTestnet.ts
@@ -1,5 +1,51 @@
+import { transactionToMessage } from '../..//zksync/utils/getEip712Domain.js'
 import { defineChain } from '../../utils/chain/defineChain.js'
 import { chainConfig } from '../../zksync/chainConfig.js'
+import type { EIP712DomainFn } from '../../zksync/types/eip712.js'
+import type {
+  ZksyncEIP712TransactionSignable,
+  ZksyncTransactionSerializable,
+  ZksyncTransactionSerializableEIP712,
+} from '../../zksync/types/transaction.js'
+import { assertEip712Transaction } from '../../zksync/utils/assertEip712Transaction.js'
+
+export const getAbstractEip712Domain: EIP712DomainFn<
+  ZksyncTransactionSerializable,
+  ZksyncEIP712TransactionSignable
+> = (transaction) => {
+  assertEip712Transaction(transaction)
+
+  const message = transactionToMessage(
+    transaction as ZksyncTransactionSerializableEIP712,
+  )
+
+  return {
+    domain: {
+      name: 'Abstract',
+      version: '2',
+      chainId: transaction.chainId,
+    },
+    types: {
+      Transaction: [
+        { name: 'txType', type: 'uint256' },
+        { name: 'from', type: 'uint256' },
+        { name: 'to', type: 'uint256' },
+        { name: 'gasLimit', type: 'uint256' },
+        { name: 'gasPerPubdataByteLimit', type: 'uint256' },
+        { name: 'maxFeePerGas', type: 'uint256' },
+        { name: 'maxPriorityFeePerGas', type: 'uint256' },
+        { name: 'paymaster', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'value', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'factoryDeps', type: 'bytes32[]' },
+        { name: 'paymasterInput', type: 'bytes' },
+      ],
+    },
+    primaryType: 'Transaction',
+    message: message,
+  }
+}
 
 export const abstractTestnet = /*#__PURE__*/ defineChain({
   ...chainConfig,
@@ -20,4 +66,7 @@ export const abstractTestnet = /*#__PURE__*/ defineChain({
     },
   },
   testnet: true,
+  custom: {
+    getAbstractEip712Domain, // Use Abstract's specific EIP712 domain
+  },
 })
