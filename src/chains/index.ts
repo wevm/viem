a541519c12bdd7fb91e3534cc3a9c576a33b8acb
Tom Haile <bradford.haile@gmail.com>
2024-09-04 01:28:43 -0500

    chore: deprecate Flow Previewnet (#2672)
    * remove previewnet
    
    * add changeset
    
    * Update src/chains/index.ts
    
    Co-authored-by: jxom  <jakemoxey@gmail.com>
    
    * Update src/chains/index.ts
    
    Co-authored-by: jxom  <jakemoxey@gmail.com>
    
    * restore the file, needs to be deprecated before removed in major release
    
    * linting
    
    * Update pink-donuts-agree.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -108,6 +108,7 @@ export { filecoinCalibration } from './definitions/filecoinCalibration.js'
 export { filecoinHyperspace } from './definitions/filecoinHyperspace.js'
 export { flare } from './definitions/flare.js'
 export { flareTestnet } from './definitions/flareTestnet.js'
+/** @deprecated */
 export { flowPreviewnet } from './definitions/flowPreviewnet.js'
 export { flowMainnet } from './definitions/flowMainnet.js'
 export { flowTestnet } from './definitions/flowTestnet.js'
