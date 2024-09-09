013be64ea8f9e4d6caa023f770deb3965867ea15
0x-jj <jj@0x.email>
2024-09-09 16:40:34 +0100

    Update Shape Sepolia

--- src/chains/definitions/shapeSepolia.ts
+++ src/chains/definitions/shapeSepolia.ts
@@ -1,6 +1,10 @@
+import { chainConfig } from '../../op-stack/chainConfig.js'
 import { defineChain } from '../../utils/chain/defineChain.js'
 
+const sourceId = 11_155_111 // sepolia
+
 export const shapeSepolia = /*#__PURE__*/ defineChain({
+  ...chainConfig,
   id: 11_011,
   name: 'Shape Sepolia Testnet',
   nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
@@ -12,8 +16,32 @@ export const shapeSepolia = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: 'blockscout',
-      url: 'https://shape-sepolia-explorer.alchemy.com',
-      apiUrl: 'https://shape-sepolia-explorer.alchemy.com/api/v2',
+      url: 'https://explorer-sepolia.shape.network/',
+      apiUrl: 'https://explorer-sepolia.shape.network/api/v2',
     },
   },
+  contracts: {
+    ...chainConfig.contracts,
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 1,
+    },
+    portal: {
+      [sourceId]: {
+        address: '0xeffE2C6cA9Ab797D418f0D91eA60807713f3536f',
+      },
+    },
+    l1StandardBridge: {
+      [sourceId]: {
+        address: '0x5376f1D543dcbB5BD416c56C189e4cB7399fCcCB',
+      },
+    },
+    l2OutputOracle: {
+      [sourceId]: {
+        address: '0x84457ca9D0163FbC4bbfe4Dfbb20ba46e48DF254',
+      },
+    },
+  },
+  testnet: true,
+  sourceId,
 })
