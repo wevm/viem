c2b2c7887eb27ffe1e1db534036a0bb7ef3cad5c
cygaar <97691933+cygaar@users.noreply.github.com>
2024-09-15 02:51:37 -0400

    feat: revert Abstract EIP712 domain changes (#2724)
    * Revert Abstract EIP712 domain changes
    
    * Update
    
    * Create cyan-owls-compete.md
    
    ---------
    
    Co-authored-by: jxom <j@wevm.dev>

--- src/zksync/utils/getEip712Domain.ts
+++ src/zksync/utils/getEip712Domain.ts
@@ -50,7 +50,7 @@ export const getEip712Domain: EIP712DomainFn<
 //////////////////////////////////////////////////////////////////////////////
 // Utilities
 
-export function transactionToMessage(
+function transactionToMessage(
   transaction: ZksyncTransactionSerializableEIP712,
 ): ZksyncEIP712TransactionSignable {
   const {
