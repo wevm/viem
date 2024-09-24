ffab24b81da7e5f02a151f00e021a4c77bea4e01
j6i <43463199+j6i@users.noreply.github.com>
2024-09-23 18:53:22 -0700

    add Shape chain

---
+++ src/chains/definitions/shape.ts
@@ -0,0 +1,24 @@
+import { chainConfig } from '../../op-stack/chainConfig.js'
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+const sourceId = 1 // mainnet
+
+export const shape = /*#__PURE__*/ defineChain({
+  ...chainConfig,
+  id: 360,
+  name: 'Shape',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://internal-shaper-explorer.alchemypreview.com'],
+    },
+  },
+  contracts: {
+    ...chainConfig.contracts,
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 1,
+    },
+  },
+  sourceId,
+})
