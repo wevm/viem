d3a16e47b5aaf71c91720c4dd22dce918d300633
Svg70 <collectionpiter@gmail.com>
2024-09-25 13:47:39 +0200

    Added Unique chains

---
+++ src/chains/definitions/uniqueOpal.ts
@@ -0,0 +1,22 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const uniqueOpal = /*#__PURE__*/ defineChain({
+  id: 8882,
+  name: 'Opal Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'OPL',
+    symbol: 'OPL',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc-opal.unique.network'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Opal Subscan',
+      url: 'https://opal.subscan.io/',
+    },
+  },
+  chainNamespace: 'eip155',
+  testnet: true,
+})
