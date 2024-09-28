84fdac29020dbbb084d27ce61689e6770a19ee5c
peterhappynow <peter@clica.tech>
2024-09-28 20:17:56 +0800

    Update victionTestnet's contracts

--- src/chains/definitions/victionTestnet.ts
+++ src/chains/definitions/victionTestnet.ts
@@ -15,5 +15,11 @@ export const victionTestnet = /*#__PURE__*/ defineChain({
       url: 'https://testnet.vicscan.xyz',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 12170179,
+    },
+  },
   testnet: true,
 })
