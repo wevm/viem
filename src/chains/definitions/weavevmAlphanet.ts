e41d5894323f1c8bcc1eb996ab095fd99e8d22c4
jxom <j@wevm.dev>
2024-09-28 19:29:06 +1000

    Update weavevmAlphanet.ts

--- src/chains/definitions/weavevmAlphanet.ts
+++ src/chains/definitions/weavevmAlphanet.ts
@@ -3,7 +3,6 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const weaveVMAlphanet = /*#__PURE__*/ defineChain({
   id: 9496,
   name: 'WeaveVM Alphanet',
-  network: 'weavevm-alphanet',
   nativeCurrency: { name: 'Testnet WeaveVM', symbol: 'tWVM', decimals: 18 },
   rpcUrls: {
     default: { http: ['https://testnet-rpc.wvm.dev'] },
@@ -12,12 +11,6 @@ export const weaveVMAlphanet = /*#__PURE__*/ defineChain({
     default: {
       name: 'WeaveVM Alphanet Explorer',
       url: 'https://explorer.wvm.dev',
-    },
-  },
-  contracts: {
-    multicall3: {
-      address: '0x7c6db9c77dCE482dFCb315cD5B9B505702651d26',
-      blockCreated: 1820864,
     },
   },
   testnet: true,
