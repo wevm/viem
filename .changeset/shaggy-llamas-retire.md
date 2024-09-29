5d0fe26b4da6e2a19868e01411f9f4612e71f4bc
zzq0826 <sandy@clica.tech>
2024-09-29 13:24:34 +0800

    feat: add multicall3 for Bitlayer

---
+++ .changeset/shaggy-llamas-retire.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Added contracts's multicall3 for Bitlayer chain.
--- src/chains/definitions/btr.ts
+++ src/chains/definitions/btr.ts
@@ -24,4 +24,10 @@ export const btr = /*#__PURE__*/ defineChain({
       url: 'https://www.btrscan.com',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 3225645,
+    },
+  },
 })
