3762edee66f79bf438abe84649898d3b0b445eda
jxom <jakemoxey@gmail.com>
2024-10-10 07:06:18 +1100

    chore: up

--- environments/next/src/app/page.tsx
+++ environments/next/src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const webSocketClient = createPublicClient({
     chain: mainnet,
     transport: webSocket(
-      'wss://eth-mainnet.g.alchemy.com/v2/4iIl6mDHqX3GFrpzmfj2Soirf3MPoAcH',
+      'wss://eth-mainnet.g.alchemy.com/v2/WV-bLot1hKjjCfpPq603Ro-jViFzwYX8',
     ),
   })
 
