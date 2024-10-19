9a60ee364134eddce887bfd6cbedf1b2aebecea5
jxom <jxom@users.noreply.github.com>
2024-10-19 04:32:23 +0000

    chore: format

--- src/chains/definitions/tiktrixTestnet.ts
+++ src/chains/definitions/tiktrixTestnet.ts
@@ -1,23 +1,23 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const tiktrixTestnet = /*#__PURE__*/ defineChain({
   id: 62092,
-  name: "TikTrix Testnet",
+  name: 'TikTrix Testnet',
   nativeCurrency: {
-    name: "tTTX",
-    symbol: "tTTX",
+    name: 'tTTX',
+    symbol: 'tTTX',
     decimals: 18,
   },
   rpcUrls: {
     default: {
-      http: ["https://tiktrix-rpc.xyz"],
+      http: ['https://tiktrix-rpc.xyz'],
     },
   },
   blockExplorers: {
     default: {
-      name: "TikTrix Testnet Explorer",
-      url: "https://tiktrix.xyz",
+      name: 'TikTrix Testnet Explorer',
+      url: 'https://tiktrix.xyz',
     },
   },
   testnet: true,
-});
+})
