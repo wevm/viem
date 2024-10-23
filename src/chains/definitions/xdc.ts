5e4fef8ce2e8710dc1b710a4e81fcc9cc25a2933
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-10-24 00:29:36 +0400

    feat: XDC Chain update (#2910)
    * Update xdc.ts
    
    * Update xdc.ts
    
    ---------
    
    Co-authored-by: jxom <j@wevm.dev>

--- src/chains/definitions/xdc.ts
+++ src/chains/definitions/xdc.ts
@@ -2,25 +2,25 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const xdc = /*#__PURE__*/ defineChain({
   id: 50,
-  name: 'XinFin Network',
+  name: "XinFin Network",
   nativeCurrency: {
     decimals: 18,
-    name: 'XDC',
-    symbol: 'XDC',
+    name: "XDC",
+    symbol: "XDC",
   },
   rpcUrls: {
-    default: { http: ['https://rpc.xinfin.network'] },
+    default: { http: ["https://rpc.xdcrpc.com"] },
   },
   blockExplorers: {
     default: {
-      name: 'Blocksscan',
-      url: 'https://xdc.blocksscan.io',
+      name: "XDCScan",
+      url: "https://xdcscan.com",
     },
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 71542788,
+      address: "0x0B1795ccA8E4eC4df02346a082df54D437F8D9aF",
+      blockCreated: 75884020,
     },
   },
 })
