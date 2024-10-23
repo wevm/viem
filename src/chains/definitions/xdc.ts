d3ea27932ce9ff0134a4c5f2421a1f141d3fd970
jxom <jxom@users.noreply.github.com>
2024-10-23 20:30:58 +0000

    chore: format

--- src/chains/definitions/xdc.ts
+++ src/chains/definitions/xdc.ts
@@ -2,24 +2,24 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const xdc = /*#__PURE__*/ defineChain({
   id: 50,
-  name: "XinFin Network",
+  name: 'XinFin Network',
   nativeCurrency: {
     decimals: 18,
-    name: "XDC",
-    symbol: "XDC",
+    name: 'XDC',
+    symbol: 'XDC',
   },
   rpcUrls: {
-    default: { http: ["https://rpc.xdcrpc.com"] },
+    default: { http: ['https://rpc.xdcrpc.com'] },
   },
   blockExplorers: {
     default: {
-      name: "XDCScan",
-      url: "https://xdcscan.com",
+      name: 'XDCScan',
+      url: 'https://xdcscan.com',
     },
   },
   contracts: {
     multicall3: {
-      address: "0x0B1795ccA8E4eC4df02346a082df54D437F8D9aF",
+      address: '0x0B1795ccA8E4eC4df02346a082df54D437F8D9aF',
       blockCreated: 75884020,
     },
   },
