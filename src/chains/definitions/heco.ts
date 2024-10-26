c995183cd82793859bf8ffbca6c19ef546f6c585
Peng Ke <pk-850@users.noreply.github.com>
2024-10-26 21:53:45 +0800

    feat: add heco chain

---
+++ src/chains/definitions/heco.ts
@@ -0,0 +1,20 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const heco = /*#__PURE__*/ defineChain({
+  id: 128,
+  name: 'Huobi ECO Chain Mainnet',
+  nativeCurrency: { name: 'HT', symbol: 'HT', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-1.zyx.network'],
+      webSocket: ['wss://ws-mainnet.hecochain.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Heco Scan',
+      url: 'https://hecoscan.com',
+    },
+  },
+  testnet: false,
+})
