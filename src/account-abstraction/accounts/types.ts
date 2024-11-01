4de3e658d2505be4d28c4064d400409664c23096
Sahil Vasava <sahilvasava@gmail.com>
2024-11-01 14:40:01 +0530

    feat: add optional param UserOperationRequest to getStubSignature (#2952)
    * feat: Add optional param UserOperationRequest to getStubSignature
    
    * Update types.ts
    
    ---------
    
    Co-authored-by: jxom <j@wevm.dev>

--- src/account-abstraction/accounts/types.ts
+++ src/account-abstraction/accounts/types.ts
@@ -112,7 +112,7 @@ export type SmartAccountImplementation<
    * // '0x...'
    * ```
    */
-  getStubSignature: () => Promise<Hex>
+  getStubSignature: (parameters?: UserOperationRequest | undefined) => Promise<Hex>
   /** Custom nonce key manager. */
   nonceKeyManager?: NonceManager | undefined
   /**
