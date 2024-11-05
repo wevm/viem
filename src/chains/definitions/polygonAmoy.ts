c8842c000c1ff85b216bf446116cc7b20fdcabcb
roylines <roy@roylines.co.uk>
2024-11-05 15:07:52 +0000

    upgrade from MATIC to POL on polygon amoy testnet

--- src/chains/definitions/polygonAmoy.ts
+++ src/chains/definitions/polygonAmoy.ts
@@ -3,7 +3,7 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const polygonAmoy = /*#__PURE__*/ defineChain({
   id: 80_002,
   name: 'Polygon Amoy',
-  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  nativeCurrency: { name: 'POL', symbol: 'POL', decimals: 18 },
   rpcUrls: {
     default: {
       http: ['https://rpc-amoy.polygon.technology'],
