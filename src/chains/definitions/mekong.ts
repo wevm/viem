ec28e0819a43d55e9fbe24b249a3c2b67cf2f5b0
Vincent Almeida <vince0656@googlemail.com>
2024-11-07 16:56:39 +0000

    Update mekong.ts

--- src/chains/definitions/mekong.ts
+++ src/chains/definitions/mekong.ts
@@ -1,6 +1,6 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const coinex = /*#__PURE__*/ defineChain({
+export const mekong = /*#__PURE__*/ defineChain({
   id: 7078815900,
   name: 'Mekong Pectra Devnet',
   nativeCurrency: { name: 'eth', symbol: 'eth', decimals: 18 },
