c5ba0283d02f8e584b7a291695753b32b1874fc8
Andy <andychert@protonmail.com>
2024-11-16 03:18:00 +0200

    feat: add Multicall3 address (#3010)
    * Add Multicall3 address
    
    Multicall3: https://hekla.taikoscan.io/address/0xca11bde05977b3631167028862be2a173976ca11
    
    * Create khaki-steaks-beam.md
    
    ---------
    
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

--- src/chains/definitions/taikoHekla.ts
+++ src/chains/definitions/taikoHekla.ts
@@ -15,5 +15,11 @@ export const taikoHekla = /*#__PURE__*/ defineChain({
       url: 'https://hekla.taikoscan.network',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 59757,
+    },
+  },
   testnet: true,
 })
