246cee02ef9552d762b97961a341bedd1ed3dbd7
Ayushi5ire <ayushi@5ire.org>
2024-11-19 14:05:40 +0530

    5ireChain mainnet
    Signed-off-by: Ayushi5ire <ayushi@5ire.org>

--- src/chains/definitions/5ireChain.ts
+++ src/chains/definitions/5ireChain.ts
@@ -1,19 +1,19 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const thunderTestnet = /*#__PURE__*/ defineChain({
+export const fireChain = /*#__PURE__*/ defineChain({
   id: 997,
-  name: '5ireChain Thunder Testnet',
+  name: '5ireChain',
   nativeCurrency: { name: '5ire Token', symbol: '5IRE', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://rpc.testnet.5ire.network'],
+      http: ['https://rpc.5ire.network'],
     },
   },
   blockExplorers: {
     default: {
       name: '5ireChain Thunder Explorer',
-      url: 'https://testnet.5irescan.io/',
+      url: 'https://5irescan.io/',
     },
   },
-  testnet: true,
+  testnet: false,
 })
