a598b5a80167609ca0b1f6597fe6ca9fd0ca47fe
Silver Poker King <ye@clica.tech>
2024-11-21 10:53:54 +0800

    feat: add Multicall address to Bitgert (#3035)

--- src/chains/definitions/bitgert.ts
+++ src/chains/definitions/bitgert.ts
@@ -17,5 +17,11 @@ export const bitgert = /*#__PURE__*/ defineChain({
       url: 'https://brisescan.com',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 2118034,
+    },
+  },
   testnet: false,
 })
