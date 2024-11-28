0f45789c5b208e95ae2df5d064b3c5b7b70edd7a
Zhangnong <zhangnong001@hotmail.com>
2024-11-28 22:58:13 +0800

    chore: add Multicall address to Fluence

--- src/chains/definitions/fluenceStage.ts
+++ src/chains/definitions/fluenceStage.ts
@@ -17,5 +17,11 @@ export const fluenceStage = /*#__PURE__*/ defineChain({
       apiUrl: 'https://blockscout.stage.fluence.dev/api',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 83227,
+    },
+  },
   testnet: true,
 })
