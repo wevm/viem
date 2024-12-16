aca3e1d75d979aa50f5737a2fabaf0088b160f46
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-12-16 19:19:02 +0100

    feat: add multicall to sonic (#3131)
    * sonic multicall3
    
    * Create clean-papayas-speak.md
    
    ---------
    
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

--- src/chains/definitions/sonic.ts
+++ src/chains/definitions/sonic.ts
@@ -14,7 +14,13 @@ export const sonic = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: 'Sonic Explorer',
-      url: 'https://explorer.soniclabs.com',
+      url: 'https://sonicscan.org/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 60,
     },
   },
   testnet: false,
