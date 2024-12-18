acc297ea51ea3c4d51b2daa68dd7ae7fd23c6805
Jordan Ribbink <jribbink@telus.net>
2024-12-17 22:39:37 -0800

    Update Flow EVM chain definitions

--- src/chains/definitions/flowTestnet.ts
+++ src/chains/definitions/flowTestnet.ts
@@ -2,7 +2,7 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const flowTestnet = /*#__PURE__*/ defineChain({
   id: 545,
-  name: 'FlowEVM Testnet',
+  name: 'Flow EVM Testnet',
   nativeCurrency: {
     decimals: 18,
     name: 'Flow',
