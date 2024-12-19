f5b0bfd7b7cca6bb53311f8532897616fa1a6bea
Phillip-Kemper <phillip-kemper@gmx.net>
2024-12-19 16:13:24 +0100

    add zircuit and zircuit testnet addresses

---
+++ .changeset/pink-ravens-hope.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+updating zircuit and zircuit testnet to also contain the l1 contract addresses
--- src/chains/definitions/zircuitTestnet.ts
+++ src/chains/definitions/zircuitTestnet.ts
@@ -1,5 +1,7 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
+const sourceId = 11_155_111 // sepolia
+
 export const zircuitTestnet = /*#__PURE__*/ defineChain({
   id: 48899,
   name: 'Zircuit Testnet',
@@ -23,6 +25,21 @@ export const zircuitTestnet = /*#__PURE__*/ defineChain({
       address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 6040287,
     },
+    l2OutputOracle: {
+      [sourceId]: {
+        address: '0x740C2dac453aEf7140809F80b72bf0e647af8148',
+      }
+    },
+    portal: {
+      [sourceId]: {
+        address: '0x787f1C8c5924178689E0560a43D848bF8E54b23e',
+      }
+    },
+    l1StandardBridge: {
+      [sourceId]: {
+        address: '0x0545c5fe980098C16fcD0eCB5E79753afa6d9af9',
+      }
+    }
   },
   testnet: true,
 })
