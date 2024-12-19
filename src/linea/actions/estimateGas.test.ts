f30597f35a4314d47ac88b3f56f88d7b499795ac
jxom <7336481+jxom@users.noreply.github.com>
2024-12-19 20:26:33 +0200

    test: up linea

--- src/linea/actions/estimateGas.test.ts
+++ src/linea/actions/estimateGas.test.ts
@@ -33,24 +33,7 @@ test('error: insufficient balance', async () => {
       to: '0x0000000000000000000000000000000000000000',
       value: parseEther('0.0001'),
     }),
-  ).rejects.toThrowErrorMatchingInlineSnapshot(`
-    [CallExecutionError: The total cost (gas * gas fee + value) of executing this transaction exceeds the balance of the account.
-
-    This error could arise when the account does not have enough funds to:
-     - pay for the total gas fee,
-     - pay for the value to send.
-     
-    The cost of the transaction is calculated as \`gas * gas fee + value\`, where:
-     - \`gas\` is the amount of gas needed for transaction to execute,
-     - \`gas fee\` is the gas fee,
-     - \`value\` is the amount of ether to send to the recipient.
-     
-    Raw Call Arguments:
-      from:   0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-      to:     0x0000000000000000000000000000000000000000
-      value:  0.0001 ETH
-
-    Details: transaction up-front cost 0x5af31a7dba00 exceeds transaction sender account balance 0x0
-    Version: viem@x.y.z]
-  `)
+  ).rejects.toThrowError(
+    'transaction up-front cost 0x5af31a7dba00 exceeds transaction sender',
+  )
 })
