fb8b76fde1cfd9dc61722b84bdc34e997c2dda43
danijelTxFusion <danijel@txfusion.io>
2024-12-22 21:20:46 +0100

    fix(zksync): fix account hoisting for `signEip712Transaction` and `sendEip712Transaction`

--- test/src/zksync.ts
+++ test/src/zksync.ts
@@ -1,7 +1,7 @@
 import { zksyncLocalNode } from '~viem/chains/index.js'
 import { createClient } from '~viem/clients/createClient.js'
 import { http } from '~viem/index.js'
-import { accounts } from './constants.js'
+import { accounts as acc } from './constants.js'
 
 export const zksyncClientLocalNode = createClient({
   chain: zksyncLocalNode,
@@ -9,7 +9,7 @@ export const zksyncClientLocalNode = createClient({
 })
 
 export const zksyncClientLocalNodeWithAccount = createClient({
-  account: accounts[0].address,
+  account: acc[0].address,
   chain: zksyncLocalNode,
   transport: http(),
 })
@@ -209,3 +209,16 @@ export function mockClientPublicActionsL2(client: any) {
     return mockRequestReturnData(method)
   }
 }
+
+export const accounts = [
+  {
+    address: '0x36615Cf349d7F6344891B1e7CA7C72883F5dc049',
+    privateKey:
+      '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110',
+  },
+  {
+    address: '0xa61464658AfeAf65CccaaFD3a512b69A83B77618',
+    privateKey:
+      '0xac1e735be8536c6534bb4f17f06f6afc73b2b5ba84ac2cfb12f7461b20c0bbe3',
+  },
+] as const
