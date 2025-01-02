3768421c3032e8e2ee0ce684a4643cdf75e90706
jxom <7336481+jxom@users.noreply.github.com>
2025-01-03 08:57:47 +1100

    chore: format

--- src/chains/definitions/humanodeTestnet5.ts
+++ src/chains/definitions/humanodeTestnet5.ts
@@ -12,7 +12,7 @@ export const humanodeTestnet5 = /*#__PURE__*/ defineChain({
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11'
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
     },
   },
 })
