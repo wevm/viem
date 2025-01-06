d555ad9304f97bdcf9d1ac3cef9e86ab9a25da80
Mary Becky <DenzilKopf3694@outlook.com>
2025-01-07 04:14:10 +0800

    fix: initVerseGenesis rpc (#3187)
    * feat: Added InitVerseGenesis Test Chain
    
    * feat:fixed initVerseGenesis rpc
    
    * feat:fixed initVerseGenesis rpc
    
    * feat:fixed initVerseGenesis rpc
    
    * feat:fixed initVerseGenesis rpc
    
    ---------
    
    Co-authored-by: caoyong <noraVemp@outlook.com>

--- src/chains/definitions/initVerseGenesis.ts
+++ src/chains/definitions/initVerseGenesis.ts
@@ -1,15 +1,15 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const initVerseGenesis = /*#__PURE__*/ defineChain({
-  id: 7234,
-  name: 'InitVerse Genesis Chain Testnet',
+  id: 7_234,
+  name: 'InitVerse Genesis Testnet',
   nativeCurrency: {
     decimals: 18,
     name: 'InitVerse',
     symbol: 'INI',
   },
   rpcUrls: {
-    default: { http: ['http://rpc-testnet.inichain.com'] },
+    default: { http: ['https://rpc-testnet.inichain.com'] },
   },
   blockExplorers: {
     default: {
