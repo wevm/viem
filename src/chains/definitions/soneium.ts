884f5ee5339e41d9724b25f15f634515666b507f
jxom <7336481+jxom@users.noreply.github.com>
2025-01-16 11:08:01 +1100

    chore: format

--- src/chains/definitions/soneium.ts
+++ src/chains/definitions/soneium.ts
@@ -24,28 +24,28 @@ export const soneium = /*#__PURE__*/ defineChain({
     ...chainConfig.contracts,
     disputeGameFactory: {
       [sourceId]: {
-        address: "0x512a3d2c7a43bd9261d2b8e8c9c70d4bd4d503c0",
+        address: '0x512a3d2c7a43bd9261d2b8e8c9c70d4bd4d503c0',
       },
     },
     l2OutputOracle: {
       [sourceId]: {
-        address: "0x0000000000000000000000000000000000000000",
+        address: '0x0000000000000000000000000000000000000000',
       },
     },
     portal: {
       [sourceId]: {
-        address: "0x88e529a6ccd302c948689cd5156c83d4614fae92",
+        address: '0x88e529a6ccd302c948689cd5156c83d4614fae92',
         blockCreated: 7061266,
       },
     },
     l1StandardBridge: {
       [sourceId]: {
-        address: "0xeb9bf100225c214efc3e7c651ebbadcf85177607",
+        address: '0xeb9bf100225c214efc3e7c651ebbadcf85177607',
         blockCreated: 7061266,
       },
     },
     multicall3: {
-      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 1,
     },
   },
