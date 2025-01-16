e1df486b02e62a9a9890752d42141671c0f37e25
jxom <7336481+jxom@users.noreply.github.com>
2025-01-16 11:31:28 +1100

    feat: remove webauthn-p256 in favor of Ox (#3232)
    * feat: remove webauthn-p256 in favor of Ox
    
    Co-Authored-By: Micah Zoltu <micah@zoltu.net>
    
    * chore: up size
    
    ---------
    
    Co-authored-by: Micah Zoltu <micah@zoltu.net>

--- src/account-abstraction/accounts/createWebAuthnCredential.ts
+++ src/account-abstraction/accounts/createWebAuthnCredential.ts
@@ -1,7 +1,27 @@
-// biome-ignore lint/performance/noBarrelFile:
-export {
-  type CreateCredentialParameters as CreateWebAuthnCredentialParameters,
-  type CreateCredentialReturnType as CreateWebAuthnCredentialReturnType,
-  type P256Credential,
-  createCredential as createWebAuthnCredential,
-} from 'webauthn-p256'
+// TODO(v3): Remove this in favor of `ox/WebAuthnP256` entirely.
+import * as PublicKey from 'ox/PublicKey'
+import * as WebAuthnP256 from 'ox/WebAuthnP256'
+
+import type { Hex } from '../../types/misc.js'
+
+export type P256Credential = {
+  id: WebAuthnP256.P256Credential['id']
+  publicKey: Hex
+  raw: WebAuthnP256.P256Credential['raw']
+}
+
+export type CreateWebAuthnCredentialParameters =
+  WebAuthnP256.createCredential.Options
+
+export type CreateWebAuthnCredentialReturnType = P256Credential
+
+export async function createWebAuthnCredential(
+  parameters: CreateWebAuthnCredentialParameters,
+): Promise<CreateWebAuthnCredentialReturnType> {
+  const credential = await WebAuthnP256.createCredential(parameters)
+  return {
+    id: credential.id,
+    publicKey: PublicKey.toHex(credential.publicKey, { includePrefix: false }),
+    raw: credential.raw,
+  }
+}
