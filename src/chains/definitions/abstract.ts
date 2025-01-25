036bf06350c60196be04bb90a77a6f9d2a145c11
Coffee ☕️ <coffee@coffeebot.dev>
2025-01-24 21:57:14 -0500

    chore: adjust config (#3272)

--- src/chains/definitions/abstract.ts
+++ src/chains/definitions/abstract.ts
@@ -1,37 +1,39 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 import { chainConfig } from '../../zksync/chainConfig.js'
 
-export const abstractTestnet = /*#__PURE__*/ defineChain({
+export const abstract = /*#__PURE__*/ defineChain({
   ...chainConfig,
-  id: 11_124,
-  name: 'Abstract Testnet',
+  id: 2_741,
+  name: 'Abstract',
   nativeCurrency: {
     decimals: 18,
     name: 'ETH',
     symbol: 'ETH',
   },
   rpcUrls: {
-    default: { http: ['https://api.testnet.abs.xyz'] },
+    default: {
+      http: ['https://api.mainnet.abs.xyz'],
+      webSocket: ['wss://api.mainnet.abs.xyz/ws'],
+    },
   },
   blockExplorers: {
     default: {
       name: 'Etherscan',
-      url: 'https://sepolia.abscan.org',
+      url: 'https://abscan.org',
     },
     native: {
       name: 'Abstract Explorer',
-      url: 'https://explorer.testnet.abs.xyz',
+      url: 'https://explorer.mainnet.abs.xyz',
     },
   },
-  testnet: true,
   contracts: {
     multicall3: {
-      address: '0xF9cda624FBC7e059355ce98a31693d299FACd963',
-      blockCreated: 358349,
+      address: '0xAa4De41dba0Ca5dCBb288b7cC6b708F3aaC759E7',
+      blockCreated: 5288,
     },
     universalSignatureVerifier: {
       address: '0xfB688330379976DA81eB64Fe4BF50d7401763B9C',
-      blockCreated: 431682,
+      blockCreated: 5263,
     },
   },
 })
