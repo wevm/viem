2ac15f0d467062792cdbf09e52ee0f14fe79ef4f
Vikas Bhansali <ensdeployer@gmail.com>
2025-01-29 11:00:43 +0800

    feat: add forta chain

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -171,6 +171,7 @@ export { fluence } from './definitions/fluence.js'
 export { fluenceStage } from './definitions/fluenceStage.js'
 export { fluenceTestnet } from './definitions/fluenceTestnet.js'
 export { forma } from './definitions/forma.js'
+export { forta } from './definitions/forta.js'
 /** @deprecated Use `anvil` instead. */
 export { foundry } from './definitions/foundry.js'
 export { fraxtal } from './definitions/fraxtal.js'
