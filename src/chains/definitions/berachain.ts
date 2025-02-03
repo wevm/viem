d21dec69e6b8abfd279136f15817477c4f2fd646
Camila <camirgarzon@gmail.com>
2025-02-03 16:20:03 -0500

    feat: add Berachain mainnet (#3299)
    * add berachain mainnet
    
    * add changeset
    
    * Update rotten-pears-begin.md
    
    ---------
    
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

---
+++ src/chains/definitions/berachain.ts
@@ -0,0 +1,20 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+export const berachain = /*#__PURE__*/ defineChain({
+  id: 80094,
+  name: 'Berachain',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BERA Token',
+    symbol: 'BERA',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.berachain.com'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Berascan',
+      url: 'https://berascan.com',
+    },
+  },
+  testnet: false,
+})
