41aab4288372b3e7a6591113be833b9c2aed6f40
Vikas Bhansali <ensdeployer@gmail.com>
2025-02-04 04:53:02 +0800

    feat: add donatuz chain (#3296)

---
+++ src/chains/definitions/donatuz.ts
@@ -0,0 +1,26 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const donatuz = /*#__PURE__*/ defineChain({
+  id: 42_026,
+  name: 'Donatuz',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: { http: ['ttps://rpc.donatuz.com'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Donatuz Explorer',
+      url: 'https://explorer.donatuz.com',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 0,
+    },
+  },
+})
