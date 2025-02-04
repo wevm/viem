5dfd7b844e2916e2e3cd92cbc51d262893878c1a
Nicolas Mahe <nicolas@mahe.me>
2025-02-04 14:38:46 +0700

    Add multicall3 to LightLink definition

--- src/chains/definitions/lightlinkPegasus.ts
+++ src/chains/definitions/lightlinkPegasus.ts
@@ -20,5 +20,11 @@ export const lightlinkPegasus = /*#__PURE__*/ defineChain({
       url: 'https://pegasus.lightlink.io',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 127_188_532,
+    },
+  },
   testnet: true,
 })
