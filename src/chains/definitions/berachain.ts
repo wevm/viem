f68da13342a8ceea434dce6cd61fef508ae946bb
Lethale <lethale@berakin.io>
2025-02-09 21:45:17 +0100

    feat: add ENS contracts to berachain L1 (#3325)
    * add ens to berachain
    
    * Create lovely-dodos-sit.md
    
    ---------
    
    Co-authored-by: Lethale <175728510+lethaale@users.noreply.github.com>
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

--- src/chains/definitions/berachain.ts
+++ src/chains/definitions/berachain.ts
@@ -12,6 +12,14 @@ export const berachain = /*#__PURE__*/ defineChain({
       address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 0,
     },
+    ensRegistry: {
+      address: '0x5b22280886a2f5e09a49bea7e320eab0e5320e28',
+      blockCreated: 877007,
+    },
+    ensUniversalResolver: {
+      address: '0xddfb18888a9466688235887dec2a10c4f5effee9',
+      blockCreated: 877008,
+    },
   },
   rpcUrls: {
     default: { http: ['https://rpc.berachain.com'] },
