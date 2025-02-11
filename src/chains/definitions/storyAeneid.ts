7e7f8447a7b94b0493c9b780267c3a36a974eeca
timtimtim.eth <timothyshen1@hotmail.com>
2025-02-11 12:22:21 -0800

    feat: add Story Mainnet/Story Aeneid testnet & remove deprecate testnet

---
+++ src/chains/definitions/storyAeneid.ts
@@ -0,0 +1,25 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const storyAeneid = /*#__PURE__*/ defineChain({
+  id: 13_15,
+  name: "aeneid",
+  nativeCurrency: { name: "IP", symbol: "IP", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://aeneid.storyrpc.io/"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Explorer",
+      url: "https://aeneid.storyscan.xyz/",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 5882,
+    },
+  },
+  testnet: true,
+});
