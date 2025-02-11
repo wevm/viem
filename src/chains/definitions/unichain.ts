e55e58725fbaa23531c821b9ba3ef7fd8df0b3f4
Yutaro-Mori-eng <Mori900103@outlook.com>
2025-02-11 08:31:58 +0800

    Add Unichain Mainnet

---
+++ src/chains/definitions/unichain.ts
@@ -0,0 +1,36 @@
+import { chainConfig } from '../../op-stack/chainConfig.js'
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+const sourceId = 1 // mainnet
+
+export const unichain = /*#__PURE__*/ defineChain({
+  ...chainConfig,
+  id: 130,
+  name: 'Unichain',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.unichain.org'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Uniscan',
+      url: 'https://uniscan.xyz',
+      apiUrl: 'https://api.uniscan.xyz/api',
+    },
+  },
+  contracts: {
+    ...chainConfig.contracts,
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 0,
+    },
+  },
+  testnet: false,
+  sourceId,
+})
