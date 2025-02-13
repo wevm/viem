7727a7d7d50aff7eb8d828982000f321e348cc5e
human85 <chenhao2110@qq.com>
2025-02-13 14:59:42 +0800

    Added blockExplorers object to token config

--- src/chains/definitions/story.ts
+++ src/chains/definitions/story.ts
@@ -8,6 +8,13 @@ export const story = /*#__PURE__*/ defineChain({
     name: 'IP Token',
     symbol: 'IP',
   },
+  blockExplorers: {
+    default: {
+      name: 'Story explorer',
+      url:'https://www.storyscan.xyz/',
+      apiUrl: 'https://www.storyscan.xyz/api/v2/'
+    }
+  },
   rpcUrls: {
     default: { http: ['https://mainnet.storyrpc.io'] },
   },
