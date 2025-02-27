170174e3e6e68cf1685ba20b7158ae0d63878f55
Emmanuel Oaikhenan <odia.emma@gmail.com>
2025-02-27 18:34:22 +0100

    Add Somnia Testnet

--- src/chains/definitions/somniaTestnet.ts
+++ src/chains/definitions/somniaTestnet.ts
@@ -1,20 +1,20 @@
-import { defineChain } from "../../utils/chain/defineChain.js";
+import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const somniaTestnet = /*#__PURE__*/ defineChain({
   id: 50312,
-  name: "Somnia Testnet",
-  nativeCurrency: { name: "STT", symbol: "STT", decimals: 18 },
+  name: 'Somnia Testnet',
+  nativeCurrency: { name: 'STT', symbol: 'STT', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ["https://dream-rpc.somnia.network/"],
+      http: ['https://dream-rpc.somnia.network/'],
     },
   },
   blockExplorers: {
     default: {
-      name: "Shannon Explorer",
-      url: "http://shannon-explorer.somnia.network/",
-      apiUrl: "https://shannon-explorer.somnia.network/api-docs",
+      name: 'Shannon Explorer',
+      url: 'http://shannon-explorer.somnia.network/',
+      apiUrl: 'https://shannon-explorer.somnia.network/api-docs',
     },
   },
   testnet: true,
-});
+})
