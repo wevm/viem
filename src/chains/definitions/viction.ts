7cb9e9098aa603f1a5fcbc685959bd927a753add
Abbas Khan <183810123+abbas9344@users.noreply.github.com>
2025-02-28 10:42:32 +0800

    Add multicall3 contract to Viction

--- src/chains/definitions/viction.ts
+++ src/chains/definitions/viction.ts
@@ -15,5 +15,11 @@ export const viction = /*#__PURE__*/ defineChain({
       url: 'https://vicscan.xyz',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 87169904,
+    },
+  },
   testnet: false,
 })
