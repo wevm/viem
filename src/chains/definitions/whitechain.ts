ee7053d7caf905e1b249ca236d9c7b8d5745d4bd
ccxdev <anywaycode@gmail.com>
2025-03-09 02:02:35 +0100

    Added Whitechain, WhitechainTestnet chains.

---
+++ src/chains/definitions/whitechain.ts
@@ -0,0 +1,26 @@
+import { defineChain } from "../../utils/chain/defineChain.js";
+
+export const whitechain = /*#__PURE__*/ defineChain({
+  testnet: false,
+  name: "Whitechain",
+  blockExplorers: {
+    default: {
+      name: "Whitechain Explorer",
+      url: "https://explorer.whitechain.io",
+    },
+  },
+  id: 1875,
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.whitechain.io"],
+    },
+    public: {
+      http: ["https://rpc.whitechain.io"],
+    },
+  },
+  nativeCurrency: {
+    decimals: 18,
+    name: "WhiteBIT Coin",
+    symbol: "WBT",
+  },
+});
