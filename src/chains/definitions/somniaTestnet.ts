b043411d2559dc975186d060dde89d920f33fdf7
jxom <7336481+jxom@users.noreply.github.com>
2025-03-17 14:22:49 +1100

    Update somniaTestnet.ts

--- src/chains/definitions/somniaTestnet.ts
+++ src/chains/definitions/somniaTestnet.ts
@@ -6,13 +6,13 @@ export const somniaTestnet = /*#__PURE__*/ defineChain({
   nativeCurrency: { name: 'STT', symbol: 'STT', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://dream-rpc.somnia.network/'],
+      http: ['https://dream-rpc.somnia.network'],
     },
   },
   blockExplorers: {
     default: {
       name: 'Somnia Testnet Explorer',
-      url: 'https://somnia-testnet.socialscan.io/',
+      url: 'https://somnia-testnet.socialscan.io',
       apiUrl: 'https://shannon-explorer.somnia.network/api',
     },
   },
