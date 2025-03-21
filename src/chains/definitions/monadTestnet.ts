158d6075862ed65b2c9cc5d39cab95dcb0a5d5a0
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2025-03-21 10:51:44 -0500

    feat: add more Monad Block Explorers

--- src/chains/definitions/monadTestnet.ts
+++ src/chains/definitions/monadTestnet.ts
@@ -2,21 +2,32 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const monadTestnet = /*#__PURE__*/ defineChain({
   id: 10_143,
+  network: 'monad-testnet',
   name: 'Monad Testnet',
   nativeCurrency: {
+    decimals: 18,
     name: 'Testnet MON Token',
     symbol: 'MON',
-    decimals: 18,
   },
   rpcUrls: {
-    default: {
-      http: ['https://testnet-rpc.monad.xyz'],
-    },
+    default: { http: ['https://testnet-rpc.monad.xyz'] },
   },
   blockExplorers: {
     default: {
-      name: 'Monad Testnet explorer',
-      url: 'https://testnet.monadexplorer.com',
+      name: 'Monad Explorer',
+      url: 'https://testnet.monadexplorer.xyz',
+      apiUrl: 'https://sourcify-api-monad.blockvision.org',
+    },
+    etherscan: {
+      name: 'MonadScan',
+      url: 'https://testnet.monadscan.com/',
+      apiUrl: 'https://api-testnet.monadscan.com/api',
+    },
+    socialscan: {
+      name: 'Monad SocialScan',
+      url: 'https://monad-testnet.socialscan.io/',
+      apiUrl:
+        'https://api.socialscan.io/monad-testnet/v1/explorer/command_api/contract',
     },
   },
   contracts: {
