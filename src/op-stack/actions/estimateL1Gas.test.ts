4087a07d840f8ceb3496db28b8440f5ede054f93
jxom <7336481+jxom@users.noreply.github.com>
2025-04-01 08:00:58 +1100

    fix: resolves #3513

--- src/op-stack/actions/estimateL1Gas.test.ts
+++ src/op-stack/actions/estimateL1Gas.test.ts
@@ -1,9 +1,14 @@
+import { Hex } from 'ox'
 import { expect, test } from 'vitest'
 
 import { accounts } from '~test/src/constants.js'
 import { anvilOptimism } from '../../../test/src/anvil.js'
-import { type TransactionRequestEIP1559, parseGwei } from '../../index.js'
-import { parseEther } from '../../utils/unit/parseEther.js'
+import { optimism } from '../../chains/index.js'
+import {
+  http,
+  type TransactionRequestEIP1559,
+  createClient,
+} from '../../index.js'
 import { estimateL1Gas } from './estimateL1Gas.js'
 
 const optimismClient = anvilOptimism.getClient()
@@ -11,20 +16,18 @@ const optimismClientWithAccount = anvilOptimism.getClient({ account: true })
 const optimismClientWithoutChain = anvilOptimism.getClient({ chain: false })
 
 const baseTransaction = {
-  maxFeePerGas: parseGwei('100'),
-  maxPriorityFeePerGas: parseGwei('1'),
+  data: '0xdeadbeef',
   to: accounts[1].address,
-  value: parseEther('0.1'),
 } as const satisfies Omit<TransactionRequestEIP1559, 'from'>
 
 test('default', async () => {
   const gas = await estimateL1Gas(optimismClientWithAccount, baseTransaction)
-  expect(gas).toBe(2028n)
+  expect(gas).toBe(2004n)
 })
 
 test('minimal', async () => {
   const gas = await estimateL1Gas(optimismClientWithAccount, {})
-  expect(gas).toBe(1600n)
+  expect(gas).toBe(1604n)
 })
 
 test('args: account', async () => {
@@ -32,7 +35,7 @@ test('args: account', async () => {
     ...baseTransaction,
     account: accounts[0].address,
   })
-  expect(gas).toBe(2028n)
+  expect(gas).toBe(2004n)
 })
 
 test('args: data', async () => {
@@ -40,7 +43,7 @@ test('args: data', async () => {
     ...baseTransaction,
     data: '0x00000000000000000000000000000000000000000000000004fefa17b7240000',
   })
-  expect(gas).toBe(2244n)
+  expect(gas).toBe(2156n)
 })
 
 test('args: gasPriceOracleAddress', async () => {
@@ -48,7 +51,7 @@ test('args: gasPriceOracleAddress', async () => {
     ...baseTransaction,
     gasPriceOracleAddress: '0x420000000000000000000000000000000000000F',
   })
-  expect(gas).toBe(2028n)
+  expect(gas).toBe(2004n)
 })
 
 test('args: nonce', async () => {
@@ -56,7 +59,7 @@ test('args: nonce', async () => {
     ...baseTransaction,
     nonce: 69,
   })
-  expect(gas).toBe(2028n)
+  expect(gas).toBe(2004n)
 })
 
 test('args: nullish chain', async () => {
@@ -65,5 +68,17 @@ test('args: nullish chain', async () => {
     account: accounts[0].address,
     chain: null,
   })
-  expect(gas).toBe(2028n)
+  expect(gas).toBe(2004n)
+})
+
+test('behavior: account with no funds', async () => {
+  const optimismClient = createClient({
+    chain: optimism,
+    transport: http(),
+  })
+  const gas = await estimateL1Gas(optimismClient, {
+    ...baseTransaction,
+    account: Hex.random(20),
+  })
+  expect(gas).toBeDefined()
 })
