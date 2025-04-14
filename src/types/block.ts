001e616f2ad84317f6b25b1cf2592c4b4a40a04c
jxom <7336481+jxom@users.noreply.github.com>
2025-04-14 10:47:13 +1000

    chore: pin foundry nightly

--- src/types/block.ts
+++ src/types/block.ts
@@ -83,7 +83,6 @@ export type BlockIdentifier<quantity = bigint> = {
     }
 )
 
-
 /** Represents a block number in the blockchain. */
 export type BlockNumber<quantity = bigint> = quantity
 
@@ -92,17 +91,17 @@ export type BlockNumber<quantity = bigint> = quantity
  *
  * - `"latest"`: the latest proposed block
  * - `"earliest"`: the earliest/genesis block – lowest numbered block the client has available
- * - `"pending"`: pending state/transactions – next block built by the client on top 
+ * - `"pending"`: pending state/transactions – next block built by the client on top
  *   of unsafe and containing the set of transactions usually taken from local mempool
- * - `"safe"`: the latest safe head block – the most recent block that is safe from 
+ * - `"safe"`: the latest safe head block – the most recent block that is safe from
  *   re-orgs under honest majority and certain synchronicity assumptions
- * - `"finalized"`: the latest finalized block – the most recent crypto-economically secure block; 
+ * - `"finalized"`: the latest finalized block – the most recent crypto-economically secure block;
  *   cannot be re-orged outside of manual intervention driven by community coordination
- * 
+ *
  * Using `pending`, while allowed, is not advised, as it may lead
  * to internally inconsistent results. Use of `latest` is safe and will not
  * lead to inconsistent results. Depending on the backing RPC networks caching system,
- * the usage of `pending` may lead to inconsistencies as a result of an 
+ * the usage of `pending` may lead to inconsistencies as a result of an
  * overly aggressive cache system. This may cause downstream errors/invalid states.
  */
 export type BlockTag = 'latest' | 'earliest' | 'pending' | 'safe' | 'finalized'
