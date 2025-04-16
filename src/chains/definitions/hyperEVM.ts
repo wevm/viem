f8d4a63334b6d4eb0baab06e822a56de43d57881
conradjohnson <skaterjames@gmail.com>
2025-04-15 20:41:08 -0500

    added hyperliquid mainnet and testnet.  Removed Wanchain testnet

---
+++ src/chains/definitions/hyperEVM.ts
@@ -0,0 +1,23 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+ 
+ export const hyperEVM = /*#__PURE__*/ defineChain({
+   id: 999,
+   name: 'Hyperliquid EVM',
+   nativeCurrency: { name: 'HYPE', symbol: 'HYPE', decimals: 18 },
+   rpcUrls: {
+     default: { http: ['https://rpc.hyperliquid.xyz/evm'] },
+   },
+   blockExplorers: {
+     default: {
+       name: 'Hyperliquid EVM Explorer',
+       url: 'https://hyperliquid.cloud.blockscout.com',
+     },
+   },
+   contracts: {
+     multicall3: {
+       address: '0xca11bde05977b3631167028862be2a173976ca11',
+       blockCreated: 13_051,
+     },
+   },
+   testnet: false,
+ })
---
+++ src/chains/definitions/hyperEVMTestnet.ts
@@ -0,0 +1,23 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+ 
+ export const hyperEVMTestnet = /*#__PURE__*/ defineChain({
+   id: 998,
+   name: 'Hyperliquid EVM Testnet',
+   nativeCurrency: { name: 'HYPE', symbol: 'HYPE', decimals: 18 },
+   rpcUrls: {
+     default: { http: ['https://rpc.hyperliquid-testnet.xyz/evm'] },
+   },
+   blockExplorers: {
+     default: {
+       name: 'Hyperliquid EVM Explorer',
+       url: 'https://testnet.purrsec.com',
+     },
+   },
+   contracts: {
+     multicall3: {
+       address: '0xca11bde05977b3631167028862be2a173976ca11',
+       blockCreated: 0,
+     },
+   },
+   testnet: true,
+ })
