60d7574d7f144dca4b1d0403a09573056d540972
jxom <7336481+jxom@users.noreply.github.com>
2025-05-05 11:50:30 +1000

    chore: tweaks

--- src/experimental/erc7846/index.ts
+++ src/experimental/erc7846/index.ts
@@ -5,5 +5,9 @@ export {
   type ConnectReturnType,
   connect,
 } from './actions/connect.js'
+export {
+  type DisconnectErrorType,
+  disconnect,
+} from './actions/disconnect.js'
 
 export { type Erc7846Actions, erc7846Actions } from './decorators/erc7846.js'
