91cb87f45b4c86106d5a1f326fc6bdefcedfcd51
jxom <7336481+jxom@users.noreply.github.com>
2025-05-15 11:32:05 +0200

    up

--- src/actions/wallet/sendCalls.test.ts
+++ src/actions/wallet/sendCalls.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { wagmiContractConfig } from '../../../test/src/abis.js'
 import { anvilMainnet } from '../../../test/src/anvil.js'
 import { accounts } from '../../../test/src/constants.js'
