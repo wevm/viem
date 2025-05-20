1bde5ecadbd9f97220a51fee519dbb09f1a940ea
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-05-20 18:16:16 +0700

    feat: up chiliz chain (#3660)
    * Update chiliz.ts
    
    the public RPC node keeps crashing and throwing errors. I’ve updated it.
    
    * Update chiliz.ts
    
    * Create brave-insects-build.md
    
    ---------
    
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

--- src/chains/definitions/chiliz.ts
+++ src/chains/definitions/chiliz.ts
@@ -11,7 +11,7 @@ export const chiliz = /*#__PURE__*/ defineChain({
   },
   rpcUrls: {
     default: {
-      http: ['https://chiliz-rpc.publicnode.com'],
+      http: ['https://rpc.chiliz.com'],
     },
   },
   blockExplorers: {
