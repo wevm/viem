38ab3df1218addfe55faf6260524a6533e33414f
sssmi <simeon.kerkola@gmail.com>
2025-05-24 16:26:59 +0300

    Add HyperEVM chain

---
+++ src/chains/definitions/hyperEvm.ts
@@ -0,0 +1,23 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const abstract = /*#__PURE__*/ defineChain({
+  id: 999,
+  name: 'HyperEVM',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'HYPE',
+    symbol: 'HYPE',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.hyperliquid.xyz/evm'],
+      webSocket: ['wss://hyperliquid.drpc.org'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Hyperscan',
+      url: 'https://www.hyperscan.com/',
+    },
+  },
+})
