f3071ab6bf43d586e9a04f3da77dd3e879b4af9a
jxom <7336481+jxom@users.noreply.github.com>
2025-05-25 20:28:22 +0200

    Update bitkub.ts

--- src/chains/definitions/bitkub.ts
+++ src/chains/definitions/bitkub.ts
@@ -2,8 +2,8 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 
 export const bitkub = /*#__PURE__*/ defineChain({
   id: 96,
-  name: 'KUB Chain',
-  nativeCurrency: { name: 'Bitkub', symbol: 'KUB', decimals: 18 },
+  name: 'KUB Mainnet',
+  nativeCurrency: { name: 'KUB Coin', symbol: 'KUB', decimals: 18 },
   rpcUrls: {
     default: {
       http: ['https://rpc.bitkubchain.io'],
