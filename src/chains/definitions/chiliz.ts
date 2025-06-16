980263e43a055c4a2b3a8e3cb627a773858dc3b9
Paul Razvan Berg <prberg@proton.me>
2025-06-16 21:36:54 +0300

    chore: list multicall3 on chains

--- src/chains/definitions/chiliz.ts
+++ src/chains/definitions/chiliz.ts
@@ -23,7 +23,7 @@ export const chiliz = /*#__PURE__*/ defineChain({
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 8080847,
     },
   },
