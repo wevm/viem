ba56c150b7e122eaa422f618d3960a1dee316a86
Emmanuel Oaikhenan <odia.emma@gmail.com>
2025-06-16 17:16:25 +0100

    Updated SomniaTestnet Multicallv3

---
+++ .changeset/selfish-chicken-jam.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Updated somniaTestnet Multicall3
--- src/chains/definitions/somniaTestnet.ts
+++ src/chains/definitions/somniaTestnet.ts
@@ -1,4 +1,4 @@
-import { defineChain } from '../../utils/chain/defineChain.js'
+import { defineChain } from '../../utils/chain/defineChain.js';
 
 export const somniaTestnet = /*#__PURE__*/ defineChain({
   id: 50312,
@@ -12,9 +12,15 @@ export const somniaTestnet = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: 'Somnia Testnet Explorer',
-      url: 'https://somnia-testnet.socialscan.io',
+      url: 'https://shannon-explorer.somnia.network/',
       apiUrl: 'https://shannon-explorer.somnia.network/api',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0x841b8199E6d3Db3C6f264f6C2bd8848b3cA64223',
+      blockCreated: 71314235,
+    },
+  },
   testnet: true,
-})
+});
