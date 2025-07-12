7df03302d7ac4ee3c62c807ffddb90c1bd429a9a
rizwanmoulvi <rizwan.m.1610@gmail.com>
2025-07-12 21:06:43 +0530

    feat(chains): add Xphere Chain

---
+++ src/chains/definitions/xphereTestnet.ts
@@ -0,0 +1,24 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const xphereTestnet = /*#__PURE__*/ defineChain({
+  id: 1998991,
+  name: 'Xphere Mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'XPT',
+    symbol: 'XPT',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://testnet.x-phere.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Xphere Tamsa Explorer',
+      url: 'https://xpt.tamsa.io/',
+    },
+  },
+  testnet: true,
+})
+
