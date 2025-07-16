e254f78b48eabbf9c134e0ffeed6fc264b100cae
Yutaro-Mori-eng <Mori900103@outlook.com>
2025-07-16 19:06:49 +0800

    feat: add plasma testnet (#3797)
    * Add Plasma Testnet
    
    * export

---
+++ src/chains/definitions/plasmaTestnet.ts
@@ -0,0 +1,23 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const plasmaTestnet = /*#__PURE__*/ defineChain({
+  id: 9746,
+  name: 'Plasma Testnet',
+  nativeCurrency: {
+    name: 'Testnet Plasma',
+    symbol: 'XPL',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://testnet-rpc.plasma.to'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'RouteScan',
+      url: 'https://testnet.plasmaexplorer.io',
+    },
+  },
+  testnet: true,
+})
