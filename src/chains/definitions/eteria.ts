ee93b20d07c7dee4e286a673e9d295e5dd4c13c6
Eteria <132741663+Eteriaio@users.noreply.github.com>
2025-09-01 23:40:25 +0800

    Add files via upload

---
+++ src/chains/definitions/eteria.ts
@@ -0,0 +1,25 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const eteria = /*#__PURE__*/ defineChain({
+  id: 140,
+  name: 'Eteria',
+  nativeCurrency: { name: 'Eteria', symbol: 'ERA', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.eteria.io/v1'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Arbiscan',
+      url: 'https://explorer.eteria.io',
+      apiUrl: 'https://explorer.eteria.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 7654707,
+    },
+  },
+})
