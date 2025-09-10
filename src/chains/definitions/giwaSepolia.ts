c6989fdb79806f61ee930935660eab9b61711865
graykode <nlkey2022@gmail.com>
2025-09-10 23:20:51 +0900

    feat: add Giwa Sepolia testnet configuration

---
+++ src/chains/definitions/giwaSepolia.ts
@@ -0,0 +1,36 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+const sourceId = 11_155_111 // sepolia
+
+export const giwaSepolia = defineChain({
+  id: 91342,
+  name: 'Giwa Sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://sepolia-rpc.giwa.io'],
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+    },
+    l2OutputOracle: {},
+    disputeGameFactory: {
+      [sourceId]: {
+        address: '0x37347caB2afaa49B776372279143D71ad1f354F6',
+      },
+    },
+    portal: {
+      [sourceId]: {
+        address: '0x956962C34687A954e611A83619ABaA37Ce6bC78A',
+      },
+    },
+    l1StandardBridge: {
+      [sourceId]: {
+        address: '0x77b2ffc0F57598cAe1DB76cb398059cF5d10A7E7',
+      },
+    },
+  },
+  testnet: true,
+})
