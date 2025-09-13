2430b6cad70341c8234da26de30f233588af5805
Phillip-Kemper <33121795+Phillip-Kemper@users.noreply.github.com>
2025-09-13 09:41:10 +0200

    Add Operator Fee to OP Stack

---
+++ src/op-stack/actions/estimateOperatorFee.test.ts
@@ -0,0 +1,70 @@
+import { expect, test } from 'vitest'
+
+import { accounts } from '~test/src/constants.js'
+
+import { anvilOptimism } from '../../../test/src/anvil.js'
+import { type TransactionRequestEIP1559, parseGwei } from '../../index.js'
+import { parseEther } from '../../utils/unit/parseEther.js'
+import { estimateOperatorFee } from './estimateOperatorFee.js'
+
+const optimismClient = anvilOptimism.getClient()
+const optimismClientWithAccount = anvilOptimism.getClient({ account: true })
+const optimismClientWithoutChain = anvilOptimism.getClient({ chain: false })
+
+const baseTransaction = {
+  maxFeePerGas: parseGwei('100'),
+  maxPriorityFeePerGas: parseGwei('1'),
+  to: accounts[1].address,
+  value: parseEther('0.1'),
+} as const satisfies Omit<TransactionRequestEIP1559, 'from'>
+
+test('default', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithAccount, baseTransaction)
+  expect(fee).toBeDefined()
+})
+
+test('minimal', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithAccount, {})
+  expect(fee).toBeDefined()
+})
+
+test('args: account', async () => {
+  const fee = await estimateOperatorFee(optimismClient, {
+    ...baseTransaction,
+    account: accounts[0].address,
+  })
+  expect(fee).toBeDefined()
+})
+
+test('args: data', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithAccount, {
+    ...baseTransaction,
+    data: '0x00000000000000000000000000000000000000000000000004fefa17b7240000',
+  })
+  expect(fee).toBeDefined()
+})
+
+test('args: l1BlockAddress', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithAccount, {
+    ...baseTransaction,
+    l1BlockAddress: '0x4200000000000000000000000000000000000015',
+  })
+  expect(fee).toBeDefined()
+})
+
+test('args: nonce', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithAccount, {
+    ...baseTransaction,
+    nonce: 69,
+  })
+  expect(fee).toBeDefined()
+})
+
+test('args: nullish chain', async () => {
+  const fee = await estimateOperatorFee(optimismClientWithoutChain, {
+    ...baseTransaction,
+    account: accounts[0].address,
+    chain: null,
+  })
+  expect(fee).toBeDefined()
+})
