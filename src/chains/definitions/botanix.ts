3d89537b036504128dabfede145878f4f924d377
jxom <7336481+jxom@users.noreply.github.com>
2025-09-15 20:52:28 +1000

    Update botanix.ts

--- src/chains/definitions/botanix.ts
+++ src/chains/definitions/botanix.ts
@@ -7,21 +7,14 @@ export const botanix = /*#__PURE__*/ defineChain({
   nativeCurrency: { name: 'Bitcoin', symbol: 'BTC', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://rpc.botanixlabs.com '],
+      http: ['https://rpc.botanixlabs.com'],
       webSocket: ['wss://rpc.botanixlabs.com/ws'],
     },
   },
   blockExplorers: {
     default: {
       name: 'Botanixscan',
-      url: 'https://botanixscan.io/',
-      apiUrl: 'https://botanixscan.io/documentation',
-    },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 516999,
+      url: 'https://botanixscan.io',
     },
   },
 })
