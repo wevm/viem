dfa8a14bd5c0d6378fb45b5cf8005f6f3624cbc5
jxom <7336481+jxom@users.noreply.github.com>
2025-09-18 08:00:32 +1000

    tests: up anvil

--- src/utils/rpc/http.test.ts
+++ src/utils/rpc/http.test.ts
@@ -23,7 +23,7 @@ describe('request', () => {
       {
         "id": 1,
         "jsonrpc": "2.0",
-        "result": "anvil/v1.3.5",
+        "result": "anvil/v1.3.6",
       }
     `)
   })
@@ -38,7 +38,7 @@ describe('request', () => {
       {
         "id": 1,
         "jsonrpc": "2.0",
-        "result": "anvil/v1.3.5",
+        "result": "anvil/v1.3.6",
       }
     `)
   })
@@ -432,12 +432,12 @@ describe('http (batch)', () => {
         {
           "id": 1,
           "jsonrpc": "2.0",
-          "result": "anvil/v1.3.5",
+          "result": "anvil/v1.3.6",
         },
         {
           "id": 2,
           "jsonrpc": "2.0",
-          "result": "anvil/v1.3.5",
+          "result": "anvil/v1.3.6",
         },
       ]
     `)
@@ -458,7 +458,7 @@ describe('http (batch)', () => {
         {
           "id": 1,
           "jsonrpc": "2.0",
-          "result": "anvil/v1.3.5",
+          "result": "anvil/v1.3.6",
         },
         {
           "error": {
@@ -484,7 +484,7 @@ describe('http (batch)', () => {
         {
           "id": 1,
           "jsonrpc": "2.0",
-          "result": "anvil/v1.3.5",
+          "result": "anvil/v1.3.6",
         },
         {
           "error": {
--- src/utils/rpc/webSocket.test.ts
+++ src/utils/rpc/webSocket.test.ts
@@ -204,7 +204,7 @@ describe.runIf(process.env.VITE_NETWORK_TRANSPORT_MODE === 'webSocket')(
       expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.3.5",
+        "result": "anvil/v1.3.6",
       }
     `)
       expect(socketClient.requests.size).toBe(0)
@@ -703,7 +703,7 @@ describe.runIf(process.env.VITE_NETWORK_TRANSPORT_MODE === 'webSocket')(
       expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.3.5",
+        "result": "anvil/v1.3.6",
       }
     `)
       expect(client.requests.size).toBe(0)
