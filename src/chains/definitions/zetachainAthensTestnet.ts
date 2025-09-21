4d8d6fa621adead3ea4863fd0b46269716ce69c5
jxom <7336481+jxom@users.noreply.github.com>
2025-09-21 14:50:17 +1000

    Update zetachainAthensTestnet.ts

--- src/chains/definitions/zetachainAthensTestnet.ts
+++ src/chains/definitions/zetachainAthensTestnet.ts
@@ -22,7 +22,7 @@ export const zetachainAthensTestnet = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: 'ZetaScan',
-      url: 'https://testnet.zetascan.com/',
+      url: 'https://testnet.zetascan.com',
     },
   },
   testnet: true,
