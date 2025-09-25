618f4102d4c9cf9fa504a25bc69e89baa5268091
SergeevDmitry <sergeevdms@gmail.com>
2025-09-25 13:53:03 +0200

    feat: add multicall3 contract to Stratis and Auroria chains

--- src/chains/definitions/stratis.ts
+++ src/chains/definitions/stratis.ts
@@ -20,4 +20,10 @@ export const stratis = /*#__PURE__*/ defineChain({
       url: 'https://explorer.stratisevm.com',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 1_420_372,
+    },
+  },
 })
