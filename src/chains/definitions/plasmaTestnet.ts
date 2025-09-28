912509b2a03072b81af0f5f0f723dca1996e0635
Ilja <3154053+xzilja@users.noreply.github.com>
2025-09-28 04:39:13 +0300

    feat: add plasma multicall3 data (#3980)
    * feat: add plasma multicall3 data
    
    * Add multicall3 contracts to Plasma
    
    ---------
    
    Co-authored-by: jxom <7336481+jxom@users.noreply.github.com>

--- src/chains/definitions/plasmaTestnet.ts
+++ src/chains/definitions/plasmaTestnet.ts
@@ -19,5 +19,11 @@ export const plasmaTestnet = /*#__PURE__*/ defineChain({
       url: 'https://testnet.plasmascan.to',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 0,
+    },
+  },
   testnet: true,
 })
