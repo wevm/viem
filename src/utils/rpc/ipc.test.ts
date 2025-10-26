ceff3273628acba5dfb711c9cbe0531dd9173223
jxom <7336481+jxom@users.noreply.github.com>
2025-10-26 15:16:15 +1100

    chore: up anvil

--- src/utils/rpc/ipc.test.ts
+++ src/utils/rpc/ipc.test.ts
@@ -64,7 +64,7 @@ describe('request', () => {
     expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.4.3",
+        "result": "anvil/v1.4.4",
       }
     `)
     expect(rpcClient.requests.size).toBe(0)
@@ -315,7 +315,7 @@ describe('requestAsync', () => {
     expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.4.3",
+        "result": "anvil/v1.4.4",
       }
     `)
     expect(client.requests.size).toBe(0)
