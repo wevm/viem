9bccdb2cb445270833468c1c273d6e0f2c1dbdf7
Coffee ☕️ <coffee@cubelabs.xyz>
2025-10-30 10:38:29 -0400

    chore: update block times for zksync fast block chains (#4064)
    * chore: update block times for zksync fast block chains
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: Tom Meagher <tom@meagher.co>

---
+++ .changeset/cruel-toys-punch.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Added `blockTime` for `abstract`, `abstractTestnet`, `sophon`, `sophonTestnet`, `zksync`, `zksyncSepoliaTestnet`.
--- src/chains/definitions/sophon.ts
+++ src/chains/definitions/sophon.ts
@@ -3,6 +3,7 @@ import { chainConfig } from '../../zksync/chainConfig.js'
 
 export const sophon = /*#__PURE__*/ defineChain({
   ...chainConfig,
+  blockTime: 200,
   id: 50104,
   name: 'Sophon',
   nativeCurrency: {
