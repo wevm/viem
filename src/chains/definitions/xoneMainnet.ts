76823e48d234acf5cb16d78f93d524ee024f153b
jxom <7336481+jxom@users.noreply.github.com>
2025-11-03 11:09:59 +1100

    Update src/chains/definitions/xoneMainnet.ts

--- src/chains/definitions/xoneMainnet.ts
+++ src/chains/definitions/xoneMainnet.ts
@@ -18,11 +18,5 @@ export const xoneMiannet = /*#__PURE__*/ defineChain({
       apiUrl: 'http://api.xonescan.com/api',
     },
   },
-  contracts: {
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 16456390,
-    },
-  },
   testnet: false,
 })
