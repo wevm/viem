3a638b3f321de3213423fc9134eef2055752f080
richiedunk <richie.dunk@gmail.com>
2025-11-06 21:17:19 +0000

    Update somnia.ts

--- src/chains/definitions/somnia.ts
+++ src/chains/definitions/somnia.ts
@@ -3,7 +3,7 @@ import { defineChain } from '../../utils/chain/defineChain.js'
 export const somnia = defineChain({
   id: 5031,
   name: 'Somnia',
-  nativeCurrency: { name: 'SOMI', symbol: 'SOMI', decimals: 18 },
+  nativeCurrency: { name: 'Somnia', symbol: 'SOMI', decimals: 18 },
   rpcUrls: {
     default: {
       http: ['https://api.infra.mainnet.somnia.network'],
