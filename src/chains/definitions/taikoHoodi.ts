0269e8cffd98f72cee0ef3daac7b45d341a9e74f
Myron Rotter <myronrotter01@t-online.de>
2025-11-06 09:56:13 +0100

    chore: use official multicall3

--- src/chains/definitions/taikoHoodi.ts
+++ src/chains/definitions/taikoHoodi.ts
@@ -7,12 +7,19 @@ export const taikoHoodi = /*#__PURE__*/ defineChain({
   rpcUrls: {
     default: {
       http: ['https://rpc.hoodi.taiko.xyz'],
+      webSocket: ['wss://ws.hoodi.taiko.xyz'],
     },
   },
   blockExplorers: {
     default: {
       name: 'Etherscan',
-      url: 'https://hoodi.taikoscan.io',
+      url: 'https://hoodi.taikoscan.io/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 581116,
     },
   },
   testnet: true,
