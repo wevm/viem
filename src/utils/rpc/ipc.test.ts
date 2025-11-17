e9e04dc2fb715e40fa20f5b52a604b60308f1d92
jxom <7336481+jxom@users.noreply.github.com>
2025-11-17 19:41:41 +1100

    chore: up anvil

--- src/utils/rpc/ipc.test.ts
+++ src/utils/rpc/ipc.test.ts
@@ -64,7 +64,7 @@ describe('request', () => {
     expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.4.4",
+        "result": "anvil/v1.5.0",
       }
     `)
     expect(rpcClient.requests.size).toBe(0)
@@ -315,7 +315,7 @@ describe('requestAsync', () => {
     expect(version).toMatchInlineSnapshot(`
       {
         "jsonrpc": "2.0",
-        "result": "anvil/v1.4.4",
+        "result": "anvil/v1.5.0",
       }
     `)
     expect(client.requests.size).toBe(0)
