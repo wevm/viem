94ff7e2b1ea94d1342d0ca11fd6b71acf40d4e45
manuelbarbas <manuel@skalelabs.com>
2025-12-15 12:26:07 +0000

    skale base chain id update

--- src/chains/definitions/skale/skaleBase.ts
+++ src/chains/definitions/skale/skaleBase.ts
@@ -1,7 +1,7 @@
 import { defineChain } from '../../../utils/chain/defineChain.js'
 
 export const skaleBase = /*#__PURE__*/ defineChain({
-  id: 1562508942,
+  id: 1187947933,
   name: 'SKALE Base',
   nativeCurrency: { name: 'Credits', symbol: 'CREDIT', decimals: 18 },
   rpcUrls: {
