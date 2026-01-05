787c8c222fc114b9bd0a9a134573cb7237fee16d
Tony D'Addeo <amdaddeo@gmail.com>
2026-01-05 16:47:50 -0600

    reviewed changes

--- src/utils/buildRequest.ts
+++ src/utils/buildRequest.ts
@@ -153,7 +153,7 @@ export function buildRequest<
         withRetry(
           async () => {
             try {
-              return await request(args, { signal })
+              return await request(args, signal ? { signal } : undefined)
             } catch (err_) {
               const err = err_ as unknown as RpcError<
                 RpcErrorCode | ProviderRpcErrorCode
