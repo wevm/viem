dbc5675f18742d95a34bb30e2f647068b3e2d252
Lukas <lukasstrassel@googlemail.com>
2026-01-11 12:02:42 +0100

    perf: decodeLogs

--- src/utils/abi/parseEventLogs.bench.ts
+++ src/utils/abi/parseEventLogs.bench.ts
@@ -1,57 +1,57 @@
-import { bench } from 'vitest'
+import { bench } from "vitest";
 
-import { anvilMainnet } from '~test/anvil.js'
-import { getLogs } from '../../actions/public/getLogs.js'
-import { mainnet } from '../../chains/index.js'
-import { createClient } from '../../clients/createClient.js'
-import { http } from '../../clients/transports/http.js'
-import { parseEventLogs } from './parseEventLogs.js'
+import { anvilMainnet } from "../../../test/src/anvil.js";
+import { getLogs } from "../../actions/public/getLogs.js";
+import { mainnet } from "../../chains/index.js";
+import { createClient } from "../../clients/createClient.js";
+import { http } from "../../clients/transports/http.js";
+import { parseEventLogs } from "./parseEventLogs.js";
 
 const client = createClient({
   chain: mainnet,
   transport: http(),
-})
+});
 
 const abi = [
   {
-    name: 'Transfer',
-    type: 'event',
+    name: "Transfer",
+    type: "event",
     inputs: [
       {
         indexed: true,
-        name: 'from',
-        type: 'address',
+        name: "from",
+        type: "address",
       },
       {
         indexed: true,
-        name: 'to',
-        type: 'address',
+        name: "to",
+        type: "address",
       },
       {
         indexed: false,
-        name: 'value',
-        type: 'uint256',
+        name: "value",
+        type: "uint256",
       },
     ],
   },
   {
-    type: 'event',
-    name: 'Approval',
+    type: "event",
+    name: "Approval",
     inputs: [
       {
         indexed: true,
-        name: 'owner',
-        type: 'address',
+        name: "owner",
+        type: "address",
       },
       {
         indexed: true,
-        name: 'spender',
-        type: 'address',
+        name: "spender",
+        type: "address",
       },
       {
         indexed: false,
-        name: 'value',
-        type: 'uint256',
+        name: "value",
+        type: "uint256",
       },
     ],
   },
@@ -59,23 +59,23 @@ const abi = [
     inputs: [
       {
         indexed: true,
-        name: 'message',
-        type: 'string',
+        name: "message",
+        type: "string",
       },
     ],
-    name: 'Foo',
-    type: 'event',
+    name: "Foo",
+    type: "event",
   },
-] as const
+] as const;
 
 const logs = await getLogs(client, {
   fromBlock: anvilMainnet.forkBlockNumber - 5n,
   toBlock: anvilMainnet.forkBlockNumber,
-})
+});
 
-bench('parseEventLogs', async () => {
+bench("parseEventLogs", async () => {
   parseEventLogs({
     abi,
     logs,
-  })
-})
+  });
+});
