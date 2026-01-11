eaf1410dabbd9912cfc00885dab0f81eb55f64be
Lukas <lukasstrassel@googlemail.com>
2026-01-11 12:07:22 +0100

    perf: decodeLogs

---
+++ src/node/trustedSetups.ts
@@ -0,0 +1,15 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(
+  // import.meta' meta-property is only allowed when the '--module' option is 'es2020', 'es2022', 'esnext', 'system', 'node16', or 'nodenext'
+  // biome-ignore lint/suspicious/noTsIgnore: _
+  // @ts-ignore
+  import.meta.url,
+)
+const __dirname = dirname(__filename)
+
+export const mainnetTrustedSetupPath = resolve(
+  __dirname,
+  '../trusted-setups/mainnet.json',
+)
