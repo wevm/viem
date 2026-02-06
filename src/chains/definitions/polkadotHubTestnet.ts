736d4b6d38ca7c5b0884c57ddf236fb80ccdae91
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2026-02-06 10:31:38 +0100

    feat: add Polkadot Hub Testnet chain definition

---
+++ src/chains/definitions/polkadotHubTestnet.ts
@@ -0,0 +1,23 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const polkadotHubTestnet = /*#__PURE__*/ defineChain({
+  id: 420_420_417,
+  name: 'Polkadot Hub TestNet',
+  nativeCurrency: {
+    name: 'PAS',
+    symbol: 'PAS',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://services.polkadothub-rpc.com/testnet'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://blockscout-testnet.polkadot.io',
+    },
+  },
+  testnet: true,
+})
