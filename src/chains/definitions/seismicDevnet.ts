91b78b7ef85d8a10e6fe3f3f3e3f87f3a59a342d
Christian Drappi <c@seismic.systems>
2026-02-10 15:20:42 -0500

    Update seismic testnet RPC url
    We launched our testnet. In light of this, we deprecated Seismic devnet 2. This commit adds the `seismicTestnet` network. To avoid breaking people's imports, we aliased `seismicDevnet` to this new Seismic testnet

--- src/chains/definitions/seismicDevnet.ts
+++ src/chains/definitions/seismicDevnet.ts
@@ -1,19 +1,22 @@
 import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const seismicDevnet = /*#__PURE__*/ defineChain({
+export const seismicTestnet = /*#__PURE__*/ defineChain({
   id: 5124,
-  name: 'Seismic Devnet',
+  name: 'Seismic Testnet',
   nativeCurrency: { name: 'Seismic Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
     default: {
-      http: ['https://node-2.seismicdev.net/rpc'],
+      http: ['https://gcp-2.seismictest.net/rpc'],
+      webSocket: ['wss://gcp-2.seismictest.net/ws']
     },
   },
   blockExplorers: {
     default: {
-      name: 'Seismic Devnet Explorer',
-      url: 'https://explorer-2.seismicdev.net',
+      name: 'Seismic Testnet Explorer',
+      url: 'https://seismic-testnet.socialscan.io',
     },
   },
   testnet: true,
 })
+
+export const seismicDevnet = seismicTestnet
