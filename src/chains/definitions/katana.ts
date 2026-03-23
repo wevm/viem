55663cd21ca4a0250ce7024f1dfc03792523f8b1
Nicolas DECOSTERD <ndecosterd@katana.network>
2026-03-23 16:10:59 +0100

    feat: add Katana chain multicall3 data

--- src/chains/definitions/katana.ts
+++ src/chains/definitions/katana.ts
@@ -20,5 +20,11 @@ export const katana = /*#__PURE__*/ defineChain({
       url: 'https://katanascan.com',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 0
+    }
+  },
   testnet: false,
 })
