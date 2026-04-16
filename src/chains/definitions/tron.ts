82be374589d3340713effc67e84fe935b6332333
Paul <108695806+pxrl@users.noreply.github.com>
2026-04-16 10:58:56 +0000

    chore: Add Tron blockTime + multicall deployment

--- src/chains/definitions/tron.ts
+++ src/chains/definitions/tron.ts
@@ -9,11 +9,18 @@ export const tron = /*#__PURE__*/ defineChain({
       http: ['https://api.trongrid.io/jsonrpc'],
     },
   },
+  blockTime: 3000,
   blockExplorers: {
     default: {
       name: 'Tronscan',
       url: 'https://tronscan.org',
       apiUrl: 'https://apilist.tronscanapi.com/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0x32a4F47A74a6810BD0bF861CABAb99656a75DE9E',
+      blockCreated: 51_067_989,
     },
   },
 })
