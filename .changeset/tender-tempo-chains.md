118bc22b80f07c30bfca4a4f61ae98a49882fe61
tmm <tmm@tmm.dev>
2026-05-18 11:58:51 -0400

    feat(tempo): chains entrypoint

---
+++ .changeset/tender-tempo-chains.md
@@ -0,0 +1,5 @@
+---
+"viem": patch
+---
+
+Added `viem/tempo/chains` entrypoint
---
+++ src/tempo/chains/index.ts
@@ -0,0 +1,8 @@
+// biome-ignore lint/performance/noBarrelFile: entrypoint module
+export { tempo, tempo as tempoMainnet } from '../../chains/definitions/tempo.js'
+export { tempoDevnet } from '../../chains/definitions/tempoDevnet.js'
+export { tempoLocalnet } from '../../chains/definitions/tempoLocalnet.js'
+export {
+  tempoModerato,
+  tempoModerato as tempoTestnet,
+} from '../../chains/definitions/tempoModerato.js'
