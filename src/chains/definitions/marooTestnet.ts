b6a3165a69d04c16ca8a6d02dfe2d9cab9223798
Wonhee Lee <2wheeh@gmail.com>
2026-06-15 11:48:44 +0900

    feat(chains): add MarooTestnet (450815)

---
+++ src/chains/definitions/marooTestnet.ts
@@ -0,0 +1,26 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const marooTestnet = /*#__PURE__*/ defineChain({
+  id: 450815,
+  name: 'Maroo Testnet',
+  nativeCurrency: { name: 'Testnet OKRW', symbol: 'tOKRW', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-testnet.maroo.io'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://explorer-testnet.maroo.io',
+      apiUrl: 'https://explorer-testnet.maroo.io/blockscout/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 0,
+    },
+  },
+  testnet: true,
+})
