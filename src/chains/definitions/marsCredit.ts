12b7a55b2ba7e02ea2712fda993113c766158e68
Jer Crane <jeremyccrane@me.com>
2026-06-18 00:08:42 +0000

    feat(chains): add Mars Credit chain (chainId 110110)

---
+++ src/chains/definitions/marsCredit.ts
@@ -0,0 +1,21 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const marsCredit = /*#__PURE__*/ defineChain({
+  id: 110110,
+  name: 'Mars Credit',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Mars Credit',
+    symbol: 'MARS',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.marscredit.xyz'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://blockscan.marscredit.xyz',
+      apiUrl: 'https://blockscan.marscredit.xyz/api',
+    },
+  },
+})
