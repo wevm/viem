e70fcfae2417df3d132cd464218fdbe7fa51d942
defiQUG <defi@defi-oracle.io>
2026-07-06 01:57:17 -0700

    add Defi Oracle Meta Mainnet chain

---
+++ src/chains/definitions/defiOracleMetaMainnet.ts
@@ -0,0 +1,41 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const defiOracleMetaMainnet = /*#__PURE__*/ defineChain({
+  id: 138,
+  name: 'Defi Oracle Meta Mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: [
+        'https://rpc-http-pub.d-bis.org',
+        'https://rpc.d-bis.org',
+        'https://rpc2.d-bis.org',
+        'https://rpc.public-0138.defi-oracle.io',
+        'https://rpc.defi-oracle.io',
+      ],
+      webSocket: [
+        'wss://rpc-ws-pub.d-bis.org',
+        'wss://ws.rpc.d-bis.org',
+        'wss://ws.rpc2.d-bis.org',
+        'wss://rpc.public-0138.defi-oracle.io',
+        'wss://wss.defi-oracle.io',
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://explorer.d-bis.org',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 5274349,
+    },
+  },
+})
