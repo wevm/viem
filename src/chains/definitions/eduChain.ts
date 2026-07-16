35933495babf0078d20bf27dcfdaca1cada22d60
m4ttw4n <m4ttw4n@users.noreply.github.com>
2026-07-16 09:12:51 +0800

    Update eduChain.ts
    Updating RPC and explorer URLs + add multicall3

--- src/chains/definitions/eduChain.ts
+++ src/chains/definitions/eduChain.ts
@@ -10,13 +10,19 @@ export const eduChain = /*#__PURE__*/ defineChain({
   },
   rpcUrls: {
     default: {
-      http: ['https://rpc.edu-chain.raas.gelato.cloud'],
+      http: ['https://rpc.educhain.xyz'],
     },
   },
   blockExplorers: {
     default: {
       name: 'EDU Chain Explorer',
-      url: 'https://educhain.blockscout.com/',
+      url: 'https://explorer.educhain.xyz/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 16410660,
     },
   },
   testnet: false,
