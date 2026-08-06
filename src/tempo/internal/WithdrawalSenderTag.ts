96d4cc7c3129a67838f117e01351673713463a10
Steven Truong <struong@users.noreply.github.com>
2026-08-06 12:52:23 -0400

    refactor(tempo): simplified withdrawal sender tag

--- src/tempo/internal/WithdrawalSenderTag.ts
+++ src/tempo/internal/WithdrawalSenderTag.ts
@@ -1,22 +1,9 @@
 import type { Address } from 'abitype'
 import { AbiParameters, Hash, type Hex } from 'ox'
 
-/** Derives a withdrawal sender tag. @internal */
+/** Derives a user withdrawal sender tag. @internal */
 export function from(options: from.Options): Hex.Hex {
   const { fallbackNonce, sender, transactionHash } = options
-  if (
-    sender === '0x0000000000000000000000000000000000000000' &&
-    fallbackNonce === 0n
-  )
-    return Hash.keccak256(
-      AbiParameters.encodePacked(
-        ['address', 'bytes32'],
-        [
-          sender,
-          '0x0000000000000000000000000000000000000000000000000000000000000000',
-        ],
-      ),
-    )
   return Hash.keccak256(
     AbiParameters.encodePacked(
       ['address', 'bytes32', 'uint64'],
