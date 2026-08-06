7b2f2275ac792e979bbed0abcd4c8543dd16cca7
struong <steven@tempo.xyz>
2026-08-06 12:59:41 -0400

    fix(tempo): include withdrawal fallback nonce (#4980)
    * fix(tempo): included withdrawal fallback nonce
    
    * refactor(tempo): simplified withdrawal sender tag
    
    ---------
    
    Co-authored-by: Steven Truong <struong@users.noreply.github.com>

--- src/tempo/internal/WithdrawalSenderTag.ts
+++ src/tempo/internal/WithdrawalSenderTag.ts
@@ -1,19 +1,20 @@
 import type { Address } from 'abitype'
 import { AbiParameters, Hash, type Hex } from 'ox'
 
-/** Derives a withdrawal sender tag from its sender and transaction hash. @internal */
+/** Derives a user withdrawal sender tag. @internal */
 export function from(options: from.Options): Hex.Hex {
-  const { sender, transactionHash } = options
+  const { fallbackNonce, sender, transactionHash } = options
   return Hash.keccak256(
     AbiParameters.encodePacked(
-      ['address', 'bytes32'],
-      [sender, transactionHash],
+      ['address', 'bytes32', 'uint64'],
+      [sender, transactionHash, fallbackNonce],
     ),
   )
 }
 
 export declare namespace from {
   type Options = {
+    fallbackNonce: bigint
     sender: Address
     transactionHash: Hex.Hex
   }
