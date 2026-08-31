d27d876b2111f5885ee4e950e5d9284cea3a8680
jxom <7336481+jxom@users.noreply.github.com>
2026-08-31 17:13:54 +1000

    Delete src/tempo/index.test-d.ts

--- src/tempo/index.test-d.ts
+++
@@ -1,9 +0,0 @@
-import type { Owner } from 'ox/tempo/MultisigConfig'
-import { expectTypeOf, test } from 'vitest'
-import type { z_MultisigConfigOwner, z_MultisigOwnerState } from './index.js'
-import type { MultisigOwnerState } from './Transaction.js'
-
-test('exports types required for inference', () => {
-  expectTypeOf<z_MultisigConfigOwner>().toEqualTypeOf<Owner>()
-  expectTypeOf<z_MultisigOwnerState>().toEqualTypeOf<MultisigOwnerState>()
-})
