410d4ee1a31c551b1d9a4d51c09061c4e76faa19
cryptocandyio <info@cryptocandy.io>
2026-09-24 14:12:23 +0530

    feat: add multicall3 to CandyChain

--- src/chains/definitions/candyChain.ts
+++ src/chains/definitions/candyChain.ts
@@ -17,4 +17,10 @@ export const candyChain = /*#__PURE__*/ defineChain({
       url: 'https://streams.candychain.io',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 2039339,
+    },
+  },
 })
